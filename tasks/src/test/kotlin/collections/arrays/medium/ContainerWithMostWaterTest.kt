796228b6e66ae6354ca0ad7c512438f4c84e07ae
skuznetsov <sergey.kuznetsov@infobip.com>
2021-08-02 08:48:04 +0300

    Rename tests

--- tasks/src/test/kotlin/collections/arrays/medium/ContainerWithMostWaterTest.kt
+++ tasks/src/test/kotlin/collections/arrays/medium/ContainerWithMostWaterTest.kt
@@ -5,28 +5,28 @@ import kotlin.test.assertEquals
 
 class ContainerWithMostWaterTest {
     @Test
-    fun `example 1 test`() {
+    fun test1() {
         val heights = intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7)
         val area = 49
         assertEquals(area, ContainerWithMostWater.maxArea(heights))
     }
 
     @Test
-    fun `example 2 test`() {
+    fun test2() {
         val heights = intArrayOf(1, 1)
         val area = 1
         assertEquals(area, ContainerWithMostWater.maxArea(heights))
     }
 
     @Test
-    fun `example 3 test`() {
+    fun test3() {
         val heights = intArrayOf(4, 3, 2, 1, 4)
         val area = 16
         assertEquals(area, ContainerWithMostWater.maxArea(heights))
     }
 
     @Test
-    fun `example 4 test`() {
+    fun test4() {
         val heights = intArrayOf(1, 2, 1)
         val area = 2
         assertEquals(area, ContainerWithMostWater.maxArea(heights))
