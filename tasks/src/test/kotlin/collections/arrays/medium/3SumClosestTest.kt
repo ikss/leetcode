181629619bac5587c6a034a5de5a342072360e0d
skuznetsov <sergey.kuznetsov@infobip.com>
2021-08-02 08:47:56 +0300

    Task: 3SumClosest

---
+++ tasks/src/test/kotlin/collections/arrays/medium/3SumClosestTest.kt
@@ -0,0 +1,20 @@
+package collections.arrays.medium
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class `3SumClosestTest` {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(-1, 2, 1, -4)
+        val expected = 2
+        assertEquals(expected, `3SumClosest`.threeSumClosest(nums, 1))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(-1, 0, 1, -4, 2, 3)
+        val expected = 0
+        assertEquals(expected, `3SumClosest`.threeSumClosest(nums, 0))
+    }
+}
