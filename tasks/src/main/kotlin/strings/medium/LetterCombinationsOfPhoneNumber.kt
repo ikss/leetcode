c39aada2eefd6428053318c897c7edb7623eada9
skuznetsov <sergey.kuznetsov@infobip.com>
2022-03-31 21:21:38 +0300

    Task: LetterCombinationsOfPhoneNumber

---
+++ tasks/src/main/kotlin/strings/medium/LetterCombinationsOfPhoneNumber.kt
@@ -0,0 +1,34 @@
+package strings.medium
+
+/**
+ * Given a string containing digits from 2-9 inclusive,
+ * return all possible letter combinations that the number could represent.
+ * Return the answer in any order.
+ *
+ * URL: [https://leetcode.com/problems/letter-combinations-of-a-phone-number/]
+ */
+object LetterCombinationsOfPhoneNumber {
+
+    val mapping: Map<Char, Set<Char>> = mapOf(
+        '2' to setOf('a', 'b', 'c'),
+        '3' to setOf('d', 'e', 'f'),
+        '4' to setOf('g', 'h', 'i'),
+        '5' to setOf('j', 'k', 'l'),
+        '6' to setOf('m', 'n', 'o'),
+        '7' to setOf('p', 'q', 'r', 's'),
+        '8' to setOf('s', 't', 'u'),
+        '9' to setOf('w', 'x', 'y', 'z'),
+    )
+
+    fun letterCombinationsOfPhoneNumber(digits: String): List<String> {
+        if (digits.isEmpty()) return emptyList()
+        val chars = digits.toCharArray()
+        return cartesianProduct(chars.map { mapping[it]!! })
+    }
+
+    private fun cartesianProduct(lists: List<Set<Char>>): List<String> =
+        lists.fold(listOf("")) { acc, set ->
+            acc.flatMap { list -> set.map { element -> list + element } }
+        }
+
+}
---
+++ tasks/src/test/kotlin/strings/medium/LetterCombinationsOfPhoneNumberTest.kt
@@ -0,0 +1,42 @@
+package strings.medium
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class LetterCombinationsOfPhoneNumberTest {
+
+    @Test
+    fun test1() {
+        val digits = "23"
+        val excepted = listOf("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf")
+        assertEquals(excepted, LetterCombinationsOfPhoneNumber.letterCombinationsOfPhoneNumber(digits))
+    }
+
+    @Test
+    fun test2() {
+        val digits = ""
+        val excepted = emptyList<String>()
+        assertEquals(excepted, LetterCombinationsOfPhoneNumber.letterCombinationsOfPhoneNumber(digits))
+    }
+
+    @Test
+    fun test3() {
+        val digits = "7"
+        val excepted = listOf("p", "q", "r", "s")
+        assertEquals(excepted, LetterCombinationsOfPhoneNumber.letterCombinationsOfPhoneNumber(digits))
+    }
+
+    @Test
+    fun test4() {
+        val digits = "2"
+        val excepted = listOf("a", "b", "c")
+        assertEquals(excepted, LetterCombinationsOfPhoneNumber.letterCombinationsOfPhoneNumber(digits))
+    }
+
+    @Test
+    fun test5() {
+        val digits = "234"
+        val excepted = listOf("adg", "adh", "adi", "aeg", "aeh", "aei", "afg", "afh", "afi", "bdg", "bdh", "bdi", "beg", "beh", "bei", "bfg", "bfh", "bfi", "cdg", "cdh", "cdi", "ceg", "ceh", "cei", "cfg", "cfh", "cfi")
+        assertEquals(excepted, LetterCombinationsOfPhoneNumber.letterCombinationsOfPhoneNumber(digits))
+    }
+}
