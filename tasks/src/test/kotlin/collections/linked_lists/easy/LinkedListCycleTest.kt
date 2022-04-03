94ea8f354128fcf587685253e2b29561e93ce334
skuznetsov <sergey.kuznetsov@infobip.com>
2022-04-03 21:07:40 +0300

    Test: linked list cycle

---
+++ tasks/src/test/kotlin/collections/linked_lists/easy/LinkedListCycleTest.kt
@@ -0,0 +1,23 @@
+package collections.linked_lists.easy
+
+import collections.linked_lists.util.ListNode
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class LinkedListCycleTest {
+
+    @Test
+    fun test1() {
+        val node2 = ListNode(2)
+        node2.next = ListNode(3, ListNode(4, node2))
+        val list = ListNode(1, node2)
+        assertTrue(LinkedListCycle.hasCycle(list))
+    }
+
+    @Test
+    fun test2() {
+        val list = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
+        assertFalse(LinkedListCycle.hasCycle(list))
+    }
+}
