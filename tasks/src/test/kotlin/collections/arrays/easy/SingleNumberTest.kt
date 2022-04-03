7d6c23d06aea9895bb25caa5e7eacb8fcb7fb3cb
skuznetsov <sergey.kuznetsov@infobip.com>
2022-04-03 17:31:01 +0300

    Task: single number

---
+++ tasks/src/test/kotlin/collections/arrays/easy/SingleNumberTest.kt
@@ -0,0 +1,27 @@
+package collections.arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SingleNumberTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(2, 2, 1)
+        val expected = 1
+        assertEquals(expected, SingleNumber.singleNumber(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(4, 1, 2, 1, 2)
+        val expected = 4
+        assertEquals(expected, SingleNumber.singleNumber(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(1)
+        val expected = 1
+        assertEquals(expected, SingleNumber.singleNumber(nums))
+    }
+}
