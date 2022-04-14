1124f01885d2517b8b99ee18765dd67ef3a3cc6d
skuznetsov <sergey.kuznetsov@infobip.com>
2022-04-14 22:45:28 +0300

    Task: RemoveNthNod

--- shared/src/main/kotlin/data_structures/ListNode.kt
+++ shared/src/main/kotlin/data_structures/ListNode.kt
@@ -6,3 +6,17 @@ data class ListNode(var value: Int, var next: ListNode? = null) {
         return "$value -> $next"
     }
 }
+
+object ListBuilder {
+    fun build(values: List<Int?>?): ListNode? {
+        if (values.isNullOrEmpty()) return null
+        return build(values, 0)
+    }
+
+    private fun build(values: List<Int?>, index: Int): ListNode? {
+        if (index >= values.size) return null
+        val `val` = values[index]
+            ?: return null
+        return ListNode(`val`, build(values, index + 1))
+    }
+}
