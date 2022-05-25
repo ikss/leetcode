b11329680f6a3ada15f06bfc47c9bea9765daa4c
skuznetsov <sergey.kuznetsov@infobip.com>
2022-05-25 23:20:42 +0300

    Task: Count Primes

---
+++ tasks/src/test/kotlin/math/easy/CountPrimesTest.kt
@@ -0,0 +1,28 @@
+package math.easy
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CountPrimesTest {
+
+    @Test
+    fun test1() {
+        val n = 10
+
+        assertEquals(4, CountPrimes.countPrimes(n))
+    }
+
+    @Test
+    fun test2() {
+        val n = 0
+
+        assertEquals(0, CountPrimes.countPrimes(n))
+    }
+
+    @Test
+    fun test3() {
+        val n = 1
+
+        assertEquals(0, CountPrimes.countPrimes(n))
+    }
+}
