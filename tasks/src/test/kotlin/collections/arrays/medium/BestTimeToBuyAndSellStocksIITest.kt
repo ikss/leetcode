a601cc43aabcb544c45bfed18c5b50f0ead87908
skuznetsov <sergey.kuznetsov@infobip.com>
2022-05-26 23:28:39 +0300

    Task: Best Time To Buy And Sell Stocks II

---
+++ tasks/src/test/kotlin/collections/arrays/medium/BestTimeToBuyAndSellStocksIITest.kt
@@ -0,0 +1,28 @@
+package collections.arrays.medium
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class BestTimeToBuyAndSellStocksIITest {
+
+    @Test
+    fun testSimple1() {
+        val arr = intArrayOf(7, 1, 5, 3, 6, 4)
+        val expected = 7
+        assertEquals(expected, BestTimeToBuyAndSellStocksII.maxProfit(arr))
+    }
+
+    @Test
+    fun testSimple2() {
+        val arr = intArrayOf(1, 2, 3, 4, 5)
+        val expected = 4
+        assertEquals(expected, BestTimeToBuyAndSellStocksII.maxProfit(arr))
+    }
+
+    @Test
+    fun testKadane1() {
+        val arr = intArrayOf(7, 6, 4, 3, 1)
+        val expected = 0
+        assertEquals(expected, BestTimeToBuyAndSellStocksII.maxProfit(arr))
+    }
+}
