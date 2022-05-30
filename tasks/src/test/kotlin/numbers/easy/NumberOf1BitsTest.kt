994df24aae1a9092ebca97fb0599ba1c7b2bcb24
skuznetsov <sergey.kuznetsov@infobip.com>
2022-05-30 16:47:41 +0300

    Task: Number of 1 bits

---
+++ tasks/src/test/kotlin/numbers/easy/NumberOf1BitsTest.kt
@@ -0,0 +1,28 @@
+package numbers.easy
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class NumberOf1BitsTest {
+
+    @Test
+    fun test1() {
+        val number = 11
+        val expected = 3
+        assertEquals(expected, NumberOf1Bits.hammingWeight(number))
+    }
+
+    @Test
+    fun test2() {
+        val number = 128
+        val expected = 1
+        assertEquals(expected, NumberOf1Bits.hammingWeight(number))
+    }
+
+    @Test
+    fun test3() {
+        val number = -3
+        val expected = 31
+        assertEquals(expected, NumberOf1Bits.hammingWeight(number))
+    }
+}
