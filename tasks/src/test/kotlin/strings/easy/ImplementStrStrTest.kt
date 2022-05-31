6b6457c5f2339ea8b5c17999cee73c6d88fda849
skuznetsov <sergey.kuznetsov@infobip.com>
2022-05-31 20:00:23 +0300

    Task: Implement StrStr

---
+++ tasks/src/test/kotlin/strings/easy/ImplementStrStrTest.kt
@@ -0,0 +1,31 @@
+package strings.easy
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ImplementStrStrTest {
+
+    @Test
+    fun test1() {
+        val haystack = "hello"
+        val needle = "ll"
+        val expected = 2
+        assertEquals(expected, ImplementStrStr.strStr(haystack, needle))
+    }
+
+    @Test
+    fun test2() {
+        val haystack = "aaaaa"
+        val needle = "baa"
+        val expected = -1
+        assertEquals(expected, ImplementStrStr.strStr(haystack, needle))
+    }
+
+    @Test
+    fun test3() {
+        val haystack = "hello"
+        val needle = ""
+        val expected = 0
+        assertEquals(expected, ImplementStrStr.strStr(haystack, needle))
+    }
+}
