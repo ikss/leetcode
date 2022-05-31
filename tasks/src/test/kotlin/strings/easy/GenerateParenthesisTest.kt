b8150c10244bdc4c994e9c728504ea5058fc8dab
skuznetsov <sergey.kuznetsov@infobip.com>
2022-05-31 19:55:38 +0300

    Rename tests

--- tasks/src/test/kotlin/strings/easy/GenerateParenthesisTest.kt
+++ tasks/src/test/kotlin/strings/easy/GenerateParenthesisTest.kt
@@ -3,7 +3,7 @@ package strings.easy
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-class GenerateParenthesesTest {
+class GenerateParenthesisTest {
 
     @Test
     fun test1() {
