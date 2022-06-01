1fc4e1a44a99afdfc923b2139a4e6d1336db8555
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-01 19:08:03 +0300

    Task: Rotate Array

---
+++ tasks/src/test/kotlin/collections/arrays/medium/RotateArrayTest.kt
@@ -0,0 +1,33 @@
+package collections.arrays.medium
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class RotateArrayTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+        val k = 3
+        RotateArray.rotate(nums, k)
+        val expected = intArrayOf(5, 6, 7, 1, 2, 3, 4)
+        assertTrue(expected.contentEquals(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(-1, -100, 3, 99)
+        val k = 2
+        RotateArray.rotate(nums, k)
+        val expected = intArrayOf(3, 99, -1, -100)
+        assertTrue(expected.contentEquals(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(-1)
+        val k = 2
+        RotateArray.rotate(nums, k)
+        val expected = intArrayOf(-1)
+        assertTrue(expected.contentEquals(nums))
+    }
+}
