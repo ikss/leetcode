939729239d55ee177cf221b16c32a047b290a982
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-02 22:09:22 +0300

    Task: Pascal Triangle

---
+++ tasks/src/main/kotlin/math/easy/PascalTriangle.kt
@@ -0,0 +1,22 @@
+package math.easy
+
+/**
+ * Given an integer numRows, return the first numRows of Pascal's triangle.
+ *
+ * [URL](https://leetcode.com/problems/pascals-triangle/)
+ */
+object PascalTriangle {
+    fun generate(numRows: Int): List<List<Int>> {
+        val result = mutableListOf<List<Int>>()
+        result += listOf(1)
+        for (i in 1 until numRows) {
+            val row = mutableListOf(1)
+            for (j in 1 until i) {
+                row += result[i - 1][j - 1] + result[i - 1][j]
+            }
+            row += 1
+            result += row
+        }
+        return result
+    }
+}
---
+++ tasks/src/test/kotlin/math/easy/PascalTriangleTest.kt
@@ -0,0 +1,21 @@
+package math.easy
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class PascalTriangleTest {
+
+    @Test
+    fun test1() {
+        val n = 5
+        val expected = listOf(listOf(1), listOf(1, 1), listOf(1, 2, 1), listOf(1, 3, 3, 1), listOf(1, 4, 6, 4, 1))
+        assertEquals(expected, PascalTriangle.generate(n))
+    }
+
+    @Test
+    fun test2() {
+        val n = 1
+        val expected = listOf(listOf(1))
+        assertEquals(expected, PascalTriangle.generate(n))
+    }
+}
