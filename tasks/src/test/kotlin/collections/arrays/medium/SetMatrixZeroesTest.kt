1f767308a06f81864a5a0ed96f2769b53fa1927f
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-05 12:49:52 +0300

    Task: Set Matrix Zeroes

---
+++ tasks/src/test/kotlin/collections/arrays/medium/SetMatrixZeroesTest.kt
@@ -0,0 +1,24 @@
+package collections.arrays.medium
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class SetMatrixZeroesTest {
+    @Test
+    fun test1() {
+        val nums = arrayOf(intArrayOf(1, 1, 1), intArrayOf(1, 0, 1), intArrayOf(1, 1, 1))
+        SetMatrixZeroes.setZeroes(nums)
+
+        val expected = arrayOf(intArrayOf(1, 0, 1), intArrayOf(0, 0, 0), intArrayOf(1, 0, 1))
+        assertTrue(expected.contentDeepEquals(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = arrayOf(intArrayOf(0, 1, 2, 0), intArrayOf(3, 4, 5, 2), intArrayOf(1, 3, 1, 5))
+        SetMatrixZeroes.setZeroes(nums)
+
+        val expected = arrayOf(intArrayOf(0, 0, 0, 0), intArrayOf(0, 4, 5, 0), intArrayOf(0, 3, 1, 0))
+        assertTrue(expected.contentDeepEquals(nums))
+    }
+}
