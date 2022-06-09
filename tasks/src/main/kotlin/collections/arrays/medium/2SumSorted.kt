52896c87bd17cba1ee8b11cba79f3ce1cec0a6fd
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-09 16:11:11 +0300

    Task: 2 Sum Sorted Array

---
+++ tasks/src/main/kotlin/collections/arrays/medium/2SumSorted.kt
@@ -0,0 +1,34 @@
+package collections.arrays.medium
+
+/**
+ * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+ * find two numbers such that they add up to a specific target number.
+ * Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+ *
+ * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+ *
+ * The tests are generated such that there is exactly one solution. You may not use the same element twice.
+ *
+ * Your solution must use only constant extra space.
+ *
+ * [URL](https://leetcode.com/problems/3sum-closest/)
+ */
+object `2SumSorted` {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        var left = 0
+        var right = numbers.size - 1
+
+        while (left < right) {
+            val sum = numbers[left] + numbers[right]
+            if (sum == target) {
+                break
+            } else if (sum < target) {
+                left++
+            } else {
+                right--
+            }
+        }
+
+        return intArrayOf(++left, ++right)
+    }
+}
