eddce65ef431f7437de54dfbe19dceb605d06814
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-09 16:37:23 +0300

    Remove unneded return

--- may-leetcoding-challenge/src/main/kotlin/week3/Day6.KthSmallestInBST.kt
+++ may-leetcoding-challenge/src/main/kotlin/week3/Day6.KthSmallestInBST.kt
@@ -5,9 +5,10 @@ import data_structures.TreeNode
 object KthSmallestInBST {
     fun kthSmallest(root: TreeNode?, k: Int): Int {
         if (root == null) return 0
+
         val countLeft = count(root.left) + 1
         return when {
-            countLeft == k -> return root.`val`
+            countLeft == k -> root.`val`
             countLeft < k -> kthSmallest(root.right, k - countLeft)
             else -> kthSmallest(root.left, k)
         }
