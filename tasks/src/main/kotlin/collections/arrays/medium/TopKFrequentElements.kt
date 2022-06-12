dcdf6708ae23efca123bec984970962b0fb5826e
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-12 11:29:25 +0300

    Task: Top K Frequent Elements (PriorityQueue)

--- tasks/src/main/kotlin/collections/arrays/medium/TopKFrequentElements.kt
+++ tasks/src/main/kotlin/collections/arrays/medium/TopKFrequentElements.kt
@@ -1,5 +1,7 @@
 package collections.arrays.medium
 
+import java.util.*
+
 /**
  * Given an integer array nums and an integer k, return the k most frequent elements.
  * You may return the answer in any order.
@@ -13,4 +15,20 @@ object TopKFrequentElements {
 
         return map.take(k).map { it.key }.toIntArray()
     }
+
+    fun topKFrequentPriorityQueue(nums: IntArray, k: Int): IntArray {
+        val countMap = mutableMapOf<Int, Int>()
+        for (n in nums) {
+            countMap.compute(n) { _, v -> (v ?: 0) + 1 }
+        }
+        val pq = PriorityQueue<Map.Entry<Int, Int>> { o1, o2 -> o2.value - o1.value }
+        pq.addAll(countMap.entries)
+
+        val ret = IntArray(k)
+        for (i in 0 until k) {
+            ret[i] = pq.poll().key
+        }
+        return ret
+    }
+
 }
--- tasks/src/test/kotlin/collections/arrays/medium/TopKFrequentElementsTest.kt
+++ tasks/src/test/kotlin/collections/arrays/medium/TopKFrequentElementsTest.kt
@@ -21,4 +21,22 @@ class TopKFrequentElementsTest {
 
         assertTrue(expected.contentEquals(TopKFrequentElements.topKFrequent(nums, k)))
     }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(1, 1, 1, 2, 2, 3)
+        val k = 2
+        val expected = intArrayOf(1, 2)
+
+        assertTrue(expected.contentEquals(TopKFrequentElements.topKFrequentPriorityQueue(nums, k)))
+    }
+
+    @Test
+    fun test4() {
+        val nums = intArrayOf(1)
+        val k = 1
+        val expected = intArrayOf(1)
+
+        assertTrue(expected.contentEquals(TopKFrequentElements.topKFrequentPriorityQueue(nums, k)))
+    }
 }
