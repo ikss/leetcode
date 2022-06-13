5b138e3d10bc3b7e3414b4b7a8ea6b4f0c4dabad
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-13 21:38:09 +0300

    Task: Triangle

---
+++ tasks/src/test/kotlin/collections/arrays/medium/TriangleTest.kt
@@ -0,0 +1,39 @@
+package collections.arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TriangleTest {
+
+    @Test
+    fun testDfs1() {
+        val triangle = listOf(listOf(2), listOf(3, 4), listOf(6, 5, 7), listOf(4, 1, 8, 3))
+        val expected = 11
+
+        assertEquals(expected, Triangle.minimumTotalDfs(triangle))
+    }
+
+    @Test
+    fun testDfs2() {
+        val triangle = listOf(listOf(-11))
+        val expected = -11
+
+        assertEquals(expected, Triangle.minimumTotalDfs(triangle))
+    }
+
+    @Test
+    fun testDp1() {
+        val triangle = listOf(listOf(2), listOf(3, 4), listOf(6, 5, 7), listOf(4, 1, 8, 3))
+        val expected = 11
+
+        assertEquals(expected, Triangle.minimumTotalDfs(triangle))
+    }
+
+    @Test
+    fun testDp2() {
+        val triangle = listOf(listOf(-11))
+        val expected = -11
+
+        assertEquals(expected, Triangle.minimumTotalDfs(triangle))
+    }
+}
