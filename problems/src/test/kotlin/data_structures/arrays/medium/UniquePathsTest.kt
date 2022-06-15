82237da713b09203a7012c9093f8c3c8e53855bd
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-15 19:33:26 +0300

    Task: Unique Path

---
+++ problems/src/test/kotlin/data_structures/arrays/medium/UniquePathsTest.kt
@@ -0,0 +1,26 @@
+package data_structures.arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class UniquePathsTest {
+
+    @Test
+    fun test1() {
+        val m = 3
+        val n = 7
+        val expected = 28
+
+        assertEquals(expected, UniquePaths.uniquePaths(m,n))
+    }
+
+    @Test
+    fun test2() {
+        val m = 3
+        val n = 2
+        val expected = 3
+
+        assertEquals(expected, UniquePaths.uniquePaths(m,n))
+    }
+}
