dd20333aba4821646e6073e32402e5099ac65932
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-17 20:04:17 +0300

    Task: Decode Ways (recursion + memo)

---
+++ problems/src/main/kotlin/strings/medium/DecodeWays.kt
@@ -0,0 +1,45 @@
+package strings.medium
+
+
+/**
+ * A message containing letters from A-Z can be encoded into numbers using the following mapping:
+ *
+ *  * 'A' -> "1"
+ *  * 'B' -> "2"
+ *  * ...
+ *  * 'Z' -> "26"
+ *
+ * To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+ *
+ *  * "AAJF" with the grouping (1 1 10 6)
+ *  * "KJF" with the grouping (11 10 6)
+ * Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+ *
+ * Given a string s containing only digits, return the number of ways to decode it.
+ *
+ * The test cases are generated so that the answer fits in a 32-bit integer.
+ *
+ * [URL](https://leetcode.com/problems/decode-ways/)
+ */
+object DecodeWays {
+    fun numDecodingsRecursive(s: String): Int {
+        if (s.isEmpty()) return 0
+        val memo = IntArray(s.length) { -1 }
+        return numDecodings(0, s, memo)
+    }
+
+    private fun numDecodings(i: Int, s: String, memo: IntArray): Int {
+        val length = s.length
+        if (i == length) return 1
+        if (s[i] == '0') return 0
+        if (memo[i] != -1) return memo[i]
+
+        var res = numDecodings(i + 1, s, memo)
+
+        if (i < length - 1 && (s[i] == '1' || (s[i] == '2' && s[i + 1] < '7'))) {
+            res += numDecodings(i + 2, s, memo)
+        }
+        memo[i] = res
+        return res
+    }
+}
---
+++ problems/src/test/kotlin/strings/medium/DecodeWaysTest.kt
@@ -0,0 +1,31 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class DecodeWaysTest {
+
+    @Test
+    fun test1() {
+        val s = "12"
+        val expected = 2
+
+        assertEquals(expected, DecodeWays.numDecodingsRecursive(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "226"
+        val expected = 3
+
+        assertEquals(expected, DecodeWays.numDecodingsRecursive(s))
+    }
+
+    @Test
+    fun test3() {
+        val s = "06"
+        val expected = 0
+
+        assertEquals(expected, DecodeWays.numDecodingsRecursive(s))
+    }
+}
