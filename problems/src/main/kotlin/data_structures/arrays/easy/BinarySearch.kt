fd4d98bcd0811a267cc80f297ef3a705b39d4b13
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-18 19:39:57 +0300

    Task: Binary Search

---
+++ problems/src/main/kotlin/data_structures/arrays/easy/BinarySearch.kt
@@ -0,0 +1,27 @@
+package data_structures.arrays.easy
+
+/**
+ * Given an array of integers nums which is sorted in ascending order, and an integer target, write a function
+ * to search target in nums. If target exists, then return its index. Otherwise, return -1.
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ * [URL](https://leetcode.com/problems/binary-search/)
+ */
+object BinarySearch {
+    fun search(nums: IntArray, target: Int): Int {
+        if (nums[0] > target || nums[nums.size - 1] < target) return -1
+
+        var l = 0
+        var r = nums.size - 1
+
+        while (l <= r) {
+            val tmp = (r - l) / 2 + l
+            if (nums[tmp] == target) return tmp
+            else if (nums[tmp] > target) r = tmp - 1
+            else l = tmp + 1
+            println("l: $l, r: $r, tmp: $tmp")
+        }
+
+        return -1
+    }
+}
