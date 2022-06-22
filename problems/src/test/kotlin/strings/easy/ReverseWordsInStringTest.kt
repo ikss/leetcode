4081eaebff65c1e039cfaaec69a269545ca43c03
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-22 10:00:40 +0300

    Task: Reverse Words In a String

---
+++ problems/src/test/kotlin/strings/easy/ReverseWordsInStringTest.kt
@@ -0,0 +1,31 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ReverseWordsInStringTest {
+
+    @Test
+    fun test1() {
+        val s = "Let's take LeetCode contest"
+        val expected = "s'teL ekat edoCteeL tsetnoc"
+
+        assertEquals(expected, ReverseWordsInStringIII.reverseWords(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "God Ding"
+        val expected = "doG gniD"
+
+        assertEquals(expected, ReverseWordsInStringIII.reverseWords(s))
+    }
+
+    @Test
+    fun test4() {
+        val s = "ABCDE"
+        val expected = "EDCBA"
+
+        assertEquals(expected, ReverseWordsInStringIII.reverseWords(s))
+    }
+}
