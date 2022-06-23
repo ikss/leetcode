751d1b39933cc90e9f06e489a3a60e38df49554b
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-23 12:07:22 +0300

    Task: Middle Of the Linked List

---
+++ problems/src/main/kotlin/data_structures/linked_lists/easy/MiddleOfTheLinkedList.kt
@@ -0,0 +1,24 @@
+package data_structures.linked_lists.easy
+
+import data_structures.ListNode
+
+/**
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ *
+ * [URL](https://leetcode.com/problems/middle-of-the-linked-list/)
+ */
+object MiddleOfTheLinkedList {
+    fun middleNode(head: ListNode?): ListNode? {
+        if (head?.next == null) return head
+
+        var slow = head
+        var fast = head.next
+        while (fast != null) {
+            slow = slow?.next
+            fast = fast.next?.next
+        }
+        return slow
+    }
+}
