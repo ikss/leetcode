2c49cdc95def8aaa292b10e5ceb283cb9d7d854d
skuznetsov <sergey.kuznetsov@infobip.com>
2022-06-30 10:36:59 +0300

    Task: Permutation in String

---
+++ problems/src/test/kotlin/strings/medium/PermutationInStringTest.kt
@@ -0,0 +1,18 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class PermutationsTest {
+
+    @Test
+    fun test1() {
+        assertTrue(PermutationInString.checkInclusion("ab", "eidbaooo"))
+    }
+
+    @Test
+    fun test2() {
+        assertFalse(PermutationInString.checkInclusion("ab", "eidboaoo"))
+    }
+}
