3300e2ce51159d8acc417847d8c5c76cd3c3a8a8
skuznetsov <sergey.kuznetsov@infobip.com>
2022-07-04 17:01:19 +0300

    Tests formatting

--- problems/src/test/kotlin/data_structures/arrays/hard/MedianTwoSortedArraysTest.kt
+++ problems/src/test/kotlin/data_structures/arrays/hard/MedianTwoSortedArraysTest.kt
@@ -6,55 +6,65 @@ import org.junit.jupiter.api.Test
 class MedianTwoSortedArraysTest {
 
     @Test
-    fun `example 1 test`() {
+    fun test1() {
         val nums1 = intArrayOf(1, 3)
         val nums2 = intArrayOf(2)
+        val expected = 2.0
 
-        assertEquals(2.0, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
+        assertEquals(expected, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
     }
 
     @Test
-    fun `example 2 test`() {
+    fun test2() {
         val nums1 = intArrayOf(1, 2)
         val nums2 = intArrayOf(3, 4)
+        val expected = 2.5
 
-        assertEquals(2.5, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
+        assertEquals(expected, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
     }
 
     @Test
-    fun `example 3 test`() {
+    fun test3() {
         val nums1 = intArrayOf(0, 0)
         val nums2 = intArrayOf(0, 0)
+        val expected = 0.0
 
-        assertEquals(0.0, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
+        assertEquals(expected, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
     }
 
     @Test
-    fun `example 4 test`() {
+    fun test4() {
         val nums1 = intArrayOf()
         val nums2 = intArrayOf(1)
+        val expected = 1.0
 
-        assertEquals(1.0, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
+        assertEquals(expected, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
     }
 
     @Test
-    fun `example 5 test`() {
+    fun test5() {
         val nums1 = intArrayOf(2)
         val nums2 = intArrayOf()
-        assertEquals(2.0, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
+        val expected = 2.0
+
+        assertEquals(expected, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
     }
 
     @Test
-    fun `example 6 test`() {
+    fun test6() {
         val nums1 = intArrayOf(2, 2, 4, 4)
         val nums2 = intArrayOf(2, 2, 4, 4)
-        assertEquals(3.0, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
+        val expected = 3.0
+
+        assertEquals(expected, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
     }
 
     @Test
-    fun `example 7 test`() {
+    fun test7() {
         val nums1 = intArrayOf()
         val nums2 = intArrayOf(1, 2, 3, 4, 5)
-        assertEquals(3.0, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
+        val expected = 3.0
+
+        assertEquals(expected, MedianTwoSortedArrays.findMedianSortedArrays(nums1, nums2))
     }
 }
