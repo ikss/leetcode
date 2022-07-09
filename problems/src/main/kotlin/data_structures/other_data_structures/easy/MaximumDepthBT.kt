405f98c4c9176991d6e36945b0cba94b7350a734
skuznetsov <sergey.kuznetsov@infobip.com>
2022-07-09 19:36:35 +0300

    Fix description of Maximum Depth Binary Tree task

--- problems/src/main/kotlin/data_structures/other_data_structures/easy/MaximumDepthBT.kt
+++ problems/src/main/kotlin/data_structures/other_data_structures/easy/MaximumDepthBT.kt
@@ -6,7 +6,8 @@ import kotlin.math.max
 /**
  * Given the root of a binary tree, return its maximum depth.
  *
- * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node
+ * down to the farthest leaf node.
  *
  * [URL](https://leetcode.com/problems/validate-binary-search-tree/)
  */
