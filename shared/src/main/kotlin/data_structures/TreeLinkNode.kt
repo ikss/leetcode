c3aea47ec4a0f90ef325f46992afa8e69d6eaeb2
skuznetsov <sergey.kuznetsov@infobip.com>
2022-07-10 10:46:21 +0300

    Task: Populating Next Right Pointers in Each Node

---
+++ shared/src/main/kotlin/data_structures/TreeLinkNode.kt
@@ -0,0 +1,8 @@
+package data_structures
+
+data class TreeLinkNode(
+    val `val`: Int,
+    var left: TreeLinkNode? = null,
+    var right: TreeLinkNode? = null,
+    var next: TreeLinkNode? = null
+)
