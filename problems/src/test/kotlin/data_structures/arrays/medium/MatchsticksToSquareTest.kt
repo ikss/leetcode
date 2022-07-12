d4f696309f5417248931803b6c87f07b24b6352b
skuznetsov <sergey.kuznetsov@infobip.com>
2022-07-12 10:41:23 +0300

    Task: Matchsticks to Square

---
+++ problems/src/test/kotlin/data_structures/arrays/medium/MatchsticksToSquareTest.kt
@@ -0,0 +1,31 @@
+package data_structures.arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MatchsticksToSquareTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, 1, 2, 2, 2)
+        val expected = true
+
+        assertEquals(expected, MatchsticksToSquare.makesquare(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(3, 3, 3, 3, 4)
+        val expected = false
+
+        assertEquals(expected, MatchsticksToSquare.makesquare(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 70, 70, 101, 102, 103)
+        val expected = false
+
+        assertEquals(expected, MatchsticksToSquare.makesquare(nums))
+    }
+
+}
