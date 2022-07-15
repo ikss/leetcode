ab843151581aa078757ed0dfd4473198ca3cfadb
skuznetsov <sergey.kuznetsov@infobip.com>
2022-07-15 10:42:06 +0300

    Task: Letter Case Permutations

---
+++ problems/src/main/kotlin/strings/medium/LetterCasePermutation.kt
@@ -0,0 +1,39 @@
+package strings.medium
+
+
+/**
+ * Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
+ *
+ * Return a list of all possible strings we could create. Return the output in any order.
+ *
+ * [URL](https://leetcode.com/problems/letter-case-permutation/)
+ */
+object LetterCasePermutation {
+
+    fun letterCasePermutation(s: String): List<String> {
+        val result = mutableListOf<String>()
+        permute(result, mutableListOf(), s, 0)
+        return result
+    }
+
+    private fun permute(result: MutableList<String>, temp: MutableList<Char>, s: String, index: Int) {
+        if (s.length == temp.size) {
+            result.add(String(temp.toCharArray()))
+            return
+        }
+
+        val c = s[index]
+        if (c.isLetter()) {
+            temp.add(c.toLowerCase())
+            permute(result, temp, s, index + 1)
+            temp.removeAt(index)
+
+            temp.add(c.toUpperCase())
+            permute(result, temp, s, index + 1)
+        } else {
+            temp.add(c)
+            permute(result, temp, s, index + 1)
+        }
+        temp.removeAt(index)
+    }
+}
---
+++ problems/src/test/kotlin/strings/medium/LetterCasePermutationTest.kt
@@ -0,0 +1,22 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LetterCasePermutationTest {
+    @Test
+    fun test1() {
+        val s = "a1b2"
+        val expected = listOf("a1b2", "a1B2", "A1b2", "A1B2")
+
+        assertEquals(expected, LetterCasePermutation.letterCasePermutation(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "3z4"
+        val expected = listOf("3z4", "3Z4")
+
+        assertEquals(expected, LetterCasePermutation.letterCasePermutation(s))
+    }
+}
