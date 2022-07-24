f46e43b54f5f67680857390a87e4ba5e8bffd13a
skuznetsov <sergey.kuznetsov@infobip.com>
2022-07-24 09:30:23 +0300

    Task: Search a 2D Matrix II

---
+++ problems/src/test/kotlin/data_structures/arrays/medium/Search2DMatrixIITest.kt
@@ -0,0 +1,48 @@
+package data_structures.arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Search2DMatrixIITest {
+    @Test
+    fun test1() {
+        val matrix = arrayOf(
+            intArrayOf(1, 4, 7, 11, 15),
+            intArrayOf(2, 5, 8, 12, 19),
+            intArrayOf(3, 6, 9, 16, 22),
+            intArrayOf(10, 13, 14, 17, 24),
+            intArrayOf(18, 21, 23, 26, 30)
+        )
+        val target = 5
+        val expected = true
+
+        assertEquals(expected, Search2DMatrixII.searchMatrix(matrix, target))
+    }
+
+    @Test
+    fun test2() {
+        val matrix = arrayOf(
+            intArrayOf(1, 4, 7, 11, 15),
+            intArrayOf(2, 5, 8, 12, 19),
+            intArrayOf(3, 6, 9, 16, 22),
+            intArrayOf(10, 13, 14, 17, 24),
+            intArrayOf(18, 21, 23, 26, 30)
+        )
+        val target = 20
+        val expected = false
+
+        assertEquals(expected, Search2DMatrixII.searchMatrix(matrix, target))
+    }
+
+    @Test
+    fun test3() {
+        val matrix = arrayOf(
+            intArrayOf(-3)
+        )
+        val target = -3
+        val expected = true
+
+        assertEquals(expected, Search2DMatrixII.searchMatrix(matrix, target))
+    }
+
+}
