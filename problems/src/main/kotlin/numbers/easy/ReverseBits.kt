1dcffbc0124e6715114364bdd591cbc6313bb16f
skuznetsov <sergey.kuznetsov@infobip.com>
2022-07-30 10:34:25 +0300

    Task: Reverse Bits

--- problems/src/main/kotlin/numbers/easy/ReverseBits.kt
+++ problems/src/main/kotlin/numbers/easy/ReverseBits.kt
@@ -13,10 +13,22 @@ package numbers.easy
  * [URL](https://leetcode.com/problems/reverse-bits/)
  */
 object ReverseBits {
-    fun reverseBits(n: Int): Int =
+    fun reverseBitsNaive(n: Int): Int =
         Integer.toBinaryString(n)
             .padStart(32, '0')
             .reversed()
             .toLong(2)
             .toInt()
+
+    fun reverseBits(n: Int): Int {
+        var n = n
+        var result = 0
+        for (i in 0..31) {
+            result += n and 1
+            n = n ushr 1 // CATCH: must do unsigned shift
+            if (i < 31) // CATCH: for last digit, don't shift!
+                result = result shl 1
+        }
+        return result
+    }
 }
--- problems/src/test/kotlin/numbers/easy/ReverseBitsTest.kt
+++ problems/src/test/kotlin/numbers/easy/ReverseBitsTest.kt
@@ -6,6 +6,22 @@ import org.junit.jupiter.api.Test
 class ReverseBitsTest {
 
     @Test
+    fun test1naive() {
+        val number = 43261596
+        val expected = 964176192
+
+        assertEquals(expected, ReverseBits.reverseBitsNaive(number))
+    }
+
+    @Test
+    fun test2naive() {
+        val number = -3
+        val expected = -1073741825
+
+        assertEquals(expected, ReverseBits.reverseBitsNaive(number))
+    }
+
+    @Test
     fun test1() {
         val number = 43261596
         val expected = 964176192
