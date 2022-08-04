f41c85616880c892c22fded433654f9301612672
skuznetsov <sergey.kuznetsov@infobip.com>
2022-08-04 13:10:07 +0300

    Task: Mirror Reflection

---
+++ problems/src/test/kotlin/math/medium/MirrorReflectionTest.kt
@@ -0,0 +1,34 @@
+package math.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MirrorReflectionTest {
+
+    @Test
+    fun test1() {
+        val p = 2
+        val q = 1
+        val expected = 2
+
+        assertEquals(expected, MirrorReflection.mirrorReflection(p, q))
+    }
+
+    @Test
+    fun test2() {
+        val p = 3
+        val q = 1
+        val expected = 1
+
+        assertEquals(expected, MirrorReflection.mirrorReflection(p, q))
+    }
+
+    @Test
+    fun test3() {
+        val p = 10
+        val q = 0
+        val expected = 0
+
+        assertEquals(expected, MirrorReflection.mirrorReflection(p, q))
+    }
+}
