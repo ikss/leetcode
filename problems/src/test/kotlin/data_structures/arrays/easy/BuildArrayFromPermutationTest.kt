e59f650a9ae707487dc4c087f082d860356242d4
skuznetsov <sergey.kuznetsov@infobip.com>
2022-08-15 21:14:00 +0300

    Task: Build Array from Permutation

---
+++ problems/src/test/kotlin/data_structures/arrays/easy/BuildArrayFromPermutationTest.kt
@@ -0,0 +1,22 @@
+package data_structures.arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class BuildArrayFromPermutationTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(0, 2, 1, 5, 3)
+        val expected = intArrayOf(0, 1, 2, 4, 5, 3)
+
+        assertTrue(expected.contentEquals(BuildArrayFromPermutation.buildArray(nums)))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(5, 0, 1, 2, 3, 4)
+        val expected = intArrayOf(4, 5, 0, 1, 2, 3)
+
+        assertTrue(expected.contentEquals(BuildArrayFromPermutation.buildArray(nums)))
+    }
+}
