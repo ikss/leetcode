0ec4e431585fd952f067946ae8e2d84f2e58b520
skuznetsov <sergey.kuznetsov@infobip.com>
2022-08-21 21:01:30 +0300

    Task: Stamping The Sequence

---
+++ problems/src/test/kotlin/strings/hard/StampingTheSequenceTest.kt
@@ -0,0 +1,25 @@
+package strings.hard
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class StampingTheSequenceTest {
+
+    @Test
+    fun test1() {
+        val stamp = "abc"
+        val target = "ababc"
+        val expected = intArrayOf(1, 0, 2)
+
+        assertTrue(expected.contentEquals(StampingTheSequence.movesToStamp(stamp, target)))
+    }
+
+    @Test
+    fun test2() {
+        val stamp = "abca"
+        val target = "aabcaca"
+        val expected = intArrayOf(2, 3, 0, 1)
+
+        assertTrue(expected.contentEquals(StampingTheSequence.movesToStamp(stamp, target)))
+    }
+}
