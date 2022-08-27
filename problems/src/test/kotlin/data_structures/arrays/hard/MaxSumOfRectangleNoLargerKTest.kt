16bad4fe0cf64b70b112e55fe003e563390baa2a
skuznetsov <sergey.kuznetsov@infobip.com>
2022-08-27 09:35:18 +0300

    Task: Max Sum of Rectangle No Larger Than K

---
+++ problems/src/test/kotlin/data_structures/arrays/hard/MaxSumOfRectangleNoLargerKTest.kt
@@ -0,0 +1,26 @@
+package data_structures.arrays.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaxSumOfRectangleNoLargerKTest {
+
+    @Test
+    fun test1() {
+        val matrix = arrayOf(intArrayOf(1, 0, 1), intArrayOf(0, -2, 3))
+        val k = 2
+        val expected = 2
+
+        assertEquals(expected, MaxSumOfRectangleNoLargerK.maxSumSubmatrix(matrix, k))
+    }
+
+    @Test
+    fun test2() {
+        val matrix = arrayOf(intArrayOf(2, 2, -1))
+        val k = 3
+        val expected = 3
+
+        assertEquals(expected, MaxSumOfRectangleNoLargerK.maxSumSubmatrix(matrix, k))
+    }
+
+}
