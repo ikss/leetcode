813ab4c110afea8270743d570acf5aade0fc1311
skuznetsov <sergey.kuznetsov@infobip.com>
2022-08-29 09:57:27 +0300

    Task: Number of Islands

---
+++ problems/src/test/kotlin/data_structures/arrays/medium/NumberOfIslandsTest.kt
@@ -0,0 +1,32 @@
+package data_structures.arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NumberOfIslandsTest {
+    @Test
+    fun test1() {
+        val grid = arrayOf(
+            charArrayOf('1', '1', '1', '1', '0'),
+            charArrayOf('1', '1', '0', '1', '0'),
+            charArrayOf('1', '1', '0', '0', '0'),
+            charArrayOf('0', '0', '0', '0', '0'),
+        )
+        val expected = 1
+
+        assertEquals(expected, NumberOfIslands.numIslands(grid))
+    }
+
+    @Test
+    fun test2() {
+        val grid = arrayOf(
+            charArrayOf('1', '1', '0', '0', '0'),
+            charArrayOf('1', '1', '0', '0', '0'),
+            charArrayOf('0', '0', '1', '0', '0'),
+            charArrayOf('0', '0', '0', '1', '1'),
+        )
+        val expected = 3
+
+        assertEquals(expected, NumberOfIslands.numIslands(grid))
+    }
+}
