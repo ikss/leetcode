1fb69e9a92989a0c445d28549946afd3153a2cb1
skuznetsov <sergey.kuznetsov@infobip.com>
2022-09-01 12:33:47 +0300

    Task: Minimum Path Sum

---
+++ problems/src/test/kotlin/data_structures/arrays/medium/MinimumPathSumTest.kt
@@ -0,0 +1,30 @@
+package data_structures.arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumPathSumTest {
+
+    @Test
+    fun test1() {
+        val grid = arrayOf(
+            intArrayOf(1, 3, 1),
+            intArrayOf(1, 5, 1),
+            intArrayOf(4, 2, 1),
+        )
+        val expected = 7
+
+        assertEquals(expected, MinimumPathSum.minPathSum(grid))
+    }
+
+    @Test
+    fun test2() {
+        val grid = arrayOf(
+            intArrayOf(1, 2, 3),
+            intArrayOf(4, 5, 6),
+        )
+        val expected = 12
+
+        assertEquals(expected, MinimumPathSum.minPathSum(grid))
+    }
+}
