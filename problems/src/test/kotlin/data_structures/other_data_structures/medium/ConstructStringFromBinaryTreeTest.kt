059a3658bd3d5eb9c621e23d36c3efeaa12c431a
skuznetsov <sergey.kuznetsov@infobip.com>
2022-09-07 11:53:52 +0300

    Task: Construct String from Binary Tree

---
+++ problems/src/test/kotlin/data_structures/other_data_structures/medium/ConstructStringFromBinaryTreeTest.kt
@@ -0,0 +1,32 @@
+package data_structures.other_data_structures.medium
+
+import data_structures.TreeBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ConstructStringFromBinaryTreeTest {
+
+    @Test
+    fun test1() {
+        val root = TreeBuilder.build(1, 2, 3, 4)
+        val expected = "1(2(4))(3)"
+
+        assertEquals(expected, ConstructStringFromBinaryTree.tree2str(root))
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeBuilder.build(1, 2, 3, null, 4)
+        val expected = "1(2()(4))(3)"
+
+        assertEquals(expected, ConstructStringFromBinaryTree.tree2str(root))
+    }
+
+    @Test
+    fun test3() {
+        val root = null
+        val expected = ""
+
+        assertEquals(expected, ConstructStringFromBinaryTree.tree2str(root))
+    }
+}
