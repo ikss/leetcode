3567840d12befbe416b2c208e1e1f8f2f58a45ea
ikss <iksss.88@gmail.com>
2022-09-15 14:28:42 +0200

    Task: Find Original Array From Doubled Array

---
+++ problems/src/test/kotlin/data_structures/arrays/medium/FindOriginalArrayFromDoubledArrayTest.kt
@@ -0,0 +1,30 @@
+package data_structures.arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class FindOriginalArrayFromDoubledArrayTest {
+    @Test
+    fun test1() {
+        val changed = intArrayOf(1, 3, 4, 2, 6, 8)
+        val expected = intArrayOf(1, 3, 4)
+
+        assertTrue(expected.contentEquals(FindOriginalArrayFromDoubledArray.findOriginalArray(changed)))
+    }
+
+    @Test
+    fun test2() {
+        val changed = intArrayOf(6, 3, 0, 1)
+        val expected = intArrayOf()
+
+        assertTrue(expected.contentEquals(FindOriginalArrayFromDoubledArray.findOriginalArray(changed)))
+    }
+
+    @Test
+    fun test3() {
+        val changed = intArrayOf(1)
+        val expected = intArrayOf()
+
+        assertTrue(expected.contentEquals(FindOriginalArrayFromDoubledArray.findOriginalArray(changed)))
+    }
+}
