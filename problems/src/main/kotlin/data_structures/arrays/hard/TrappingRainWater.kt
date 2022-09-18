e8b9ac40e3db5fc394d6c2b6a4a8aef46fac31f0
skuznetsov <sergey.kuznetsov@infobip.com>
2022-09-18 10:52:13 +0200

    Task: Trapping Rain Water

--- problems/src/main/kotlin/data_structures/arrays/hard/TrappingRainWater.kt
+++ problems/src/main/kotlin/data_structures/arrays/hard/TrappingRainWater.kt
@@ -8,30 +8,29 @@ package data_structures.arrays.hard
  */
 object TrappingRainWater {
     fun trap(height: IntArray): Int {
+        var result = 0
         var left = 0
-        var leftMax = 0
         var right = height.size - 1
+        var leftMax = 0
         var rightMax = 0
-        var ans = 0
+
         while (left < right) {
-            val hLeft = height[left]
-            val hRight = height[right]
-            if (hLeft < hRight) {
-                if (hLeft >= leftMax) {
-                    leftMax = hLeft
+            if (height[left] < height[right]) {
+                if (height[left] < leftMax) {
+                    result += leftMax - height[left]
                 } else {
-                    ans += leftMax - hLeft
+                    leftMax = height[left]
                 }
-                ++left
+                left++
             } else {
-                if (hRight >= rightMax) {
-                    rightMax = hRight
+                if (height[right] < rightMax) {
+                    result += rightMax - height[right]
                 } else {
-                    ans += rightMax - hRight
+                    rightMax = height[right]
                 }
-                --right
+                right--
             }
         }
-        return ans
+        return result
     }
 }
