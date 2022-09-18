25eda7a1f7796d477f5f591a1eefd06ba2da2744
skuznetsov <sergey.kuznetsov@infobip.com>
2022-09-18 10:13:47 +0200

    Fix palindrome pairs test

--- problems/src/test/kotlin/data_structures/arrays/hard/PalindromePairsTest.kt
+++ problems/src/test/kotlin/data_structures/arrays/hard/PalindromePairsTest.kt
@@ -8,7 +8,7 @@ class PalindromePairsTest {
     @Test
     fun test1() {
         val words = arrayOf("abcd", "dcba", "lls", "s", "sssll")
-        val expected = listOf(listOf(0, 1), listOf(1, 0), listOf(3, 2), listOf(2, 4))
+        val expected = listOf(listOf(1, 0), listOf(0, 1), listOf(3, 2), listOf(2, 4))
 
         assertEquals(expected, PalindromePairs.palindromePairs(words))
     }
@@ -16,7 +16,7 @@ class PalindromePairsTest {
     @Test
     fun test2() {
         val words = arrayOf("bat", "tab", "cat")
-        val expected = listOf(listOf(0, 1), listOf(1, 0))
+        val expected = listOf(listOf(1, 0), listOf(0, 1))
 
         assertEquals(expected, PalindromePairs.palindromePairs(words))
     }
@@ -24,7 +24,7 @@ class PalindromePairsTest {
     @Test
     fun test3() {
         val words = arrayOf("a", "")
-        val expected = listOf(listOf(0, 1))
+        val expected = listOf(listOf(1, 0), listOf(0, 1))
 
         assertEquals(expected, PalindromePairs.palindromePairs(words))
     }
