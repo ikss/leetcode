205089e22ea0ac0ba95abbfb300a8c0598b78540
ikss <iksss.88@gmail.com>
2022-09-23 11:05:01 +0200

    Fix description

--- problems/src/main/kotlin/numbers/easy/FizzBuzz.kt
+++ problems/src/main/kotlin/numbers/easy/FizzBuzz.kt
@@ -2,10 +2,10 @@ package numbers.easy
 
 /**
  * Given an integer n, return a string array answer (1-indexed) where:
- * * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
- * * answer[i] == "Fizz" if i is divisible by 3.
- * * answer[i] == "Buzz" if i is divisible by 5.
- * * answer[i] == i (as a string) if none of the above conditions are true.
+ * * `answer[i] == "FizzBuzz"` if i is divisible by 3 and 5.
+ * * `answer[i] == "Fizz"` if i is divisible by 3.
+ * * `answer[i] == "Buzz"` if i is divisible by 5.
+ * * `answer[i] == i (as a string)` if none of the above conditions are true.
  *
  * [URL](https://leetcode.com/problems/fizz-buzz/)
  */
