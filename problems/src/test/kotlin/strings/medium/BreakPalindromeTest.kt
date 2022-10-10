ba6ef667cbcd5566f6f52bc570db4772bbb53a1b
ikss <iksss.88@gmail.com>
2022-10-10 14:23:49 +0200

    Task: Break a Palindrome

---
+++ problems/src/test/kotlin/strings/medium/BreakPalindromeTest.kt
@@ -0,0 +1,31 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class BreakPalindromeTest {
+
+    @Test
+    fun test1() {
+        val palindrome = "abccba"
+        val expected = "aaccba"
+
+        assertEquals(expected, BreakPalindrome.breakPalindrome(palindrome))
+    }
+
+    @Test
+    fun test2() {
+        val palindrome = "abccba"
+        val expected = "aaccba"
+
+        assertEquals(expected, BreakPalindrome.breakPalindrome(palindrome))
+    }
+
+    @Test
+    fun test3() {
+        val palindrome = "aba"
+        val expected = "abb"
+
+        assertEquals(expected, BreakPalindrome.breakPalindrome(palindrome))
+    }
+}
