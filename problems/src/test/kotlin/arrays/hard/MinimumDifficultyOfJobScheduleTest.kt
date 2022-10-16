9a0586821d82d83b9311a9c59297abee3e4be30f
ikss <iksss.88@gmail.com>
2022-10-16 16:54:38 +0200

    Task: Minimum Difficulty of a Job Schedule

---
+++ problems/src/test/kotlin/arrays/hard/MinimumDifficultyOfJobScheduleTest.kt
@@ -0,0 +1,34 @@
+package arrays.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumDifficultyOfJobScheduleTest {
+
+    @Test
+    fun test1() {
+        val jobDifficulty = intArrayOf(6, 5, 4, 3, 2, 1)
+        val d = 2
+        val expected = 7
+
+        assertEquals(expected, MinimumDifficultyOfJobSchedule.minDifficulty(jobDifficulty, d))
+    }
+
+    @Test
+    fun test2() {
+        val jobDifficulty = intArrayOf(9, 9, 9)
+        val d = 4
+        val expected = -1
+
+        assertEquals(expected, MinimumDifficultyOfJobSchedule.minDifficulty(jobDifficulty, d))
+    }
+
+    @Test
+    fun test3() {
+        val jobDifficulty = intArrayOf(1, 1, 1)
+        val d = 3
+        val expected = 3
+
+        assertEquals(expected, MinimumDifficultyOfJobSchedule.minDifficulty(jobDifficulty, d))
+    }
+}
