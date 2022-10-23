a4eb109f98c525caffaf7c744c1547f4ef0341b5
ikss <iksss.88@gmail.com>
2022-10-23 21:44:39 +0200

    Task: Minimum Window Substring

---
+++ problems/src/test/kotlin/strings/hard/MinimumWindowSubstringTest.kt
@@ -0,0 +1,34 @@
+package strings.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumWindowSubstringTest {
+
+    @Test
+    fun test1() {
+        val s = "ADOBECODEBANC"
+        val t = "ABC"
+        val expected = "BANC"
+
+        assertEquals(expected, MinimumWindowSubstring.minWindow(s, t))
+    }
+
+    @Test
+    fun test2() {
+        val s = "a"
+        val t = "a"
+        val expected = "a"
+
+        assertEquals(expected, MinimumWindowSubstring.minWindow(s, t))
+    }
+
+    @Test
+    fun test3() {
+        val s = "a"
+        val t = "aa"
+        val expected = ""
+
+        assertEquals(expected, MinimumWindowSubstring.minWindow(s, t))
+    }
+}
