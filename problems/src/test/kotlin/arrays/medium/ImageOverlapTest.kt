392be4365a41c4a5ba2fe658dafbde6b9c0edcf2
ikss <iksss.88@gmail.com>
2022-10-27 10:14:47 +0200

    Task: Image Overlap

---
+++ problems/src/test/kotlin/arrays/medium/ImageOverlapTest.kt
@@ -0,0 +1,41 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class ImageOverlapTest {
+    @Test
+    fun test1() {
+        val img1 = arrayOf(
+            intArrayOf(1, 1, 0),
+            intArrayOf(0, 1, 0),
+            intArrayOf(0, 1, 0),
+        )
+        val img2 = arrayOf(
+            intArrayOf(0, 0, 0),
+            intArrayOf(0, 1, 1),
+            intArrayOf(0, 0, 1),
+        )
+        val expected = 3
+
+        assertEquals(expected, ImageOverlap.largestOverlap(img1, img2))
+    }
+
+    @Test
+    fun test2() {
+        val img1 = arrayOf(intArrayOf(1))
+        val img2 = arrayOf(intArrayOf(1))
+        val expected = 1
+
+        assertEquals(expected, ImageOverlap.largestOverlap(img1, img2))
+    }
+
+    @Test
+    fun test3() {
+        val img1 = arrayOf(intArrayOf(0))
+        val img2 = arrayOf(intArrayOf(0))
+        val expected = 0
+
+        assertEquals(expected, ImageOverlap.largestOverlap(img1, img2))
+    }
+}
