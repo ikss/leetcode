7410d61fb0b9cc798d6809dfdc60f9030e084dd0
ikss <iksss.88@gmail.com>
2022-11-05 20:10:49 +0100

    Task: Reverse Vowels of a String

---
+++ problems/src/test/kotlin/strings/easy/ReverseVowelsOfStringTest.kt
@@ -0,0 +1,34 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class ReverseVowelsOfStringTest {
+
+    @Test
+    fun test1() {
+        val s = "hello"
+        val expected = "holle"
+
+        assertEquals(expected, ReverseVowelsOfString.reverseVowels(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "FLaG".toCharArray()
+        val expected = "GaLF".toCharArray()
+        ReverseString.reverseString(s)
+
+        assertTrue(expected.contentEquals(s))
+    }
+
+    @Test
+    fun test3() {
+        val s = "A".toCharArray()
+        val expected = "A".toCharArray()
+        ReverseString.reverseString(s)
+
+        assertTrue(expected.contentEquals(s))
+    }
+}
