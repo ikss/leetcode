0121e3770dd9e9cb76420985b59041742c203e9a
ikss <iksss.88@gmail.com>
2022-11-05 12:39:33 +0100

    Task: Word Search II

---
+++ problems/src/main/kotlin/strings/hard/WordSearchII.kt
@@ -0,0 +1,70 @@
+package strings.hard
+
+
+/**
+ * Given an m x n board of characters and a list of strings words, return all words on the board.
+ *
+ * Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or
+ * vertically neighboring. The same letter cell may not be used more than once in a word.
+ *
+ * [URL](https://leetcode.com/problems/word-search-ii/)
+ */
+object WordSearchII {
+    private val DIRECTIONS_X = intArrayOf(0, 0, 1, -1)
+    private val DIRECTIONS_Y = intArrayOf(1, -1, 0, 0)
+
+    fun findWords(board: Array<CharArray>, words: Array<String>): List<String> {
+        val root = Trie()
+        for (word in words) {
+            root.add(word)
+        }
+
+        val res = mutableListOf<String>()
+
+        for (i in board.indices) {
+            for (j in board[i].indices) {
+                findWords(board, i, j, root.getChild(board[i][j]) ?: continue, res)
+            }
+        }
+        return res
+    }
+
+    private fun findWords(board: Array<CharArray>, bx: Int, by: Int, node: Trie, res: MutableList<String>) {
+        node.word?.let {
+            res.add(it)
+            node.word = null
+        }
+        board[bx][by].let {
+            board[bx][by] = 'z' + 1
+            for (i in 0 until 4) {
+                val x = bx + DIRECTIONS_X[i]
+                val y = by + DIRECTIONS_Y[i]
+                if (x >= 0 && x < board.size && y >= 0 && y < board[x].size) {
+                    findWords(board, x, y, node.getChild(board[x][y]) ?: continue, res)
+                }
+            }
+            board[bx][by] = it
+        }
+    }
+
+    private class Trie {
+        private val children = Array<Trie?>(27) { null }
+        var word: String? = null
+
+        fun add(word: String) {
+            var node = this
+            for (c in word) {
+                node = node.getAndSetChildDefault(c)
+            }
+            node.word = word
+        }
+
+        fun getChild(c: Char) = this.hash(c).let { this.children[it] }
+
+        private fun hash(c: Char): Int = c - 'a'
+
+        private fun getAndSetChildDefault(c: Char): Trie = this.hash(c).let { key ->
+            children[key] ?: Trie().also { children[key] = it }
+        }
+    }
+}
