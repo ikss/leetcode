367f1923d1a43be606cf7b1886a253bdeb543055
ikss <iksss.88@gmail.com>
2022-11-06 10:00:46 +0100

    Task: Orderly Queue

---
+++ problems/src/main/kotlin/strings/hard/OrderlyQueue.kt
@@ -0,0 +1,28 @@
+package strings.hard
+
+/**
+ * You are given a string s and an integer k. You can choose one of the first k letters of s and append it
+ * at the end of the string...
+ *
+ * Return the lexicographically smallest string you could have after applying the mentioned step any number of moves.
+ *
+ * [URL](https://leetcode.com/problems/orderly-queue/)
+ */
+object OrderlyQueue {
+    fun orderlyQueue(s: String, k: Int): String {
+        return if (k == 1) {
+            var ans: String = s
+            for (i in s.indices) {
+                val temp = s.substring(i) + s.substring(0, i)
+                if (temp < ans) {
+                    ans = temp
+                }
+            }
+            ans
+        } else {
+            val chars = s.toCharArray()
+            chars.sort()
+            String(chars)
+        }
+    }
+}
---
+++ problems/src/test/kotlin/strings/hard/OrderlyQueueTest.kt
@@ -0,0 +1,25 @@
+package strings.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class OrderlyQueueTest {
+
+    @Test
+    fun test1() {
+        val s = "cba"
+        val k = 1
+        val expected = "acb"
+
+        assertEquals(expected, OrderlyQueue.orderlyQueue(s, k))
+    }
+
+    @Test
+    fun test2() {
+        val s = "baaca"
+        val k = 3
+        val expected = "aaabc"
+
+        assertEquals(expected, OrderlyQueue.orderlyQueue(s, k))
+    }
+}
