b76d0bd8458bd1477fcf80f3dc14c855737ea6d4
ikss <iksss.88@gmail.com>
2022-11-08 09:38:49 +0100

    Task: Make The String Great

---
+++ problems/src/test/kotlin/strings/easy/MakeTheStringGreatTest.kt
@@ -0,0 +1,70 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class MakeTheStringGreatTest {
+    @Test
+    fun test1cycle() {
+        val s = "leEeetcode"
+        val expected = "leetcode"
+
+        assertEquals(expected, MakeTheStringGreat.makeGoodCycle(s))
+    }
+
+    @Test
+    fun test2cycle() {
+        val s = "s"
+        val expected = "s"
+
+        assertEquals(expected, MakeTheStringGreat.makeGoodCycle(s))
+    }
+
+    @Test
+    fun test3cycle() {
+        val s = ""
+        val expected = ""
+
+        assertEquals(expected, MakeTheStringGreat.makeGoodCycle(s))
+    }
+
+    @Test
+    fun test4cycle() {
+        val s = "abBAcC"
+        val expected = ""
+
+        assertEquals(expected, MakeTheStringGreat.makeGoodCycle(s))
+    }
+
+    @Test
+    fun test1stack() {
+        val s = "leEeetcode"
+        val expected = "leetcode"
+
+        assertEquals(expected, MakeTheStringGreat.makeGoodStack(s))
+    }
+
+    @Test
+    fun test2stack() {
+        val s = "s"
+        val expected = "s"
+
+        assertEquals(expected, MakeTheStringGreat.makeGoodStack(s))
+    }
+
+    @Test
+    fun test3stack() {
+        val s = ""
+        val expected = ""
+
+        assertEquals(expected, MakeTheStringGreat.makeGoodStack(s))
+    }
+
+    @Test
+    fun test4stack() {
+        val s = "abBAcC"
+        val expected = ""
+
+        assertEquals(expected, MakeTheStringGreat.makeGoodStack(s))
+    }
+}
