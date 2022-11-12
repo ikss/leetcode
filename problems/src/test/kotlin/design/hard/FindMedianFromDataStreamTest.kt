2334b4158e2927b4168db89187e40c469675da63
ikss <iksss.88@gmail.com>
2022-11-12 10:06:30 +0100

    Task: Find Median from Data Stream

---
+++ problems/src/test/kotlin/design/hard/FindMedianFromDataStreamTest.kt
@@ -0,0 +1,16 @@
+package design.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class FindMedianFromDataStreamTest {
+    @Test
+    fun test1() {
+        val medianFinder = FindMedianFromDataStream.MedianFinder()
+        medianFinder.addNum(1)                              // arr = [1]
+        medianFinder.addNum(2)                              // arr = [1, 2]
+        assertEquals(1.5, medianFinder.findMedian())    // return 1.5 (i.e., (1 + 2) / 2)
+        medianFinder.addNum(3)                              // arr[1, 2, 3]
+        assertEquals(2.0, medianFinder.findMedian())    // return 2.0
+    }
+}
