529c7126a277e33f6dfb67b3033d8813966c1f82
ikss <iksss.88@gmail.com>
2022-11-12 10:16:26 +0100

    Task: 27. Remove Element

---
+++ problems/src/test/kotlin/arrays/easy/RemoveElementTest.kt
@@ -0,0 +1,28 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class RemoveElementTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(3, 2, 2, 3)
+        val `val` = 3
+        val expected = 2
+        val result = RemoveElement.removeElement(nums, `val`)
+
+        assertEquals(expected, result)
+        assertEquals(listOf(2, 2), nums.take(result))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(0, 1, 2, 2, 3, 0, 4, 2)
+        val `val` = 2
+        val expected = 5
+        val result = RemoveElement.removeElement(nums, `val`)
+
+        assertEquals(expected, result)
+        assertEquals(listOf(0, 1, 3, 0, 4), nums.take(result))
+    }
+}
