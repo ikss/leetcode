ca9ead5fd85c6f1daabf146d1eabedc26ca6bb6a
ikss <iksss.88@gmail.com>
2022-11-13 10:24:21 +0100

    Task: 151. Reverse Words in a String

---
+++ problems/src/main/kotlin/strings/medium/ReverseWordsInString.kt
@@ -0,0 +1,40 @@
+package strings.medium
+
+/**
+ * Given an input string s, reverse the order of the words.
+ *
+ * A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+ *
+ * Return a string of the words in reverse order concatenated by a single space.
+ *
+ * Note that s may contain leading or trailing spaces or multiple spaces between two words.
+ * The returned string should only have a single space separating the words. Do not include any extra spaces.
+ *
+ * [URL](https://leetcode.com/problems/reverse-words-in-a-string/)
+ */
+object ReverseWordsInString {
+    fun reverseWordsOneLiner(s: String): String =
+        s.split(" ").map { it.trim() }.filter { it.isNotEmpty() }.reversed().joinToString(" ")
+
+    fun reverseWordsSimple(s: String): String {
+        val result = mutableListOf<String>()
+
+        var start = -1
+        for (i in s.indices) {
+            val char = s[i]
+            if (char == ' ') {
+                if (start != -1) {
+                    result.add(s.substring(start, i))
+                    start = -1
+                }
+            } else if (start == -1) {
+                start = i
+            }
+        }
+        if (start != -1) {
+            result.add(s.substring(start))
+        }
+
+        return result.reversed().joinToString(" ")
+    }
+}
