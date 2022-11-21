681042f164579fa048110650c2166f52494bc187
ikss <iksss.88@gmail.com>
2022-11-21 11:10:30 +0100

    Task: 1926. Nearest Exit from Entrance in Maze

---
+++ problems/src/test/kotlin/arrays/medium/NearestExitFromEntranceInMazeTest.kt
@@ -0,0 +1,43 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NearestExitFromEntranceInMazeTest {
+    @Test
+    fun test1() {
+        val maze = arrayOf(
+            charArrayOf('+', '+', '.', '+'),
+            charArrayOf('.', '.', '.', '+'),
+            charArrayOf('+', '+', '+', '.'),
+        )
+        val entrance = intArrayOf(1, 2)
+        val expected = 1
+
+        assertEquals(expected, NearestExitFromEntranceInMaze.nearestExit(maze, entrance))
+    }
+
+    @Test
+    fun test2() {
+        val maze = arrayOf(
+            charArrayOf('+', '+', '+'),
+            charArrayOf('.', '.', '.'),
+            charArrayOf('+', '+', '+'),
+        )
+        val entrance = intArrayOf(1, 0)
+        val expected = 2
+
+        assertEquals(expected, NearestExitFromEntranceInMaze.nearestExit(maze, entrance))
+    }
+
+    @Test
+    fun test3() {
+        val maze = arrayOf(
+            charArrayOf('.', '+'),
+        )
+        val entrance = intArrayOf(0, 0)
+        val expected = -1
+
+        assertEquals(expected, NearestExitFromEntranceInMaze.nearestExit(maze, entrance))
+    }
+}
