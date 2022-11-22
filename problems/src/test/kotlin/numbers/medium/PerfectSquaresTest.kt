deac72753e8f8a38c2aa31bfac0b1c0f7a230dbf
ikss <iksss.88@gmail.com>
2022-11-22 10:22:44 +0100

    Task: 279. Perfect Squares

---
+++ problems/src/test/kotlin/numbers/medium/PerfectSquaresTest.kt
@@ -0,0 +1,24 @@
+package numbers.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PerfectSquaresTest {
+
+    @Test
+    fun test1() {
+        val n = 12
+        val expected = 3
+
+        assertEquals(expected, PerfectSquares.numSquares(n))
+    }
+
+    @Test
+    fun test2() {
+        val n = 13
+        val expected = 2
+
+        assertEquals(expected, PerfectSquares.numSquares(n))
+    }
+
+}
