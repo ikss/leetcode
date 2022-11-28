49041fac431a2afc7a2ef4b2bccc073c3868b451
ikss <iksss.88@gmail.com>
2022-11-28 11:17:57 +0100

    Task: 2225. Find Players With Zero or One Losses

---
+++ problems/src/main/kotlin/arrays/medium/FindPlayersWithZeroOrOneLosses.kt
@@ -0,0 +1,42 @@
+package arrays.medium
+
+/**
+ * ou are given an integer array matches where `matches[i] = [winneri, loseri]` indicates that
+ * the player winneri defeated player loseri in a match.
+ *
+ * Return a list answer of size 2 where:
+ * * answer[0] is a list of all players that have not lost any matches.
+ * * answer[1] is a list of all players that have lost exactly one match.
+ *
+ * The values in the two lists should be returned in increasing order.
+ *
+ * __Note__:
+ * * You should only consider the players that have played at least one match.
+ * * The testcases will be generated such that no two matches will have the same outcome.
+ *
+ * [URL](https://leetcode.com/problems/find-players-with-zero-or-one-losses/)
+ */
+object FindPlayersWithZeroOrOneLosses {
+    fun findWinners(matches: Array<IntArray>): List<List<Int>> {
+        val players = hashMapOf<Int, Int>()
+
+        for ((win, loose) in matches) {
+            if (!players.containsKey(win)) {
+                players[win] = 0
+            }
+            val looser = players[loose] ?: 0
+            players[loose] = looser + 1
+        }
+
+        val winners = mutableListOf<Int>()
+        val losers = mutableListOf<Int>()
+        for ((player, losses) in players) {
+            if (losses == 0) {
+                winners.add(player)
+            } else if (losses == 1) {
+                losers.add(player)
+            }
+        }
+        return listOf(winners.sorted(), losers.sorted())
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/FindPlayersWithZeroOrOneLossesTest.kt
@@ -0,0 +1,38 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindPlayersWithZeroOrOneLossesTest {
+    @Test
+    fun test1() {
+        val matches = arrayOf(
+            intArrayOf(1, 3),
+            intArrayOf(2, 3),
+            intArrayOf(3, 6),
+            intArrayOf(5, 6),
+            intArrayOf(5, 7),
+            intArrayOf(4, 5),
+            intArrayOf(4, 8),
+            intArrayOf(4, 9),
+            intArrayOf(10, 4),
+            intArrayOf(10, 9),
+        )
+        val expected = listOf(listOf(1, 2, 10), listOf(4, 5, 7, 8))
+
+        assertEquals(expected, FindPlayersWithZeroOrOneLosses.findWinners(matches))
+    }
+
+    @Test
+    fun test2() {
+        val matches = arrayOf(
+            intArrayOf(2, 3),
+            intArrayOf(1, 3),
+            intArrayOf(5, 4),
+            intArrayOf(6, 4),
+        )
+        val expected = listOf(listOf(1, 2, 5, 6), emptyList())
+
+        assertEquals(expected, FindPlayersWithZeroOrOneLosses.findWinners(matches))
+    }
+}
