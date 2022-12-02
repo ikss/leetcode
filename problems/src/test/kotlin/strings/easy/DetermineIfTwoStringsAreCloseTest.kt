f809b87359340fe87d01bcf8df1af8ae7f151cb4
ikss <iksss.88@gmail.com>
2022-12-02 10:10:34 +0100

    Task: 1657. Determine if Two Strings Are Close

---
+++ problems/src/test/kotlin/strings/easy/DetermineIfTwoStringsAreCloseTest.kt
@@ -0,0 +1,34 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DetermineIfTwoStringsAreCloseTest {
+
+    @Test
+    fun test1() {
+        val word1 = "abc"
+        val word2 = "bca"
+        val expected = true
+
+        assertEquals(expected, DetermineIfTwoStringsAreClose.closeStrings(word1, word2))
+    }
+
+    @Test
+    fun test2() {
+        val word1 = "a"
+        val word2 = "aa"
+        val expected = false
+
+        assertEquals(expected, DetermineIfTwoStringsAreClose.closeStrings(word1, word2))
+    }
+
+    @Test
+    fun test3() {
+        val word1 = "cabbba"
+        val word2 = "abbccc"
+        val expected = true
+
+        assertEquals(expected, DetermineIfTwoStringsAreClose.closeStrings(word1, word2))
+    }
+}
