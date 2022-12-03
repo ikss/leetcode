ef5805059ee3c51709de21cf108378852fac7e35
ikss <iksss.88@gmail.com>
2022-12-03 08:59:14 +0100

    Task: 451. Sort Characters By Frequency

---
+++ problems/src/test/kotlin/strings/medium/SortCharactersByFrequencyTest.kt
@@ -0,0 +1,31 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SortCharactersByFrequencyTest {
+
+    @Test
+    fun test1() {
+        val s = "tree"
+        val expected = "eetr"
+
+        assertEquals(expected, SortCharactersByFrequency.frequencySort(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "cccaaa"
+        val expected = "cccaaa"
+
+        assertEquals(expected, SortCharactersByFrequency.frequencySort(s))
+    }
+
+    @Test
+    fun test3() {
+        val s = "Aabb"
+        val expected = "bbAa"
+
+        assertEquals(expected, SortCharactersByFrequency.frequencySort(s))
+    }
+}
