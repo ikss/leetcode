500ef2570a5c7b9af5fd61b39c0b150cdcefb30f
ikss <iksss.88@gmail.com>
2022-12-07 09:44:14 +0100

    Task: 938. Range Sum of BST

---
+++ problems/src/main/kotlin/linked_lists/easy/RangeSumOfBST.kt
@@ -0,0 +1,33 @@
+package linked_lists.easy
+
+import data_structures.TreeNode
+import java.util.*
+
+/**
+ * Given the root node of a binary search tree and two integers low and high,
+ * return the sum of values of all nodes with a value in the inclusive range `[low, high]`.
+ *
+ * [URL](https://leetcode.com/problems/range-sum-of-bst/description/)
+ */
+object RangeSumOfBST {
+    fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
+        var res = 0
+        val stack = Stack<TreeNode?>()
+        stack.push(root)
+        while (!stack.isEmpty()) {
+            val node = stack.pop()
+                ?: continue
+            val value = node.`val`
+            if (value in low..high) {
+                res += value
+            }
+            if (low < value) {
+                stack.push(node.left)
+            }
+            if (value < high) {
+                stack.push(node.right)
+            }
+        }
+        return res
+    }
+}
---
+++ problems/src/test/kotlin/linked_lists/easy/RangeSumOfBSTTest.kt
@@ -0,0 +1,28 @@
+package linked_lists.easy
+
+import data_structures.TreeBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class RangeSumOfBSTTest {
+
+    @Test
+    fun test1() {
+        val root = TreeBuilder.build(10, 5, 15, 3, 7, null, 18)
+        val low = 7
+        val high = 15
+        val expected = 32
+
+        assertEquals(expected, RangeSumOfBST.rangeSumBST(root, low, high))
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeBuilder.build(10, 5, 15, 3, 7, 13, 18, 1, null, 6)
+        val low = 6
+        val high = 10
+        val expected = 23
+
+        assertEquals(expected, RangeSumOfBST.rangeSumBST(root, low, high))
+    }
+}
