3bf2fc4fab2c00c34b19356a31b6b2d9ae4d9034
ikss <iksss.88@gmail.com>
2022-12-09 10:44:19 +0100

    Task: 1026. Maximum Difference Between Node and Ancestor

---
+++ problems/src/test/kotlin/trees/medium/MaximumDifferenceBetweenNodeAndAncestorTest.kt
@@ -0,0 +1,24 @@
+package trees.medium
+
+import data_structures.TreeBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumDifferenceBetweenNodeAndAncestorTest {
+
+    @Test
+    fun test1() {
+        val root = TreeBuilder.build(8, 3, 10, 1, 6, null, 14, null, null, 4, 7, 13)
+        val expected = 7
+
+        assertEquals(expected, MaximumDifferenceBetweenNodeAndAncestor.maxAncestorDiff(root))
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeBuilder.build(1, null, 2, null, null, null, 0, null, null, null, null, null, null, 3)
+        val expected = 3
+
+        assertEquals(expected, MaximumDifferenceBetweenNodeAndAncestor.maxAncestorDiff(root))
+    }
+}
