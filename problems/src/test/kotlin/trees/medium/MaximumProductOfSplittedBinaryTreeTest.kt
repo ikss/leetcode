c9c2eab6c79a73271f3af9a12c02ca1e8ab44a2f
ikss <iksss.88@gmail.com>
2022-12-11 20:17:51 +0100

    Task: 1339. Maximum Product of Splitted Binary Tree

---
+++ problems/src/test/kotlin/trees/medium/MaximumProductOfSplittedBinaryTreeTest.kt
@@ -0,0 +1,24 @@
+package trees.medium
+
+import data_structures.TreeBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumProductOfSplittedBinaryTreeTest {
+
+    @Test
+    fun test1() {
+        val root = TreeBuilder.build(1, 2, 3, 4, 5, 6)
+        val expected = 110
+
+        assertEquals(expected, MaximumProductOfSplittedBinaryTree.maxProduct(root))
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeBuilder.build(1, null, 2, null, null, 3, 4, null, null, null, null, null, null, 5, 6)
+        val expected = 90
+
+        assertEquals(expected, MaximumProductOfSplittedBinaryTree.maxProduct(root))
+    }
+}
