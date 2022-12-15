198b75e8e0f33f530509827d9b5bd64ad651a767
ikss <iksss.88@gmail.com>
2022-12-15 09:25:43 +0100

    Task: 1143. Longest Common Subsequence

---
+++ problems/src/test/kotlin/strings/medium/LongestCommonSubsequenceTest.kt
@@ -0,0 +1,33 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LongestCommonSubsequenceTest {
+    @Test
+    fun test1() {
+        val text1 = "abcde"
+        val text2 = "ace"
+        val expected = 3
+
+        assertEquals(expected, LongestCommonSubsequence.longestCommonSubsequence(text1, text2))
+    }
+
+    @Test
+    fun test2() {
+        val text1 = "abc"
+        val text2 = "abc"
+        val expected = 3
+
+        assertEquals(expected, LongestCommonSubsequence.longestCommonSubsequence(text1, text2))
+    }
+
+    @Test
+    fun test3() {
+        val text1 = "abc"
+        val text2 = "def"
+        val expected = 0
+
+        assertEquals(expected, LongestCommonSubsequence.longestCommonSubsequence(text1, text2))
+    }
+}
