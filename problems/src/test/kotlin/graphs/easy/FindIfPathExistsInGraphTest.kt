7b3747e9c4bc818fbc62e44cfd86153e743dcd67
ikss <iksss.88@gmail.com>
2022-12-19 07:39:15 +0100

    Task: 1971. Find if Path Exists in Graph

---
+++ problems/src/test/kotlin/graphs/easy/FindIfPathExistsInGraphTest.kt
@@ -0,0 +1,29 @@
+package graphs.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindIfPathExistsInGraphTest {
+
+    @Test
+    fun test1() {
+        val n = 3
+        val edges = arrayOf(intArrayOf(0, 1), intArrayOf(1, 2), intArrayOf(2, 0))
+        val source = 0
+        val destination = 2
+        val expected = true
+
+        assertEquals(expected, FindIfPathExistsInGraph.validPathUnionFind(n, edges, source, destination))
+    }
+
+    @Test
+    fun test2() {
+        val n = 6
+        val edges = arrayOf(intArrayOf(0, 1), intArrayOf(0, 2), intArrayOf(3, 5), intArrayOf(5, 4), intArrayOf(4, 3))
+        val source = 0
+        val destination = 5
+        val expected = false
+
+        assertEquals(expected, FindIfPathExistsInGraph.validPathUnionFind(n, edges, source, destination))
+    }
+}
---
+++ shared/src/main/kotlin/data_structures/UnionFind.kt
@@ -0,0 +1,34 @@
+package data_structures
+
+class UnionFind(n: Int) {
+    private val root = IntArray(n)
+    private val rank = IntArray(n)
+
+    init {
+        for (i in 0 until n) {
+            this.root[i] = i
+        }
+    }
+
+    fun find(x: Int): Int {
+        if (root[x] != x) {
+            root[x] = find(root[x])
+        }
+        return root[x]
+    }
+
+    fun union(x: Int, y: Int) {
+        var rootX = find(x)
+        var rootY = find(y)
+        if (rootX == rootY) return
+        if (rank[rootX] > rank[rootY]) {
+            val tmp = rootX
+            rootX = rootY
+            rootY = tmp
+        }
+        // Modify the root of the smaller group as the root of the
+        // larger group, also increment the size of the larger group.
+        root[rootX] = rootY
+        rank[rootY] += rank[rootX]
+    }
+}
