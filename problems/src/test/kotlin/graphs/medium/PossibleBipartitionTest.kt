4dbb51124706f551fe5066cfdef39dd5a4fe7acb
ikss <iksss.88@gmail.com>
2022-12-21 09:46:11 +0100

    Task: 886. Possible Bipartition

---
+++ problems/src/test/kotlin/graphs/medium/PossibleBipartitionTest.kt
@@ -0,0 +1,34 @@
+package graphs.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PossibleBipartitionTest {
+
+    @Test
+    fun test1() {
+        val n = 4
+        val dislikes = arrayOf(intArrayOf(1, 2), intArrayOf(1, 3), intArrayOf(2, 4))
+        val expected = true
+
+        assertEquals(expected, PossibleBipartition.possibleBipartition(n, dislikes))
+    }
+
+    @Test
+    fun test2() {
+        val n = 3
+        val dislikes = arrayOf(intArrayOf(1, 2), intArrayOf(1, 3), intArrayOf(2, 3))
+        val expected = false
+
+        assertEquals(expected, PossibleBipartition.possibleBipartition(n, dislikes))
+    }
+
+    @Test
+    fun test3() {
+        val n = 5
+        val dislikes = arrayOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(3, 4), intArrayOf(4, 5), intArrayOf(1, 5))
+        val expected = false
+
+        assertEquals(expected, PossibleBipartition.possibleBipartition(n, dislikes))
+    }
+}
