278e9342eb2d0455ba349e963b1e5db48b613b81
ikss <iksss.88@gmail.com>
2022-12-30 10:45:58 +0100

    Task: 797. All Paths From Source to Target

---
+++ problems/src/main/kotlin/graphs/medium/AllPathsFromSourceToTarget.kt
@@ -0,0 +1,46 @@
+package graphs.medium
+
+
+/**
+ * Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1,
+ * find all possible paths from node 0 to node n - 1 and return them in any order.
+ *
+ * The graph is given as follows: `graph[i]` is a list of all nodes you can visit from node i
+ * (i.e., there is a directed edge from node i to node `graph[i][j]`).
+ *
+ * [URL](https://leetcode.com/problems/all-paths-from-source-to-target/)
+ */
+object AllPathsFromSourceToTarget {
+    fun allPathsSourceTarget(graph: Array<IntArray>): List<List<Int>> {
+        val source = 0
+        val target = graph.size - 1
+
+        val result = mutableListOf<List<Int>>()
+        val visited = mutableSetOf<Int>()
+        traverse(source, target, graph, visited, result)
+
+        return result
+    }
+
+    private fun traverse(
+        current: Int,
+        target: Int,
+        graph: Array<IntArray>,
+        visited: MutableSet<Int>,
+        result: MutableList<List<Int>>,
+    ) {
+        if (!visited.add(current)) {
+            return
+        }
+
+        if (current == target) {
+            result.add(visited.toList())
+            visited.remove(current)
+            return
+        }
+        for (next in graph[current]) {
+            traverse(next, target, graph, visited, result)
+        }
+        visited.remove(current)
+    }
+}
---
+++ problems/src/test/kotlin/graphs/medium/AllPathsFromSourceToTargetTest.kt
@@ -0,0 +1,23 @@
+package graphs.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class AllPathsFromSourceToTargetTest {
+
+    @Test
+    fun test1() {
+        val graph = arrayOf(intArrayOf(1, 2), intArrayOf(3), intArrayOf(3), intArrayOf())
+        val expected = listOf(listOf(0, 1, 3), listOf(0, 2, 3))
+
+        assertEquals(expected, AllPathsFromSourceToTarget.allPathsSourceTarget(graph))
+    }
+
+    @Test
+    fun test2() {
+        val graph = arrayOf(intArrayOf(4, 3, 1), intArrayOf(3, 2, 4), intArrayOf(3), intArrayOf(4), intArrayOf())
+        val expected = listOf(listOf(0, 4), listOf(0, 3, 4), listOf(0, 1, 3, 4), listOf(0, 1, 2, 3, 4), listOf(0, 1, 4))
+
+        assertEquals(expected, AllPathsFromSourceToTarget.allPathsSourceTarget(graph))
+    }
+}
