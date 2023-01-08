d0e2b7c260f199f00f7cda80d380113217d89b4d
ikss <iksss.88@gmail.com>
2023-01-08 19:45:18 +0100

    Task: 149. Max Points on a Line

---
+++ problems/src/test/kotlin/math/hard/MaxPointsOnLineTest.kt
@@ -0,0 +1,34 @@
+package math.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaxPointsOnLineTest {
+
+    @Test
+    fun test1() {
+        val points = arrayOf(
+            intArrayOf(1, 1),
+            intArrayOf(2, 2),
+            intArrayOf(3, 3),
+        )
+        val expected = 3
+
+        assertEquals(expected, MaxPointsOnLine.maxPoints(points))
+    }
+
+    @Test
+    fun test2() {
+        val points = arrayOf(
+            intArrayOf(1, 1),
+            intArrayOf(3, 2),
+            intArrayOf(5, 3),
+            intArrayOf(4, 1),
+            intArrayOf(2, 3),
+            intArrayOf(1, 4),
+        )
+        val expected = 4
+
+        assertEquals(expected, MaxPointsOnLine.maxPoints(points))
+    }
+}
