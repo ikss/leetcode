a5f29ecc5db7c8c8ab5745dd7265d44b35ce8f24
ikss <iksss.88@gmail.com>
2023-01-12 12:53:58 +0100

    Task: 991. Broken Calculator

---
+++ problems/src/main/kotlin/math/medium/BrokenCalculator.kt
@@ -0,0 +1,22 @@
+package math.medium
+
+/**
+ * There is a broken calculator that has the integer startValue on its display initially. In one operation, you can:
+ * * multiply the number on display by 2, or
+ * * subtract 1 from the number on display.
+ *
+ * Given two integers startValue and target, return the minimum number of operations needed to display target on the calculator.
+ *
+ * [URL](https://leetcode.com/problems/broken-calculator/)
+ */
+object BrokenCalculator {
+    fun brokenCalc(startValue: Int, target: Int): Int {
+        var target = target
+        var ans = 0
+        while (target > startValue) {
+            ans++
+            if (target % 2 == 1) target++ else target /= 2
+        }
+        return ans + startValue - target
+    }
+}
