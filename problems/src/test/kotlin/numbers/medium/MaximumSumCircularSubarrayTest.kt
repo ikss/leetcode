73f4c1f7750c78a68cf050d81c1d954dd8a1d8fb
ikss <iksss.88@gmail.com>
2023-01-18 11:03:30 +0100

    Task: 918. Maximum Sum Circular Subarray

---
+++ problems/src/test/kotlin/numbers/medium/MaximumSumCircularSubarrayTest.kt
@@ -0,0 +1,31 @@
+package numbers.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumSumCircularSubarrayTest {
+
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, -2, 3, -2)
+        val expected = 3
+
+        assertEquals(expected, MaximumSumCircularSubarray.maxSubarraySumCircular(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(5, -3, 5)
+        val expected = 10
+
+        assertEquals(expected, MaximumSumCircularSubarray.maxSubarraySumCircular(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(-3, -2, -3)
+        val expected = -2
+
+        assertEquals(expected, MaximumSumCircularSubarray.maxSubarraySumCircular(nums))
+    }
+}
