561fe605699c0d75999e652fb44de6c2ef858f52
ikss <iksss.88@gmail.com>
2023-01-30 09:59:16 +0100

    Task: 1137. N-th Tribonacci Number

---
+++ problems/src/test/kotlin/numbers/easy/NthTribonacciNumberTest.kt
@@ -0,0 +1,24 @@
+package numbers.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NthTribonacciNumberTest {
+
+    @Test
+    fun test1() {
+        val num = 4
+        val expected = 4
+
+        assertEquals(expected, NthTribonacciNumber.tribonacci(num))
+    }
+
+    @Test
+    fun test2() {
+        val num = 25
+        val expected = 1389537
+
+        assertEquals(expected, NthTribonacciNumber.tribonacci(num))
+    }
+
+}
