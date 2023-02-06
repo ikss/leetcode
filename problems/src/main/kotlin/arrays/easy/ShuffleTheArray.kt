5977bc51bebb837095c1237db448f62367e9dae8
ikss <iksss.88@gmail.com>
2023-02-06 10:06:58 +0100

    Task: 1470. Shuffle the Array (add solution with bit shift)

--- problems/src/main/kotlin/arrays/easy/ShuffleTheArray.kt
+++ problems/src/main/kotlin/arrays/easy/ShuffleTheArray.kt
@@ -8,12 +8,34 @@ package arrays.easy
  * [URL](https://leetcode.com/problems/shuffle-the-array/)
  */
 object ShuffleTheArray {
-    fun shuffle(nums: IntArray, n: Int): IntArray {
+    fun shuffleNaive(nums: IntArray, n: Int): IntArray {
         val result = IntArray(2*n)
         for (i in 0 until n) {
             result[2*i] = nums[i]
             result[2*i+1] = nums[i+n]
         }
         return result
+    }
+
+    fun shuffleWithBits(nums: IntArray, n: Int): IntArray? {
+        // Store each y(i) with respective x(i).
+        for (i in n until 2 * n) {
+            val secondNum = nums[i] shl 10
+            nums[i - n] = nums[i - n] or secondNum
+        }
+
+        // '0000000000 1111111111' in decimal.
+        val allOnes = Math.pow(2.0, 10.0).toInt() - 1
+
+        // We will start putting all numbers from the end,
+        // as they are empty places.
+        for (i in n - 1 downTo 0) {
+            // Fetch both the numbers from the current index.
+            val secondNum = nums[i] shr 10
+            val firstNum = nums[i] and allOnes
+            nums[2 * i + 1] = secondNum
+            nums[2 * i] = firstNum
+        }
+        return nums
     }
 }
--- problems/src/test/kotlin/arrays/easy/ShuffleTheArrayTest.kt
+++ problems/src/test/kotlin/arrays/easy/ShuffleTheArrayTest.kt
@@ -5,29 +5,56 @@ import org.junit.jupiter.api.Test
 
 class ShuffleTheArrayTest {
     @Test
-    fun test1() {
+    fun test1naive() {
         val nums = intArrayOf(2, 5, 1, 3, 4, 7)
         val n = 3
         val expected = intArrayOf(2, 3, 5, 4, 1, 7)
 
-        assertTrue(expected.contentEquals(ShuffleTheArray.shuffle(nums, n)))
+        assertTrue(expected.contentEquals(ShuffleTheArray.shuffleNaive(nums, n)))
     }
 
     @Test
-    fun test2() {
+    fun test2naive() {
         val nums = intArrayOf(1, 2, 3, 4, 4, 3, 2, 1)
         val n = 4
         val expected = intArrayOf(1, 4, 2, 3, 3, 2, 4, 1)
 
-        assertTrue(expected.contentEquals(ShuffleTheArray.shuffle(nums, n)))
+        assertTrue(expected.contentEquals(ShuffleTheArray.shuffleNaive(nums, n)))
     }
 
     @Test
-    fun test3() {
+    fun test3naive() {
         val nums = intArrayOf(1, 1, 2, 2)
         val n = 2
         val expected = intArrayOf(1, 2, 1, 2)
 
-        assertTrue(expected.contentEquals(ShuffleTheArray.shuffle(nums, n)))
+        assertTrue(expected.contentEquals(ShuffleTheArray.shuffleNaive(nums, n)))
+    }
+
+    @Test
+    fun test1withBits() {
+        val nums = intArrayOf(2, 5, 1, 3, 4, 7)
+        val n = 3
+        val expected = intArrayOf(2, 3, 5, 4, 1, 7)
+
+        assertTrue(expected.contentEquals(ShuffleTheArray.shuffleWithBits(nums, n)))
+    }
+
+    @Test
+    fun test2withBits() {
+        val nums = intArrayOf(1, 2, 3, 4, 4, 3, 2, 1)
+        val n = 4
+        val expected = intArrayOf(1, 4, 2, 3, 3, 2, 4, 1)
+
+        assertTrue(expected.contentEquals(ShuffleTheArray.shuffleWithBits(nums, n)))
+    }
+
+    @Test
+    fun test3withBits() {
+        val nums = intArrayOf(1, 1, 2, 2)
+        val n = 2
+        val expected = intArrayOf(1, 2, 1, 2)
+
+        assertTrue(expected.contentEquals(ShuffleTheArray.shuffleWithBits(nums, n)))
     }
 }
