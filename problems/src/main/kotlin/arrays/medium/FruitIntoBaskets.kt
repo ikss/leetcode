ccd1415dc4118bb4eef6f64544377751c21c136a
ikss <iksss.88@gmail.com>
2023-02-07 08:30:30 +0100

    Task: 904. Fruit Into Baskets (sliding window)

--- problems/src/main/kotlin/arrays/medium/FruitIntoBaskets.kt
+++ problems/src/main/kotlin/arrays/medium/FruitIntoBaskets.kt
@@ -1,5 +1,6 @@
 package arrays.medium
 
+
 /**
  * You are visiting a farm that has a single row of fruit trees arranged from left to right.
  * The trees are represented by an integer array fruits where `fruits[i]` is the type of fruit the ith tree produces.
@@ -16,6 +17,37 @@ package arrays.medium
  * [URL](https://leetcode.com/problems/fruit-into-baskets/)
  */
 object FruitIntoBaskets {
+    fun totalFruitSlidingWindow(fruits: IntArray): Int {
+        // We use a hash map 'basket' to store the number of each type of fruit.
+        val basket = hashMapOf<Int, Int>()
+        var left = 0
+        var maxPicked = 0
+
+        // Add fruit from the right index (right) of the window.
+        for (right in fruits.indices) {
+            val fruitRight = fruits[right]
+            basket[fruitRight] = basket.getOrDefault(fruitRight, 0) + 1
+
+            // If the current window has more than 2 types of fruit,
+            // we remove fruit from the left index (left) of the window,
+            // until the window has only 2 types of fruit.
+            while (basket.size > 2) {
+                val fruitLeft = fruits[left]
+                basket[fruitLeft] = basket[fruitLeft]!! - 1
+                if (basket[fruitLeft] == 0) {
+                    basket.remove(fruitLeft)
+                }
+                left++
+            }
+
+            // Update maxPicked.
+            maxPicked = maxOf(maxPicked, right - left + 1)
+        }
+
+        // Return maxPicked as the maximum number of fruits we can collect.
+        return maxPicked
+    }
+
     fun totalFruitNaive(fruits: IntArray): Int {
         var result = 0
         for (i in fruits.indices) {
--- problems/src/test/kotlin/arrays/medium/FruitIntoBasketsTest.kt
+++ problems/src/test/kotlin/arrays/medium/FruitIntoBasketsTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 class FruitIntoBasketsTest {
     @Test
-    fun test1() {
+    fun test1naive() {
         val fruits = intArrayOf(1, 2, 1)
         val expected = 3
 
@@ -13,7 +13,7 @@ class FruitIntoBasketsTest {
     }
 
     @Test
-    fun test2() {
+    fun test2naive() {
         val fruits = intArrayOf(0, 1, 2, 2)
         val expected = 3
 
@@ -21,10 +21,34 @@ class FruitIntoBasketsTest {
     }
 
     @Test
-    fun test3() {
+    fun test3naive() {
         val fruits = intArrayOf(1, 2, 3, 2, 2)
         val expected = 4
 
         assertEquals(expected, FruitIntoBaskets.totalFruitNaive(fruits))
+    }
+
+    @Test
+    fun test1slidingWindow() {
+        val fruits = intArrayOf(1, 2, 1)
+        val expected = 3
+
+        assertEquals(expected, FruitIntoBaskets.totalFruitSlidingWindow(fruits))
+    }
+
+    @Test
+    fun test2slidingWindow() {
+        val fruits = intArrayOf(0, 1, 2, 2)
+        val expected = 3
+
+        assertEquals(expected, FruitIntoBaskets.totalFruitSlidingWindow(fruits))
+    }
+
+    @Test
+    fun test3slidingWindow() {
+        val fruits = intArrayOf(1, 2, 3, 2, 2)
+        val expected = 4
+
+        assertEquals(expected, FruitIntoBaskets.totalFruitSlidingWindow(fruits))
     }
 }
