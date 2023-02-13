821dbd3faf381fe109cf9f43a1b0dd2939b281e6
ikss <iksss.88@gmail.com>
2023-02-13 09:45:25 +0100

    Task: 1523. Count Odd Numbers in an Interval Range

---
+++ problems/src/main/kotlin/numbers/easy/CountOddNumbersInAnIntervalRange.kt
@@ -0,0 +1,33 @@
+package numbers.easy
+
+
+/**
+ * Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+ *
+ * [URL](https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/)
+ */
+object CountOddNumbersInAnIntervalRange {
+    fun countOddsMy(low: Int, high: Int): Int {
+        var result = 0
+        if (low % 2 != 0) {
+            result++
+        }
+        if (high % 2 != 0) {
+            result++
+        }
+
+        return result + (high - low - result) / 2
+    }
+
+    fun countOddsOfficial(low: Int, high: Int): Int {
+        // If low is even, make it odd.
+        var low = low
+        if (low and 1 == 0) {
+            low++
+        }
+
+        // low could become greater than high due to incrementation
+        // if it is, the answer is 0; otherwise, use the formula.
+        return if (low > high) 0 else (high - low) / 2 + 1
+    }
+}
