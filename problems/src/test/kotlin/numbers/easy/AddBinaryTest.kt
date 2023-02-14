ebdddc5c3c8a11854eb6624aae3216bfeab02574
ikss <iksss.88@gmail.com>
2023-02-14 09:47:39 +0100

    Task: 67. Add Binary

---
+++ problems/src/test/kotlin/numbers/easy/AddBinaryTest.kt
@@ -0,0 +1,25 @@
+package numbers.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class AddBinaryTest {
+
+    @Test
+    fun test1() {
+        val a = "11"
+        val b = "1"
+        val expected = "100"
+
+        assertEquals(expected, AddBinary.addBinary(a, b))
+    }
+
+    @Test
+    fun test2() {
+        val a = "1010"
+        val b = "1011"
+        val expected = "10101"
+
+        assertEquals(expected, AddBinary.addBinary(a, b))
+    }
+}
