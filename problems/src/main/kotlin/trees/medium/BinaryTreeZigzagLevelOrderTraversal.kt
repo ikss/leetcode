4d9be1e4c33706d22751182b0d1de7af2e3481b2
ikss <iksss.88@gmail.com>
2023-02-19 21:09:23 +0100

    Task: 103. Binary Tree Zigzag Level Order Traversal

---
+++ problems/src/main/kotlin/trees/medium/BinaryTreeZigzagLevelOrderTraversal.kt
@@ -0,0 +1,61 @@
+package trees.medium
+
+import data_structures.TreeNode
+import java.util.*
+
+/**
+ * Given the root of a binary tree, return the zigzag level order traversal of its nodes' values.
+ * (i.e., from left to right, then right to left for the next level and alternate between).
+ *
+ *  [URL](https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/)
+ */
+object BinaryTreeZigzagLevelOrderTraversal {
+    fun zigzagLevelOrderNaive(root: TreeNode?): List<List<Int>> {
+        if (root == null) return emptyList()
+
+        val result = arrayListOf<ArrayList<Int>>()
+        var stack = Stack<TreeNode>()
+        stack.push(root)
+        var reversed = true
+
+        while (stack.isNotEmpty()) {
+            val newStack = Stack<TreeNode>()
+            val level = arrayListOf<Int>()
+            val size = stack.size
+
+            for (i in 0 until size) {
+                val node = stack.pop()
+                level.add(node.`val`)
+                if (reversed) {
+                    node.left?.let { newStack.push(it) }
+                    node.right?.let { newStack.push(it) }
+                } else {
+                    node.right?.let { newStack.push(it) }
+                    node.left?.let { newStack.push(it) }
+                }
+            }
+            reversed = !reversed
+            result.add(level)
+            stack = newStack
+        }
+
+        return result
+    }
+
+    fun zigzagLevelOrderDfs(root: TreeNode?): List<List<Int>> {
+        val ans = mutableListOf<MutableList<Int>>()
+        dfs(root, 0, ans)
+        return ans
+    }
+
+    fun dfs(curr: TreeNode?, index: Int, ans: MutableList<MutableList<Int>>) {
+        if (curr != null) {
+            val row = ans.getOrElse(index) { mutableListOf<Int>().also { ans.add(it) } }
+            if (index % 2 == 0) row.add(curr.`val`)
+            else row.add(0, curr.`val`)
+
+            dfs(curr.left, index + 1, ans)
+            dfs(curr.right, index + 1, ans)
+        }
+    }
+}
---
+++ problems/src/test/kotlin/trees/medium/BinaryTreeZigzagLevelOrderTraversalTest.kt
@@ -0,0 +1,72 @@
+package trees.medium
+
+import data_structures.TreeBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class BinaryTreeZigzagLevelOrderTraversalTest {
+
+    @Test
+    fun test1naive() {
+        val tree = TreeBuilder.build(3, 9, 20, null, null, 15, 7)
+        val expected = arrayListOf(arrayListOf(3), arrayListOf(20, 9), arrayListOf(15, 7))
+
+        assertEquals(expected, BinaryTreeZigzagLevelOrderTraversal.zigzagLevelOrderNaive(tree))
+    }
+
+    @Test
+    fun test2naive() {
+        val tree = TreeBuilder.build(1)
+        val expected = arrayListOf(arrayListOf(1))
+
+        assertEquals(expected, BinaryTreeZigzagLevelOrderTraversal.zigzagLevelOrderNaive(tree))
+    }
+
+    @Test
+    fun test3naive() {
+        val tree = TreeBuilder.build()
+        val expected = emptyList<ArrayList<Int>>()
+
+        assertEquals(expected, BinaryTreeZigzagLevelOrderTraversal.zigzagLevelOrderNaive(tree))
+    }
+
+    @Test
+    fun test4naive() {
+        val tree = TreeBuilder.build(0, 2, 4, 1, null, 3, -1, 5, 1, null, null, 6, null, 8)
+        val expected = arrayListOf(arrayListOf(0), arrayListOf(4, 2), arrayListOf(1, 3, -1), arrayListOf(8, 6, 1, 5))
+
+        assertEquals(expected, BinaryTreeZigzagLevelOrderTraversal.zigzagLevelOrderNaive(tree))
+    }
+
+    @Test
+    fun test1dfs() {
+        val tree = TreeBuilder.build(3, 9, 20, null, null, 15, 7)
+        val expected = arrayListOf(arrayListOf(3), arrayListOf(20, 9), arrayListOf(15, 7))
+
+        assertEquals(expected, BinaryTreeZigzagLevelOrderTraversal.zigzagLevelOrderDfs(tree))
+    }
+
+    @Test
+    fun test2dfs() {
+        val tree = TreeBuilder.build(1)
+        val expected = arrayListOf(arrayListOf(1))
+
+        assertEquals(expected, BinaryTreeZigzagLevelOrderTraversal.zigzagLevelOrderDfs(tree))
+    }
+
+    @Test
+    fun test3dfs() {
+        val tree = TreeBuilder.build()
+        val expected = emptyList<ArrayList<Int>>()
+
+        assertEquals(expected, BinaryTreeZigzagLevelOrderTraversal.zigzagLevelOrderDfs(tree))
+    }
+
+    @Test
+    fun test4dfs() {
+        val tree = TreeBuilder.build(0, 2, 4, 1, null, 3, -1, 5, 1, null, null, 6, null, 8)
+        val expected = arrayListOf(arrayListOf(0), arrayListOf(4, 2), arrayListOf(1, 3, -1), arrayListOf(8, 6, 1, 5))
+
+        assertEquals(expected, BinaryTreeZigzagLevelOrderTraversal.zigzagLevelOrderDfs(tree))
+    }
+}
