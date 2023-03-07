49cfa7769772f074d90a37e542f1fb2fef3796d8
ikss <iksss.88@gmail.com>
2023-03-07 09:07:30 +0100

    Task: 1539. Kth Missing Positive Number

---
+++ problems/src/main/kotlin/arrays/easy/KthMissingPositiveNumber.kt
@@ -0,0 +1,46 @@
+package arrays.easy
+
+/**
+ * Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+ *
+ * Return the kth positive integer that is missing from this array.
+ *
+ * [URL](https://leetcode.com/problems/kth-missing-positive-number/)
+ */
+object KthMissingPositiveNumber {
+    fun findKthPositiveNaive(arr: IntArray, k: Int): Int {
+        var prev = arr[0]
+        var missing = prev - 1
+        if (missing >= k) {
+            return k
+        }
+        for (n in arr) {
+            if (prev < n - 1) {
+                missing += n - prev - 1
+                if (missing >= k) {
+                    return n - (missing - k) - 1
+                }
+            }
+            prev = n
+        }
+
+        return arr[arr.size - 1] + k - missing
+    }
+
+    fun findKthPositiveBinarySearch(arr: IntArray, k: Int): Int {
+        var left = 0
+        var right = arr.size
+
+        while (left < right) {
+            val candidate = (left + right) / 2
+            println("candidate: $candidate, val: ${arr[candidate]}")
+            if (arr[candidate] - candidate <= k) {
+                left = candidate + 1
+            } else {
+                right = candidate
+            }
+        }
+
+        return left + k
+    }
+}
