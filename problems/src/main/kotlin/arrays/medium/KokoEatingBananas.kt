df0486f31f458b981449954041322a2966c905d5
ikss <iksss.88@gmail.com>
2023-03-08 08:48:06 +0100

    Task: 875. Koko Eating Bananas

---
+++ problems/src/main/kotlin/arrays/medium/KokoEatingBananas.kt
@@ -0,0 +1,42 @@
+package arrays.medium
+
+/**
+ * Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+ *
+ * Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and
+ * eats k bananas from that pile. If the pile has less than k bananas,
+ * she eats all of them instead and will not eat any more bananas during this hour.
+ *
+ * Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+ *
+ * Return the minimum integer k such that she can eat all the bananas within h hours.
+ *
+ * [URL](https://leetcode.com/problems/koko-eating-bananas/)
+ */
+object KokoEatingBananas {
+    fun minEatingSpeed(piles: IntArray, h: Int): Int {
+        var left = Math.ceil(piles.sum().toDouble() / h).toInt()
+        var right = piles.max()!!
+
+        while (left < right) {
+            val mid = (right - left) / 2 + left
+            if (hasEnoughTime(mid, piles, h)) {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+
+        return left
+    }
+
+    private fun hasEnoughTime(speed: Int, piles: IntArray, h: Int): Boolean {
+        var total = 0
+        val speed = speed.toDouble()
+        for (p in piles) {
+            total += Math.ceil(p / speed).toInt()
+            if (total > h) break
+        }
+        return total <= h
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/KokoEatingBananasTest.kt
@@ -0,0 +1,33 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class KokoEatingBananasTest {
+    @Test
+    fun test1() {
+        val piles = intArrayOf(3, 6, 7, 11)
+        val h = 8
+        val expected = 4
+
+        assertEquals(expected, KokoEatingBananas.minEatingSpeed(piles, h))
+    }
+
+    @Test
+    fun test2() {
+        val piles = intArrayOf(30, 11, 23, 4, 20)
+        val h = 5
+        val expected = 30
+
+        assertEquals(expected, KokoEatingBananas.minEatingSpeed(piles, h))
+    }
+
+    @Test
+    fun test3() {
+        val piles = intArrayOf(30, 11, 23, 4, 20)
+        val h = 6
+        val expected = 23
+
+        assertEquals(expected, KokoEatingBananas.minEatingSpeed(piles, h))
+    }
+}
