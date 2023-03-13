f4e8f29a82762df9042028bed10e3484eac53ec9
ikss <iksss.88@gmail.com>
2023-03-13 08:03:05 +0100

    Task: 109. Convert Sorted List to Binary Search Tree

---
+++ problems/src/main/kotlin/trees/medium/ConvertSortedListToBinarySearchTree.kt
@@ -0,0 +1,35 @@
+package trees.medium
+
+import data_structures.ListNode
+import data_structures.TreeNode
+
+
+/**
+ * Given the head of a singly linked list where elements are sorted in ascending order, convert it to a
+ * height-balanced binary search tree.
+ *
+ * [URL](https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/)
+ */
+object ConvertSortedListToBinarySearchTree {
+    fun sortedListToBST(head: ListNode?): TreeNode? {
+        if (head == null) {
+            return null
+        } else if (head.next == null) {
+            return TreeNode(head.`val`)
+        }
+        var slow: ListNode = head
+        var pre = slow
+        var fast = head
+        while (fast?.next != null) {
+            pre = slow
+            slow = slow.next!!
+            fast = fast.next!!.next
+        }
+        // cut left sub list here
+        pre.next = null
+        return TreeNode(slow.`val`).apply {
+            left = sortedListToBST(head)
+            right = sortedListToBST(slow.next)
+        }
+    }
+}
---
+++ problems/src/test/kotlin/trees/medium/ConvertSortedListToBinarySearchTreeTest.kt
@@ -0,0 +1,25 @@
+package trees.medium
+
+import data_structures.ListBuilder
+import data_structures.TreeBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ConvertSortedListToBinarySearchTreeTest {
+
+    @Test
+    fun test1() {
+        val head = ListBuilder.build(-10, -3, 0, 5, 9)
+        val expected = TreeBuilder.build(0, -3, 9, -10, null, 5, null)
+
+        assertEquals(expected, ConvertSortedListToBinarySearchTree.sortedListToBST(head))
+    }
+
+    @Test
+    fun test2() {
+        val head = ListBuilder.build()
+        val expected = TreeBuilder.build()
+
+        assertEquals(expected, ConvertSortedListToBinarySearchTree.sortedListToBST(head))
+    }
+}
