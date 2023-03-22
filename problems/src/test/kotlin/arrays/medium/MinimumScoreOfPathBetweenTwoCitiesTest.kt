1916aca9ac06aca6b7276cfaab5895f7ca6b9545
ikss <iksss.88@gmail.com>
2023-03-22 11:49:04 +0100

    Task: 2492. Minimum Score of a Path Between Two Cities

---
+++ problems/src/test/kotlin/arrays/medium/MinimumScoreOfPathBetweenTwoCitiesTest.kt
@@ -0,0 +1,24 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumScoreOfPathBetweenTwoCitiesTest {
+    @Test
+    fun test1() {
+        val n = 4
+        val roads = arrayOf(intArrayOf(1, 2, 9), intArrayOf(2, 3, 6), intArrayOf(2, 4, 5), intArrayOf(1, 4, 7))
+        val expected = 5
+
+        assertEquals(expected, MinimumScoreOfPathBetweenTwoCities.minScore(n, roads))
+    }
+
+    @Test
+    fun test2() {
+        val n = 4
+        val roads = arrayOf(intArrayOf(1, 2, 2), intArrayOf(1, 3, 4), intArrayOf(3, 4, 7))
+        val expected = 2
+
+        assertEquals(expected, MinimumScoreOfPathBetweenTwoCities.minScore(n, roads))
+    }
+}
