efcc830103288e66b03b1d870f29f2d0d4092efa
ikss <iksss.88@gmail.com>
2023-04-05 09:32:36 +0200

    Task: 2405. Optimal Partition of String

---
+++ problems/src/test/kotlin/strings/medium/OptimalPartitionOfStringTest.kt
@@ -0,0 +1,39 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class OptimalPartitionOfStringTest {
+
+    @Test
+    fun test1naive() {
+        val s = "abacaba"
+        val expected = 4
+
+        assertEquals(expected, OptimalPartitionOfString.partitionStringNaive(s))
+    }
+
+    @Test
+    fun test2naive() {
+        val s = "ssssss"
+        val expected = 6
+
+        assertEquals(expected, OptimalPartitionOfString.partitionStringNaive(s))
+    }
+
+    @Test
+    fun test1optimized() {
+        val s = "abacaba"
+        val expected = 4
+
+        assertEquals(expected, OptimalPartitionOfString.partitionStringOptimized(s))
+    }
+
+    @Test
+    fun test2optimized() {
+        val s = "ssssss"
+        val expected = 6
+
+        assertEquals(expected, OptimalPartitionOfString.partitionStringOptimized(s))
+    }
+}
