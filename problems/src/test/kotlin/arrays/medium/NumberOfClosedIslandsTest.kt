cd7be69dc886eb9191d0d8d8cc0bd9f813cfdaa4
ikss <iksss.88@gmail.com>
2023-04-06 16:01:33 +0200

    Task: 1254. Number of Closed Islands

---
+++ problems/src/test/kotlin/arrays/medium/NumberOfClosedIslandsTest.kt
@@ -0,0 +1,48 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NumberOfClosedIslandsTest {
+    @Test
+    fun test1() {
+        val grid = arrayOf(
+            intArrayOf(1, 1, 1, 1, 1, 1, 1, 0),
+            intArrayOf(1, 0, 0, 0, 0, 1, 1, 0),
+            intArrayOf(1, 0, 1, 0, 1, 1, 1, 0),
+            intArrayOf(1, 0, 0, 0, 0, 1, 0, 1),
+            intArrayOf(1, 1, 1, 1, 1, 1, 1, 0),
+        )
+        val expected = 2
+
+        assertEquals(expected, NumberOfClosedIslands.closedIsland(grid))
+    }
+
+    @Test
+    fun test2() {
+        val grid = arrayOf(
+            intArrayOf(0, 0, 1, 0, 0),
+            intArrayOf(0, 1, 0, 1, 0),
+            intArrayOf(0, 1, 1, 1, 0),
+        )
+        val expected = 1
+
+        assertEquals(expected, NumberOfClosedIslands.closedIsland(grid))
+    }
+
+    @Test
+    fun test3() {
+        val grid = arrayOf(
+            intArrayOf(1, 1, 1, 1, 1, 1, 1),
+            intArrayOf(1, 0, 0, 0, 0, 0, 1),
+            intArrayOf(1, 0, 1, 1, 1, 0, 1),
+            intArrayOf(1, 0, 1, 0, 1, 0, 1),
+            intArrayOf(1, 0, 1, 1, 1, 0, 1),
+            intArrayOf(1, 0, 0, 0, 0, 0, 1),
+            intArrayOf(1, 1, 1, 1, 1, 1, 1),
+        )
+        val expected = 2
+
+        assertEquals(expected, NumberOfClosedIslands.closedIsland(grid))
+    }
+}
