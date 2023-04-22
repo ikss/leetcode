8b35bcbf719e0a07c6cffb10276a444c76676d6c
ikss <iksss.88@gmail.com>
2023-04-22 22:26:34 +0200

    63. Unique Paths II

---
+++ problems/src/test/kotlin/arrays/medium/UniquePathsIITest.kt
@@ -0,0 +1,23 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class UniquePathsIITest {
+
+    @Test
+    fun test1() {
+        val obstacleGrid = arrayOf(intArrayOf(0, 0, 0), intArrayOf(0, 1, 0), intArrayOf(0, 0, 0))
+        val expected = 2
+
+        assertEquals(expected, UniquePathsII.uniquePathsWithObstacles(obstacleGrid))
+    }
+
+    @Test
+    fun test2() {
+        val obstacleGrid = arrayOf(intArrayOf(0, 1), intArrayOf(0, 0))
+        val expected = 1
+
+        assertEquals(expected, UniquePathsII.uniquePathsWithObstacles(obstacleGrid))
+    }
+}
