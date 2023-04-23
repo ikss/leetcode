2560dfab76986739b709bd1264a5335461f8724d
ikss <iksss.88@gmail.com>
2023-04-23 20:48:41 +0200

    Task: 205. Isomorphic Strings

---
+++ problems/src/test/kotlin/strings/easy/IsomorphicStringsTest.kt
@@ -0,0 +1,34 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class IsomorphicStringsTest {
+
+    @Test
+    fun test1() {
+        val s = "egg"
+        val t = "add"
+        val expected = true
+
+        assertEquals(expected, IsomorphicStrings.isIsomorphic(s, t))
+    }
+
+    @Test
+    fun test2() {
+        val s = "foo"
+        val t = "bar"
+        val expected = false
+
+        assertEquals(expected, IsomorphicStrings.isIsomorphic(s, t))
+    }
+
+    @Test
+    fun test3() {
+        val s = "paper"
+        val t = "title"
+        val expected = true
+
+        assertEquals(expected, IsomorphicStrings.isIsomorphic(s, t))
+    }
+}
