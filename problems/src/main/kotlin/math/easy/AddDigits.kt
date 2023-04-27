66478448d512a20c708013103ef7acacf567f59e
ikss <iksss.88@gmail.com>
2023-04-27 08:31:59 +0200

    Task: 258. Add Digits

---
+++ problems/src/main/kotlin/math/easy/AddDigits.kt
@@ -0,0 +1,24 @@
+package math.easy
+
+/**
+ * Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+ *
+ * [URL](https://leetcode.com/problems/add-digits/)
+ */
+object AddDigits {
+    fun addDigitsTailrec(num: Int): Int {
+        return if (num < 9) {
+            num
+        } else {
+            var res = 0
+            for (c in num.toString()) {
+                res += c - '0'
+            }
+            res
+        }
+    }
+
+    fun addDigitsMath(num: Int): Int {
+        return if (num == 0) 0 else 1 + (num - 1) % 9
+    }
+}
