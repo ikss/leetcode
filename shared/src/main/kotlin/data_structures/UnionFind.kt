0c5380ac006ac8ff9235d7a7a251ff61c8984e25
ikss <iksss.88@gmail.com>
2023-04-30 21:21:12 +0200

    Task: 1579. Remove Max Number of Edges to Keep Graph Fully Traversable

--- shared/src/main/kotlin/data_structures/UnionFind.kt
+++ shared/src/main/kotlin/data_structures/UnionFind.kt
@@ -1,8 +1,9 @@
 package data_structures
 
 class UnionFind(n: Int) {
-    private val root = IntArray(n)
-    private val rank = IntArray(n)
+    private val root = IntArray(n + 1)
+    private val rank = IntArray(n + 1)
+    private var components = n
 
     init {
         for (i in 0 until n) {
@@ -17,10 +18,10 @@ class UnionFind(n: Int) {
         return root[x]
     }
 
-    fun union(x: Int, y: Int) {
+    fun union(x: Int, y: Int): Int {
         var rootX = find(x)
         var rootY = find(y)
-        if (rootX == rootY) return
+        if (rootX == rootY) return 0
         if (rank[rootX] > rank[rootY]) {
             val tmp = rootX
             rootX = rootY
@@ -28,11 +29,15 @@ class UnionFind(n: Int) {
         }
         // Modify the root of the smaller group as the root of the
         // larger group, also increment the size of the larger group.
+        components--
         root[rootX] = rootY
         rank[rootY] += rank[rootX]
+        return 1
     }
 
     fun areConnected(node1: Int, node2: Int): Boolean {
         return find(node1) == find(node2)
     }
+
+    fun allIsConnected(): Boolean = components == 1
 }
