ed2ced2671c93a0977089e824f36091e669fb894
ikss <iksss.88@gmail.com>
2023-05-01 20:26:41 +0200

    Task: 1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ problems/src/main/kotlin/arrays/easy/AverageSalaryExcludingTheMinimumAndMaximumSalary.kt
@@ -0,0 +1,27 @@
+package arrays.easy
+
+/**
+ * You are given an array of unique integers salary where `salary[i]` is the salary of the ith employee.
+ *
+ * Return the average salary of employees excluding the minimum and maximum salary.
+ * Answers within 10-5 of the actual answer will be accepted.
+  *
+ * [URL](https://leetcode.com/problems/two-sum/)
+ */
+object AverageSalaryExcludingTheMinimumAndMaximumSalary {
+    fun average(salary: IntArray): Double {
+        var min = Int.MAX_VALUE
+        var max = Int.MIN_VALUE
+        var sum = 0L
+
+        for (s in salary) {
+            sum += s
+            min = minOf(min, s)
+            max = maxOf(max, s)
+        }
+
+        sum -= (min + max)
+
+        return sum.toDouble() / (salary.size - 2)
+    }
+}
