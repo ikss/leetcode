05cd157ead9489a4e3a0827e59b29affff4389c8
ikss <iksss.88@gmail.com>
2023-05-05 10:12:10 +0200

    Task: 1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ problems/src/main/kotlin/strings/medium/MaximumNumberOfVowelsInSubstringOfGivenLength.kt
@@ -0,0 +1,32 @@
+package strings.medium
+
+/**
+ * Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+ *
+ * Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+ *
+ * [URL](https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/)
+ */
+object MaximumNumberOfVowelsInSubstringOfGivenLength {
+    fun maxVowels(s: String, k: Int): Int {
+        val vowels = hashSetOf('a', 'e', 'i', 'o', 'u')
+        var count = 0
+        for (i in 0 until k) {
+            if (vowels.contains(s[i])) {
+                count++
+            }
+        }
+        var result = count
+        for (i in k until s.length) {
+            if (vowels.contains(s[i])) {
+                count++
+            }
+            if (vowels.contains(s[i - k])) {
+                count--
+            }
+            result = maxOf(result, count)
+        }
+
+        return result
+    }
+}
