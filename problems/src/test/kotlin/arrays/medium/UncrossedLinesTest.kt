ae5d4b439a4be536d62f217da7611a69b7f2c3ff
ikss <iksss.88@gmail.com>
2023-05-11 10:36:51 +0200

    Task: 1035. Uncrossed Lines

---
+++ problems/src/test/kotlin/arrays/medium/UncrossedLinesTest.kt
@@ -0,0 +1,34 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class UncrossedLinesTest {
+
+    @Test
+    fun test1() {
+        val nums1 = intArrayOf(1, 4, 2)
+        val nums2 = intArrayOf(1, 2, 4)
+        val expected = 2
+
+        assertEquals(expected, UncrossedLines.maxUncrossedLines(nums1, nums2))
+    }
+
+    @Test
+    fun test2() {
+        val nums1 = intArrayOf(2, 5, 1, 2, 5)
+        val nums2 = intArrayOf(10, 5, 2, 1, 5, 2)
+        val expected = 3
+
+        assertEquals(expected, UncrossedLines.maxUncrossedLines(nums1, nums2))
+    }
+
+    @Test
+    fun test3() {
+        val nums1 = intArrayOf(1, 3, 7, 1, 7, 5)
+        val nums2 = intArrayOf(1, 9, 2, 5, 1)
+        val expected = 2
+
+        assertEquals(expected, UncrossedLines.maxUncrossedLines(nums1, nums2))
+    }
+}
