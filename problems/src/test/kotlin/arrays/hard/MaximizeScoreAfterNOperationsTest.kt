fd3a2dfd06f9e159e2881add7ec3b84d370c077c
ikss <iksss.88@gmail.com>
2023-05-14 22:36:23 +0200

    Task: 1799. Maximize Score After N Operations

---
+++ problems/src/test/kotlin/arrays/hard/MaximizeScoreAfterNOperationsTest.kt
@@ -0,0 +1,23 @@
+package arrays.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximizeScoreAfterNOperationsTest {
+
+    @Test
+    fun test1recursive() {
+        val nums = intArrayOf(1, 2)
+        val expected = 1
+
+        assertEquals(expected, MaximizeScoreAfterNOperations.maxScore(nums))
+    }
+
+    @Test
+    fun test2recursive() {
+        val nums = intArrayOf(3, 4, 6, 8)
+        val expected = 11
+
+        assertEquals(expected, MaximizeScoreAfterNOperations.maxScore(nums))
+    }
+}
