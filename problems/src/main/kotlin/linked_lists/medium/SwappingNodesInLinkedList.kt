131082272fc897bbf4a63f378851af27c58bc940
ikss <iksss.88@gmail.com>
2023-05-15 14:56:30 +0200

    Task: 1721. Swapping Nodes in a Linked List

---
+++ problems/src/main/kotlin/linked_lists/medium/SwappingNodesInLinkedList.kt
@@ -0,0 +1,35 @@
+package linked_lists.medium
+
+import data_structures.ListNode
+
+/**
+ * You are given the head of a linked list, and an integer k.
+ *
+ * Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node
+ * from the end (the list is 1-indexed).
+ *
+ * [URL](https://leetcode.com/problems/swapping-nodes-in-a-linked-list/)
+ */
+object SwappingNodesInLinkedList {
+    fun swapNodes(head: ListNode?, k: Int): ListNode? {
+        var fast = head
+        for (i in 1 until k) {
+            fast = fast!!.next
+        }
+        val first = fast
+
+        var slow = head
+        while (fast!!.next != null) {
+            fast = fast.next
+            slow = slow!!.next
+        }
+        val second = slow
+
+        val tmp = first!!.`val`
+        first.`val` = second!!.`val`
+        second.`val` = tmp
+
+        return head
+    }
+
+}
---
+++ problems/src/test/kotlin/linked_lists/medium/SwappingNodesInLinkedListTest.kt
@@ -0,0 +1,26 @@
+package linked_lists.medium
+
+import data_structures.ListBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SwappingNodesInLinkedListTest {
+
+    @Test
+    fun test1() {
+        val head = ListBuilder.build(1, 2, 3, 4, 5)
+        val k = 2
+        val expected = ListBuilder.build(1, 4, 3, 2, 5)
+
+        assertEquals(expected, SwappingNodesInLinkedList.swapNodes(head, k))
+    }
+
+    @Test
+    fun test2() {
+        val head = ListBuilder.build(7, 9, 6, 6, 7, 8, 3, 0, 9, 5)
+        val k = 5
+        val expected = ListBuilder.build(7, 9, 6, 6, 8, 7, 3, 0, 9, 5)
+
+        assertEquals(expected, SwappingNodesInLinkedList.swapNodes(head, k))
+    }
+}
