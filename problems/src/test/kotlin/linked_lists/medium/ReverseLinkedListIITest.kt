354407e4eb0ade4b6dd5192a1a6bcf893de49943
ikss <iksss.88@gmail.com>
2023-05-15 14:54:31 +0200

    More tests in ReverseLinkedListII

--- problems/src/test/kotlin/linked_lists/medium/ReverseLinkedListIITest.kt
+++ problems/src/test/kotlin/linked_lists/medium/ReverseLinkedListIITest.kt
@@ -5,25 +5,44 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class ReverseLinkedListIITest {
-    private val initial = ListBuilder.build(1, 2, 3, 4, 5)
-    private val expected = ListBuilder.build(1, 4, 3, 2, 5)
 
     @Test
-    fun testReverseRecursive() {
+    fun test1Recursive() {
+        val initial = ListBuilder.build(1, 2, 3, 4, 5)
         val start = 2
         val end = 4
+        val expected = ListBuilder.build(1, 4, 3, 2, 5)
 
         assertEquals(expected, ReverseLinkedListII.reverseBetweenRecursive(initial, start, end))
     }
 
     @Test
-    fun testReverseLoop() {
+    fun test2Recursive() {
+        val initial = ListBuilder.build(2, 1)
         val start = 1
         val end = 2
+        val expected = ListBuilder.build(1, 2)
 
-        assertEquals(
-            ListBuilder.build(2, 1),
-            ReverseLinkedListII.reverseBetweenLoop(ListBuilder.build(1, 2), start, end)
-        )
+        assertEquals(initial, ReverseLinkedListII.reverseBetweenLoop(expected, start, end))
+    }
+
+    @Test
+    fun test1Loop() {
+        val initial = ListBuilder.build(1, 2, 3, 4, 5)
+        val start = 2
+        val end = 4
+        val expected = ListBuilder.build(1, 4, 3, 2, 5)
+
+        assertEquals(expected, ReverseLinkedListII.reverseBetweenRecursive(initial, start, end))
+    }
+
+    @Test
+    fun test2Loop() {
+        val initial = ListBuilder.build(2, 1)
+        val start = 1
+        val end = 2
+        val expected = ListBuilder.build(1, 2)
+
+        assertEquals(initial, ReverseLinkedListII.reverseBetweenLoop(expected, start, end))
     }
 }
