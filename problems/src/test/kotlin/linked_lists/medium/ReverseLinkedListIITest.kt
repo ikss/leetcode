9f85864d1f031fb83403988b826cb2a10d3105ca
ikss <iksss.88@gmail.com>
2023-05-16 08:38:02 +0200

    Fix tests in ReverseLinkedListII

--- problems/src/test/kotlin/linked_lists/medium/ReverseLinkedListIITest.kt
+++ problems/src/test/kotlin/linked_lists/medium/ReverseLinkedListIITest.kt
@@ -8,41 +8,41 @@ class ReverseLinkedListIITest {
 
     @Test
     fun test1Recursive() {
-        val initial = ListBuilder.build(1, 2, 3, 4, 5)
+        val head = ListBuilder.build(1, 2, 3, 4, 5)
         val start = 2
         val end = 4
         val expected = ListBuilder.build(1, 4, 3, 2, 5)
 
-        assertEquals(expected, ReverseLinkedListII.reverseBetweenRecursive(initial, start, end))
+        assertEquals(expected, ReverseLinkedListII.reverseBetweenRecursive(head, start, end))
     }
 
     @Test
     fun test2Recursive() {
-        val initial = ListBuilder.build(2, 1)
+        val head = ListBuilder.build(2, 1)
         val start = 1
         val end = 2
         val expected = ListBuilder.build(1, 2)
 
-        assertEquals(initial, ReverseLinkedListII.reverseBetweenLoop(expected, start, end))
+        assertEquals(expected, ReverseLinkedListII.reverseBetweenLoop(head, start, end))
     }
 
     @Test
     fun test1Loop() {
-        val initial = ListBuilder.build(1, 2, 3, 4, 5)
+        val head = ListBuilder.build(1, 2, 3, 4, 5)
         val start = 2
         val end = 4
         val expected = ListBuilder.build(1, 4, 3, 2, 5)
 
-        assertEquals(expected, ReverseLinkedListII.reverseBetweenRecursive(initial, start, end))
+        assertEquals(expected, ReverseLinkedListII.reverseBetweenRecursive(head, start, end))
     }
 
     @Test
     fun test2Loop() {
-        val initial = ListBuilder.build(2, 1)
+        val head = ListBuilder.build(2, 1)
         val start = 1
         val end = 2
         val expected = ListBuilder.build(1, 2)
 
-        assertEquals(initial, ReverseLinkedListII.reverseBetweenLoop(expected, start, end))
+        assertEquals(expected, ReverseLinkedListII.reverseBetweenLoop(head, start, end))
     }
 }
