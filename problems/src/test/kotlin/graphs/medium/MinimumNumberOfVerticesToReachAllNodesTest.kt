df3f7ce12050c23a6618cce6187b9c8bc4ed3fd4
ikss <iksss.88@gmail.com>
2023-05-18 09:11:42 +0200

    Task: 1557. Minimum Number of Vertices to Reach All Nodes

---
+++ problems/src/test/kotlin/graphs/medium/MinimumNumberOfVerticesToReachAllNodesTest.kt
@@ -0,0 +1,25 @@
+package graphs.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumNumberOfVerticesToReachAllNodesTest {
+
+    @Test
+    fun test1() {
+        val n = 6
+        val edges = listOf(listOf(0, 1), listOf(0, 2), listOf(2, 5), listOf(3, 4), listOf(4, 2))
+        val expected = listOf(0, 3)
+
+        assertEquals(expected, MinimumNumberOfVerticesToReachAllNodes.findSmallestSetOfVertices(n, edges))
+    }
+
+    @Test
+    fun test2() {
+        val n = 5
+        val edges = listOf(listOf(0, 1), listOf(2, 1), listOf(3, 1), listOf(1, 4), listOf(2, 4))
+        val expected = listOf(0, 2, 3)
+
+        assertEquals(expected, MinimumNumberOfVerticesToReachAllNodes.findSmallestSetOfVertices(n, edges))
+    }
+}
