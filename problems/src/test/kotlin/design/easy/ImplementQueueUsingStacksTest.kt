6ed4131a6a439734586910953badaa0029085126
ikss <iksss.88@gmail.com>
2023-05-23 08:57:38 +0200

    Rename test

--- problems/src/test/kotlin/design/easy/ImplementQueueUsingStacksTest.kt
+++ problems/src/test/kotlin/design/easy/ImplementQueueUsingStacksTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class ImplementQueueUsingStacksTest {
 
     @Test
-    fun test1node() {
+    fun test1() {
         val myQueue = ImplementQueueUsingStacks.MyQueue()
         myQueue.push(1)                             // queue is: [1]
         myQueue.push(2)                             // queue is: [1, 2] (leftmost is front of the queue)
