39f7e2be9e030891f45da47e01d9e761af5f0f40
ikss <iksss.88@gmail.com>
2023-05-26 08:06:31 +0200

    1140. Stone Game II (simplified solution)

--- problems/src/main/kotlin/arrays/medium/StoneGameII.kt
+++ problems/src/main/kotlin/arrays/medium/StoneGameII.kt
@@ -1,5 +1,8 @@
 package arrays.medium
 
+import java.util.*
+
+
 /**
  * Alice and Bob continue their games with piles of stones. There are a number of piles arranged in a row,
  * and each pile has a positive integer number of stones `piles[i]`.
@@ -18,35 +21,28 @@ package arrays.medium
  */
 object StoneGameII {
     fun stoneGameII(piles: IntArray): Int {
-        if (piles.isEmpty()) return 0
-
-        // cache[i][j] is the max number of stones a player can get when the first pile is piles[i] and M == j.
-        val cache = Array(piles.size) { IntArray(piles.size) }
-        // suffixSum[i] starting from i sum up everything to the right: sum(piles[i, piles.length - 1])
-        val suffixSum = IntArray(piles.size)
-        suffixSum[suffixSum.size - 1] = piles[piles.size - 1]
-        for (i in piles.size - 2 downTo 0) {
-            suffixSum[i] = piles[i] + suffixSum[i + 1]
+        val suffixSum = piles.copyOf()
+        for (i in suffixSum.size - 2 downTo 0) {
+            suffixSum[i] += suffixSum[i + 1]
         }
-        return helper(piles, suffixSum, cache, 0, 1)
+        val dp = Array(piles.size) { IntArray(piles.size) }
+        return dfs(suffixSum, 1, 0, dp)
     }
 
-    // dfs with memoization
-    private fun helper(piles: IntArray, suffixSum: IntArray, cache: Array<IntArray>, firstPile: Int, M: Int): Int {
-        if (firstPile == piles.size) return 0 // no more piles left
-        // Number of remaining piles is <= than the number of piles we can take in the current turn 2*M.
-        // We just take all remaining piles.
-        if (piles.size - firstPile <= 2 * M) return suffixSum[firstPile]
-        if (cache[firstPile][M] != 0) return cache[firstPile][M]
-        var result = 0
-        // Try out all possible next moves, store the max amount of stones we can get
-        for (x in 1..2 * M) {
-            // suffixSum[firstPile] is the total number of stones left in the game, it's the maximum possible gain.
-            // helper(...) is the max amount of stones the next player can get if we make the current move x.
-            // We want to make the move that minimizes the final gain of the next player, this move maximizes our own final gain.
-            result = maxOf(result, suffixSum[firstPile] - helper(piles, suffixSum, cache, firstPile + x, maxOf(M, x)))
+    private fun dfs(presum: IntArray, m: Int, pile: Int, memo: Array<IntArray>): Int {
+        if (pile + 2 * m >= presum.size) { // last player takes all
+            return presum[pile]
         }
-        cache[firstPile][M] = result
-        return result
+        if (memo[pile][m] > 0) return memo[pile][m]
+        var res = 0
+        var take = 0
+        for (i in 1..2 * m) {
+            // current take
+            take = presum[pile] - presum[pile + i]
+            // take max of current + what lefts from other player max take
+            res = maxOf(res, take + presum[pile + i] - dfs(presum, maxOf(i, m), pile + i, memo))
+        }
+        memo[pile][m] = res
+        return res
     }
 }
