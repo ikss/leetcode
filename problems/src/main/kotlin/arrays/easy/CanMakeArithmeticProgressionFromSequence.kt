8130911f5e3690c45a08acd62b6a09a34c8f1e44
ikss <iksss.88@gmail.com>
2023-06-06 10:41:09 +0200

    Task: 1502. Can Make Arithmetic Progression From Sequence (optimized)

--- problems/src/main/kotlin/arrays/easy/CanMakeArithmeticProgressionFromSequence.kt
+++ problems/src/main/kotlin/arrays/easy/CanMakeArithmeticProgressionFromSequence.kt
@@ -10,7 +10,7 @@ package arrays.easy
  * [URL](https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/)
  */
 object CanMakeArithmeticProgressionFromSequence {
-    fun canMakeArithmeticProgression(arr: IntArray): Boolean {
+    fun canMakeArithmeticProgressionSort(arr: IntArray): Boolean {
         arr.sort()
         val diff = arr[1] - arr[0]
 
@@ -18,5 +18,31 @@ object CanMakeArithmeticProgressionFromSequence {
             if (arr[i] - arr[i - 1] != diff) return false
         }
         return true
+    }
+
+    fun canMakeArithmeticProgressionOptimized(arr: IntArray): Boolean {
+        val n = arr.size
+        val (min, max) = arr.findMinMax()
+
+        if (min == max) return true
+        if ((max - min) % (n - 1) != 0) return false
+        val delta = (max - min) / (n - 1)
+
+        val set = HashSet<Int>()
+        for (el in arr) {
+            if (((el - min) % delta != 0) || !set.add(el)) return false
+        }
+        return true
+    }
+
+    private fun IntArray.findMinMax(): Pair<Int, Int> {
+        var min = Int.MAX_VALUE
+        var max = Int.MIN_VALUE
+
+        for (n in this) {
+            if (n < min) min = n
+            if (n > max) max = n
+        }
+        return min to max
     }
 }
--- problems/src/test/kotlin/arrays/easy/CanMakeArithmeticProgressionFromSequenceTest.kt
+++ problems/src/test/kotlin/arrays/easy/CanMakeArithmeticProgressionFromSequenceTest.kt
@@ -5,18 +5,50 @@ import org.junit.jupiter.api.Test
 
 class CanMakeArithmeticProgressionFromSequenceTest {
     @Test
-    fun test1() {
+    fun test1sort() {
         val arr = intArrayOf(3, 5, 1)
         val expected = true
 
-        assertEquals(expected, CanMakeArithmeticProgressionFromSequence.canMakeArithmeticProgression(arr))
+        assertEquals(expected, CanMakeArithmeticProgressionFromSequence.canMakeArithmeticProgressionSort(arr))
     }
 
     @Test
-    fun test2() {
+    fun test2sort() {
         val arr = intArrayOf(1, 2, 4)
         val expected = false
 
-        assertEquals(expected, CanMakeArithmeticProgressionFromSequence.canMakeArithmeticProgression(arr))
+        assertEquals(expected, CanMakeArithmeticProgressionFromSequence.canMakeArithmeticProgressionSort(arr))
+    }
+
+    @Test
+    fun test3sort() {
+        val arr = intArrayOf(0, 0, 0)
+        val expected = true
+
+        assertEquals(expected, CanMakeArithmeticProgressionFromSequence.canMakeArithmeticProgressionSort(arr))
+    }
+
+    @Test
+    fun test1optimized() {
+        val arr = intArrayOf(3, 5, 1)
+        val expected = true
+
+        assertEquals(expected, CanMakeArithmeticProgressionFromSequence.canMakeArithmeticProgressionOptimized(arr))
+    }
+
+    @Test
+    fun test2optimized() {
+        val arr = intArrayOf(1, 2, 4)
+        val expected = false
+
+        assertEquals(expected, CanMakeArithmeticProgressionFromSequence.canMakeArithmeticProgressionOptimized(arr))
+    }
+
+    @Test
+    fun test3optimized() {
+        val arr = intArrayOf(0, 0, 0)
+        val expected = true
+
+        assertEquals(expected, CanMakeArithmeticProgressionFromSequence.canMakeArithmeticProgressionOptimized(arr))
     }
 }
