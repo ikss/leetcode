2242e6741b70b7c5910e75112b99c51cecf40f52
ikss <iksss.88@gmail.com>
2023-06-07 11:34:42 +0200

    Task: 1318. Minimum Flips to Make a OR b Equal to c

---
+++ problems/src/test/kotlin/numbers/medium/MinimumFlipsToMakeAOrBEqualToCTest.kt
@@ -0,0 +1,37 @@
+package numbers.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumFlipsToMakeAOrBEqualToCTest {
+
+    @Test
+    fun test1() {
+        val a = 2
+        val b = 6
+        val c = 5
+        val expected = 3
+
+        assertEquals(expected, MinimumFlipsToMakeAOrBEqualToC.minFlips(a, b, c))
+    }
+
+    @Test
+    fun test2() {
+        val a = 4
+        val b = 2
+        val c = 7
+        val expected = 1
+
+        assertEquals(expected, MinimumFlipsToMakeAOrBEqualToC.minFlips(a, b, c))
+    }
+
+    @Test
+    fun test3() {
+        val a = 1
+        val b = 2
+        val c = 3
+        val expected = 0
+
+        assertEquals(expected, MinimumFlipsToMakeAOrBEqualToC.minFlips(a, b, c))
+    }
+}
