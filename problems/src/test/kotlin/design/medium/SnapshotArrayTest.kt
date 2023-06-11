50a05a942a2d8574f83177f43d99fe5da09a690d
ikss <iksss.88@gmail.com>
2023-06-11 14:02:46 +0200

    Task: 1146. Snapshot Array

---
+++ problems/src/test/kotlin/design/medium/SnapshotArrayTest.kt
@@ -0,0 +1,26 @@
+package design.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SnapshotArrayTest {
+
+    @Test
+    fun test1() {
+        // set the length to be 3
+        val snapshotArr = SnapshotArray.SnapshotArray(3)
+
+        // Set array[0] = 5
+        snapshotArr.set(0, 5)
+
+        // Take a snapshot, return snap_id = 0
+        snapshotArr.snap()
+
+        snapshotArr.set(0, 6)
+        // Get the value of array[0] with snap_id = 0, return 5
+        assertEquals(5, snapshotArr.get(0, 0))
+
+        // Get the value of array[0] with snap_id = 1, return 6
+        assertEquals(6, snapshotArr.get(0, 1))
+    }
+}
