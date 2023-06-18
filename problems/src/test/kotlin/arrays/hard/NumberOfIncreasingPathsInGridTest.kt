a7cc0bc17945a81825f47e45d60c46f5d5e7674d
ikss <iksss.88@gmail.com>
2023-06-18 22:11:57 +0200

    Task: 2328. Number of Increasing Paths in a Grid

---
+++ problems/src/test/kotlin/arrays/hard/NumberOfIncreasingPathsInGridTest.kt
@@ -0,0 +1,22 @@
+package arrays.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NumberOfIncreasingPathsInGridTest {
+    @Test
+    fun test1() {
+        val grid = arrayOf(intArrayOf(1, 1), intArrayOf(3, 4))
+        val expected = 8
+
+        assertEquals(expected, NumberOfIncreasingPathsInGrid.countPaths(grid))
+    }
+
+    @Test
+    fun test2() {
+        val grid = arrayOf(intArrayOf(1), intArrayOf(2))
+        val expected = 3
+
+        assertEquals(expected, NumberOfIncreasingPathsInGrid.countPaths(grid))
+    }
+}
