d3d988a83a838d8ad9d37f55cbda7497e1f84a36
ikss <iksss.88@gmail.com>
2023-07-20 11:14:22 +0200

    Task: 735. Asteroid Collision

---
+++ problems/src/test/kotlin/arrays/medium/AsteroidCollisionTest.kt
@@ -0,0 +1,30 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class AsteroidCollisionTest {
+    @Test
+    fun test1() {
+        val asteroids = intArrayOf(5, 10, -5)
+        val expected = intArrayOf(5, 10)
+        val actual = AsteroidCollision.asteroidCollision(asteroids)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun test2() {
+        val asteroids = intArrayOf(8, -8)
+        val expected = intArrayOf()
+        val actual = AsteroidCollision.asteroidCollision(asteroids)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun test3() {
+        val asteroids = intArrayOf(10, 2, -5)
+        val expected = intArrayOf(10)
+        val actual = AsteroidCollision.asteroidCollision(asteroids)
+        assertArrayEquals(expected, actual)
+    }
+}
