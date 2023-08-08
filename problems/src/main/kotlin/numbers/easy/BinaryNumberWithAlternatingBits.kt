aa5a03b206cbd8753752a7cbbd7f3db68963303a
ikss <iksss.88@gmail.com>
2023-08-08 14:34:31 +0200

    Test: 693. Binary Number with Alternating Bits (division by 2)

--- problems/src/main/kotlin/numbers/easy/BinaryNumberWithAlternatingBits.kt
+++ problems/src/main/kotlin/numbers/easy/BinaryNumberWithAlternatingBits.kt
@@ -7,10 +7,21 @@ package numbers.easy
  * [URL](https://leetcode.com/problems/climbing-stairs/)
  */
 object BinaryNumberWithAlternatingBits {
-    fun hasAlternatingBits(n: Int): Boolean {
+    fun hasAlternatingBitsBinaryString(n: Int): Boolean {
         val bits = Integer.toBinaryString(n)
         for (i in 0 until bits.length - 1) {
             if (bits[i] == bits[i + 1]) return false
+        }
+        return true
+    }
+
+    fun hasAlternatingBitsDivision(n: Int): Boolean {
+        var cur = n % 2
+        var rest = n / 2
+        while (rest > 0) {
+            if (cur == rest % 2) return false
+            cur = rest % 2
+            rest /= 2
         }
         return true
     }
--- problems/src/test/kotlin/numbers/easy/BinaryNumberWithAlternatingBitsTest.kt
+++ problems/src/test/kotlin/numbers/easy/BinaryNumberWithAlternatingBitsTest.kt
@@ -5,26 +5,50 @@ import org.junit.jupiter.api.Test
 
 class BinaryNumberWithAlternatingBitsTest {
     @Test
-    fun test1() {
+    fun test1binaryString() {
         val input = 5
         val expected = true
 
-        assertEquals(expected, BinaryNumberWithAlternatingBits.hasAlternatingBits(input))
+        assertEquals(expected, BinaryNumberWithAlternatingBits.hasAlternatingBitsBinaryString(input))
     }
 
     @Test
-    fun test2() {
+    fun test2binaryString() {
         val input = 7
         val expected = false
 
-        assertEquals(expected, BinaryNumberWithAlternatingBits.hasAlternatingBits(input))
+        assertEquals(expected, BinaryNumberWithAlternatingBits.hasAlternatingBitsBinaryString(input))
     }
 
     @Test
-    fun test3() {
+    fun test3binaryString() {
         val input = 11
         val expected = false
 
-        assertEquals(expected, BinaryNumberWithAlternatingBits.hasAlternatingBits(input))
+        assertEquals(expected, BinaryNumberWithAlternatingBits.hasAlternatingBitsBinaryString(input))
+    }
+
+    @Test
+    fun test1division() {
+        val input = 5
+        val expected = true
+
+        assertEquals(expected, BinaryNumberWithAlternatingBits.hasAlternatingBitsDivision(input))
+    }
+
+    @Test
+    fun test2division() {
+        val input = 7
+        val expected = false
+
+        assertEquals(expected, BinaryNumberWithAlternatingBits.hasAlternatingBitsDivision(input))
+    }
+
+    @Test
+    fun test3division() {
+        val input = 11
+        val expected = false
+
+        assertEquals(expected, BinaryNumberWithAlternatingBits.hasAlternatingBitsDivision(input))
     }
 }
