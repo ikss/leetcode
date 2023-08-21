f766630b8e6247702a7a693db762c4052a40dfaf
ikss <iksss.88@gmail.com>
2023-08-21 14:57:15 +0200

    Task: 221. Maximal Square (dp optimized space)

--- problems/src/main/kotlin/arrays/medium/MaximalSquare.kt
+++ problems/src/main/kotlin/arrays/medium/MaximalSquare.kt
@@ -7,7 +7,7 @@ package arrays.medium
  * [URL](https://leetcode.com/problems/maximal-square/)
  */
 object MaximalSquare {
-    fun maximalSquare(matrix: Array<CharArray>): Int {
+    fun maximalSquareDp(matrix: Array<CharArray>): Int {
         val m = matrix.size
         val n = matrix[0].size
         val dp = Array(m + 1) { IntArray(n + 1) }
@@ -19,6 +19,24 @@ object MaximalSquare {
                 dp[i][j] = minOf(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1
                 max = maxOf(max, dp[i][j])
             }
+        }
+        return max * max
+    }
+
+    fun maximalSquareOptimizedDp(matrix: Array<CharArray>): Int {
+        val m = matrix.size
+        val n = matrix[0].size
+        var dp = IntArray(n + 1)
+        var max = 0
+
+        for (i in 1..m) {
+            val tmpDp = IntArray(n + 1)
+            for (j in 1..n) {
+                if (matrix[i - 1][j - 1] == '0') continue
+                tmpDp[j] = minOf(dp[j - 1], dp[j], tmpDp[j - 1]) + 1
+                max = maxOf(max, tmpDp[j])
+            }
+            dp = tmpDp
         }
         return max * max
     }
--- problems/src/test/kotlin/arrays/medium/MaximalSquareTest.kt
+++ problems/src/test/kotlin/arrays/medium/MaximalSquareTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 class MaximalSquareTest {
     @Test
-    fun test1() {
+    fun test1dp() {
         val matrix = arrayOf(
             charArrayOf('1', '0', '1', '0', '0'),
             charArrayOf('1', '0', '1', '1', '1'),
@@ -14,27 +14,60 @@ class MaximalSquareTest {
         )
         val expected = 4
 
-        assertEquals(expected, MaximalSquare.maximalSquare(matrix))
+        assertEquals(expected, MaximalSquare.maximalSquareDp(matrix))
     }
 
     @Test
-    fun test2() {
+    fun test2dp() {
         val matrix = arrayOf(
             charArrayOf('0', '1'),
             charArrayOf('1', '0'),
         )
         val expected = 1
 
-        assertEquals(expected, MaximalSquare.maximalSquare(matrix))
+        assertEquals(expected, MaximalSquare.maximalSquareDp(matrix))
     }
 
     @Test
-    fun test3() {
+    fun test3dp() {
         val matrix = arrayOf(
             charArrayOf('0'),
         )
         val expected = 0
 
-        assertEquals(expected, MaximalSquare.maximalSquare(matrix))
+        assertEquals(expected, MaximalSquare.maximalSquareDp(matrix))
+    }
+
+    fun test1optimizedDp() {
+        val matrix = arrayOf(
+            charArrayOf('1', '0', '1', '0', '0'),
+            charArrayOf('1', '0', '1', '1', '1'),
+            charArrayOf('1', '1', '1', '1', '1'),
+            charArrayOf('1', '0', '0', '1', '0'),
+        )
+        val expected = 4
+
+        assertEquals(expected, MaximalSquare.maximalSquareOptimizedDp(matrix))
+    }
+
+    @Test
+    fun test2optimizedDp() {
+        val matrix = arrayOf(
+            charArrayOf('0', '1'),
+            charArrayOf('1', '0'),
+        )
+        val expected = 1
+
+        assertEquals(expected, MaximalSquare.maximalSquareOptimizedDp(matrix))
+    }
+
+    @Test
+    fun test3optimizedDp() {
+        val matrix = arrayOf(
+            charArrayOf('0'),
+        )
+        val expected = 0
+
+        assertEquals(expected, MaximalSquare.maximalSquareOptimizedDp(matrix))
     }
 }
