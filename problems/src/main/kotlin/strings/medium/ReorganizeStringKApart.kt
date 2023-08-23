36f6516d3fba811447a5ba27fe5deb5c8ea1566e
ikss <iksss.88@gmail.com>
2023-08-23 16:44:52 +0200

    Reorganize String K Apart

---
+++ problems/src/main/kotlin/strings/medium/ReorganizeStringKApart.kt
@@ -0,0 +1,41 @@
+package strings.medium
+
+/**
+ * Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+ *
+ * Return any possible rearrangement of s or return "" if not possible.
+ *
+ * [URL](https://leetcode.com/problems/reorganize-string/)
+ */
+object ReorganizeStringKApart {
+    fun reorganizeString(s: String, k: Int): String {
+        val chars = HashMap<Char, Int>(26)
+        for (c in s) {
+            chars.merge(c, 1, Integer::sum)
+        }
+        val pairs = chars.entries.sortedByDescending { it.value }
+        
+        val result = CharArray(s.length)
+        var index = 0
+        for ((char, count) in pairs) {
+            for (i in 0 until count) {
+                result[index] = char
+                index += k
+                if (index >= s.length) {
+                    index = index % k + 1
+                }
+            }
+        }
+        val hash = IntArray(26)
+        for (i in result.indices) {
+            if (i >= k) {
+                hash[result[i - k] - 'a']--
+            }
+            hash[result[i] - 'a']++
+            if (hash[result[i] - 'a'] > 1) {
+                return ""
+            }
+        }
+        return String(result)
+    }
+}
---
+++ problems/src/test/kotlin/strings/medium/ReorganizeStringKApartTest.kt
@@ -0,0 +1,24 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ReorganizeStringKApartTest {
+    @Test
+    fun test1() {
+        val input = "aaabc"
+        val k = 3
+        val expected = ""
+
+        assertEquals(expected, ReorganizeStringKApart.reorganizeString(input, k))
+    }
+
+    @Test
+    fun test2() {
+        val input = "aaaabbbccd"
+        val k = 3
+        val expected = "abcabcabda"
+
+        assertEquals(expected, ReorganizeStringKApart.reorganizeString(input, k))
+    }
+}
