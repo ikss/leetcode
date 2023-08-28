1ed8dbb63391879c2bd76a94da0abc19b39fc9aa
ikss <iksss.88@gmail.com>
2023-08-28 09:35:54 +0200

    Task: 225. Implement Stack using Queues

---
+++ problems/src/main/kotlin/design/easy/ImplementStackUsingQueues.kt
@@ -0,0 +1,50 @@
+package design.easy
+
+import java.util.*
+
+/**
+ * Implement a last-in-first-out (LIFO) stack using only two queues.
+ * The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+ *
+ * Implement the MyStack class:
+ * * void push(int x) Pushes element x to the top of the stack.
+ * * int pop() Removes the element on the top of the stack and returns it.
+ * * int top() Returns the element on the top of the stack.
+ * * boolean empty() Returns true if the stack is empty, false otherwise.
+ *
+ * __Notes:__
+ * 
+ * You must use only standard operations of a queue, which means that only push to back, peek/pop from front,
+ * size and is empty operations are valid.
+ * Depending on your language, the queue may not be supported natively. You may simulate a queue using a list
+ * or deque (double-ended queue) as long as you use only a queue's standard operations.
+ *
+ * [URL](https://leetcode.com/problems/implement-stack-using-queues/)
+ */
+object ImplementStackUsingQueues {
+    class MyStack() {
+        private var queue = LinkedList<Pair<Int, LinkedList<Any>>>()
+
+        fun push(x: Int) {
+            val temp = LinkedList<Pair<Int, LinkedList<Any>>>()
+            temp.offer(x to queue as LinkedList<Any>)
+            queue = temp
+        }
+
+        fun pop(): Int {
+            val (value, temp) = queue.poll()
+            queue = temp as LinkedList<Pair<Int, LinkedList<Any>>>
+            return value
+        }
+
+        fun top(): Int {
+            val (value, _) = queue.peek()
+            return value
+        }
+
+        fun empty(): Boolean {
+            return queue.isEmpty()
+        }
+
+    }
+}
---
+++ problems/src/test/kotlin/design/easy/ImplementStackUsingQueuesTest.kt
@@ -0,0 +1,17 @@
+package design.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+
+class ImplementStackUsingQueuesTest {
+    @Test
+    fun test1() {
+        val stack = ImplementStackUsingQueues.MyStack()
+        stack.push(1)
+        stack.push(2)
+        assertEquals(2, stack.top())
+        assertEquals(2, stack.pop())
+        assertFalse(stack.empty())
+    }
+}
