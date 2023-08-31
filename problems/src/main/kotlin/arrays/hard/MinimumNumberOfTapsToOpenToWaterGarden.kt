fe87da525dbd8bc354cd70b9e4a5b44b6c510e3b
ikss <iksss.88@gmail.com>
2023-08-31 10:06:39 +0200

    Task: 1326. Minimum Number of Taps to Open to Water a Garden (official solutions)

--- problems/src/main/kotlin/arrays/hard/MinimumNumberOfTapsToOpenToWaterGarden.kt
+++ problems/src/main/kotlin/arrays/hard/MinimumNumberOfTapsToOpenToWaterGarden.kt
@@ -1,6 +1,9 @@
 package arrays.hard
 
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
+
 
 /**
  * There is a one-dimensional garden on the x-axis. The garden starts at the point 0 and ends at the point n.
@@ -17,7 +20,7 @@ import java.util.*
  * [URL](https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/)
  */
 object MinimumNumberOfTapsToOpenToWaterGarden {
-    fun minTapsOwnSolution(n: Int, ranges: IntArray): Int {
+    fun minTapsOwnGreedy(n: Int, ranges: IntArray): Int {
         val pq = PriorityQueue<Pair<Int, Int>> { a1, a2 -> a2.second.compareTo(a1.second) }
         var start = 0
         var result = 0
@@ -43,5 +46,70 @@ object MinimumNumberOfTapsToOpenToWaterGarden {
         }
 
         return -1
+    }
+
+    fun minTapsDp(n: Int, ranges: IntArray): Int {
+        val inf = 1_000_000_000
+        // Create an array to store the minimum number of taps needed for each position
+        val dp = IntArray(n + 1) { inf }
+
+        // Initialize the starting position of the garden
+        dp[0] = 0
+        for (i in 0..n) {
+            // Calculate the leftmost position reachable by the current tap
+            val tapStart = maxOf(0, i - ranges[i])
+            // Calculate the rightmost position reachable by the current tap
+            val tapEnd = minOf(n, i + ranges[i])
+            for (j in tapStart..tapEnd) {
+                // Update with the minimum number of taps
+                dp[tapEnd] = minOf(dp[tapEnd], dp[j] + 1)
+            }
+        }
+
+        return if (dp[n] == inf) -1 else dp[n]
+    }
+
+    fun minTapsGreedy(n: Int, ranges: IntArray): Int {
+        // Create an array to track the maximum reach for each position
+        val maxReach = IntArray(n + 1)
+
+        // Calculate the maximum reach for each tap
+        for (i in ranges.indices) {
+            // Calculate the leftmost position the tap can reach
+            val start = maxOf(0, i - ranges[i])
+            // Calculate the rightmost position the tap can reach
+            val end = minOf(n, i + ranges[i])
+
+            // Update the maximum reach for the leftmost position
+            maxReach[start] = maxOf(maxReach[start], end)
+        }
+
+        // Number of taps used
+        var taps = 0
+        // Current rightmost position reached
+        var currEnd = 0
+        // Next rightmost position that can be reached
+        var nextEnd = 0
+
+        // Iterate through the garden
+        for (i in 0..n) {
+            // Current position cannot be reached
+            if (i > nextEnd) {
+                return -1
+            }
+
+            // Increment taps when moving to a new tap
+            if (i > currEnd) {
+                taps++
+                // Move to the rightmost position that can be reached
+                currEnd = nextEnd
+            }
+
+            // Update the next rightmost position that can be reached
+            nextEnd = max(nextEnd, maxReach[i])
+        }
+
+        // Return the minimum number of taps used
+        return taps
     }
 }
