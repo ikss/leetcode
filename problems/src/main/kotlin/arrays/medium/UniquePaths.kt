683e17d9f467ac9bd8154e12482a4b6ef374ac18
ikss <iksss.88@gmail.com>
2023-09-03 16:14:05 +0200

    Task: 2707. Extra Characters in a String (docs adn style)

--- problems/src/main/kotlin/arrays/medium/UniquePaths.kt
+++ problems/src/main/kotlin/arrays/medium/UniquePaths.kt
@@ -1,8 +1,9 @@
 package arrays.medium
 
 /**
- * There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]).
- * The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+ * There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., `grid[0][0]`).
+ * The robot tries to move to the bottom-right corner (i.e., `grid[m - 1][n - 1]`).
+ * The robot can only move either down or right at any point in time.
  *
  * Given the two integers m and n, return the number of possible unique paths that the robot can take
  * to reach the bottom-right corner.
@@ -17,7 +18,7 @@ object UniquePaths {
 
         for (i in 1 until m) {
             for (j in 1 until n) {
-                dp[j] = dp[j - 1] + dp[j]
+                dp[j] += dp[j - 1]
             }
         }
         return dp[n - 1]
