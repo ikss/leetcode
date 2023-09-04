955fd200f93c81de43aaa051c9f5cd90e5f38a32
ikss <iksss.88@gmail.com>
2023-09-04 11:56:10 +0200

    Task: 1585. Check If String Is Transformable With Substring Sort Operations

---
+++ problems/src/test/kotlin/strings/hard/CheckIfStringIsTransformableWithSubstringSortOperationsTest.kt
@@ -0,0 +1,33 @@
+package strings.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CheckIfStringIsTransformableWithSubstringSortOperationsTest {
+    @Test
+    fun test1() {
+        val s = "84532"
+        val t = "34852"
+        val expected = true
+
+        assertEquals(expected, CheckIfStringIsTransformableWithSubstringSortOperations.isTransformable(s, t))
+    }
+
+    @Test
+    fun test2() {
+        val s = "34521"
+        val t = "23415"
+        val expected = true
+
+        assertEquals(expected, CheckIfStringIsTransformableWithSubstringSortOperations.isTransformable(s, t))
+    }
+
+    @Test
+    fun test3() {
+        val s = "12345"
+        val t = "12435"
+        val expected = false
+
+        assertEquals(expected, CheckIfStringIsTransformableWithSubstringSortOperations.isTransformable(s, t))
+    }
+}
