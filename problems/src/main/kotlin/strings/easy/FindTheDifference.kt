6b75a1ae5f98eb8b3682faee4165eb19afadba0f
ikss <iksss.88@gmail.com>
2023-09-25 11:45:06 +0200

    Task: 389. Find the Difference (use new Kotlin functions in xor variant)

--- problems/src/main/kotlin/strings/easy/FindTheDifference.kt
+++ problems/src/main/kotlin/strings/easy/FindTheDifference.kt
@@ -10,6 +10,7 @@ package strings.easy
  * [URL](https://leetcode.com/problems/find-the-difference/)
  */
 object FindTheDifference {
+
     fun findTheDifference(s: String, t: String): Char {
         val hash1 = getHash(s)
         val hash2 = getHash(t)
@@ -30,5 +31,16 @@ object FindTheDifference {
         }
 
         return hash
+    }
+
+    fun findTheDifferenceXor(s: String, t: String): Char {
+        var c = 0
+
+        for (i in s.indices) {
+            c = c xor (s[i].code) xor (t[i].code)
+        }
+
+        c = c xor (t[t.length - 1].code)
+        return c.toChar()
     }
 }
--- problems/src/test/kotlin/strings/easy/FindTheDifferenceTest.kt
+++ problems/src/test/kotlin/strings/easy/FindTheDifferenceTest.kt
@@ -21,4 +21,21 @@ class FindTheDifferenceTest {
 
         assertEquals(expected, FindTheDifference.findTheDifference(s, t))
     }
+    @Test
+    fun test1xor() {
+        val s = "abcd"
+        val t = "abcde"
+        val expected = 'e'
+
+        assertEquals(expected, FindTheDifference.findTheDifferenceXor(s, t))
+    }
+
+    @Test
+    fun test2xor() {
+        val s = ""
+        val t = "y"
+        val expected = 'y'
+
+        assertEquals(expected, FindTheDifference.findTheDifferenceXor(s, t))
+    }
 }
