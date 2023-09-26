32388e8122b4aa3a04e8a5bd2754cbd780eb9adb
ikss <iksss.88@gmail.com>
2023-09-26 09:10:07 +0200

    316. Remove Duplicate Letters

---
+++ problems/src/test/kotlin/strings/medium/RemoveDuplicateLettersTest.kt
@@ -0,0 +1,22 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class RemoveDuplicateLettersTest {
+    @Test
+    fun test1() {
+        val s = "bcabc"
+        val expected = "abc"
+
+        assertEquals(expected, RemoveDuplicateLetters.removeDuplicateLetters(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "cbacdcbc"
+        val expected = "acdb"
+
+        assertEquals(expected, RemoveDuplicateLetters.removeDuplicateLetters(s))
+    }
+}
