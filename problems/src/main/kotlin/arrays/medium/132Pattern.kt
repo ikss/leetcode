367bcc109dc46910b29228c5b0eb09d3ef884a81
ikss <iksss.88@gmail.com>
2023-10-01 16:29:04 +0200

    Task: 456. 132 Pattern

---
+++ problems/src/main/kotlin/arrays/medium/132Pattern.kt
@@ -0,0 +1,36 @@
+package arrays.medium
+
+import kotlin.math.min
+
+/**
+ * Given an array of n integers nums, a 132 pattern is a subsequence of three integers `nums[i]`,`nums[j]` and `nums[k]`
+ * such that i < j < k and `nums[i] < nums[k] < nums[j]`.
+ *
+ * Return true if there is a 132 pattern in nums, otherwise, return false.
+ *
+ * [URL](https://leetcode.com/problems/132-pattern/)
+ */
+object `132Pattern` {
+    fun find132pattern(nums: IntArray): Boolean {
+        if (nums.size < 3) return false
+        
+        val min = IntArray(nums.size)
+        min[0] = nums[0]
+        for (i in 1 until nums.size) {
+            min[i] = min(min[i - 1], nums[i])
+        }
+        var j = nums.size - 1
+        var k = nums.size
+        while (j >= 0) {
+            if (nums[j] > min[j]) {
+                while (k < nums.size && nums[k] <= min[j]) k++
+                if (k < nums.size && nums[k] < nums[j]) {
+                    return true
+                }
+                nums[--k] = nums[j]
+            }
+            j--
+        }
+        return false
+    }
+}
