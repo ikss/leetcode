4324581d1328665a5570fc261f16a5361f277c70
ikss <iksss.88@gmail.com>
2023-10-02 08:56:11 +0200

    Task: 2038. Remove Colored Pieces if Both Neighbors are the Same Color

---
+++ problems/src/test/kotlin/strings/medium/RemoveColoredPiecesIfBothNeighborsAreTheSameColorTest.kt
@@ -0,0 +1,30 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class RemoveColoredPiecesIfBothNeighborsAreTheSameColorTest {
+    @Test
+    fun test1() {
+        val colors = "AAABABB"
+        val expected = true
+
+        assertEquals(expected, RemoveColoredPiecesIfBothNeighborsAreTheSameColor.winnerOfGame(colors))
+    }
+
+    @Test
+    fun test2() {
+        val colors = "AA"
+        val expected = false
+
+        assertEquals(expected, RemoveColoredPiecesIfBothNeighborsAreTheSameColor.winnerOfGame(colors))
+    }
+
+    @Test
+    fun test3() {
+        val colors = "ABBBBBBBAAA"
+        val expected = false
+
+        assertEquals(expected, RemoveColoredPiecesIfBothNeighborsAreTheSameColor.winnerOfGame(colors))
+    }
+}
