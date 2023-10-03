48700a7b29df2d25ea644db48830c204dfbccd35
ikss <iksss.88@gmail.com>
2023-10-03 09:36:30 +0200

    Task: 1512. Number of Good Pairs

---
+++ problems/src/test/kotlin/arrays/easy/NumberOfGoodPairsTest.kt
@@ -0,0 +1,78 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NumberOfGoodPairsTest {
+    @Test
+    fun test1() {
+        val input = intArrayOf(1, 2, 3, 1, 1, 3)
+        val expected = 4
+
+        assertEquals(expected, NumberOfGoodPairs.numIdenticalPairs(input))
+    }
+
+    @Test
+    fun test2() {
+        val input = intArrayOf(1, 1, 1, 1)
+        val expected = 6
+
+        assertEquals(expected, NumberOfGoodPairs.numIdenticalPairs(input))
+    }
+
+    @Test
+    fun test3() {
+        val input = intArrayOf(1, 2, 3)
+        val expected = 0
+
+        assertEquals(expected, NumberOfGoodPairs.numIdenticalPairs(input))
+    }
+
+    @Test
+    fun test1array() {
+        val input = intArrayOf(1, 2, 3, 1, 1, 3)
+        val expected = 4
+
+        assertEquals(expected, NumberOfGoodPairs.numIdenticalPairsArray(input))
+    }
+
+    @Test
+    fun test2array() {
+        val input = intArrayOf(1, 1, 1, 1)
+        val expected = 6
+
+        assertEquals(expected, NumberOfGoodPairs.numIdenticalPairsArray(input))
+    }
+
+    @Test
+    fun test3array() {
+        val input = intArrayOf(1, 2, 3)
+        val expected = 0
+
+        assertEquals(expected, NumberOfGoodPairs.numIdenticalPairsArray(input))
+    }
+
+    @Test
+    fun test1map() {
+        val input = intArrayOf(1, 2, 3, 1, 1, 3)
+        val expected = 4
+
+        assertEquals(expected, NumberOfGoodPairs.numIdenticalPairsMap(input))
+    }
+
+    @Test
+    fun test2map() {
+        val input = intArrayOf(1, 1, 1, 1)
+        val expected = 6
+
+        assertEquals(expected, NumberOfGoodPairs.numIdenticalPairsMap(input))
+    }
+
+    @Test
+    fun test3map() {
+        val input = intArrayOf(1, 2, 3)
+        val expected = 0
+
+        assertEquals(expected, NumberOfGoodPairs.numIdenticalPairsMap(input))
+    }
+}
