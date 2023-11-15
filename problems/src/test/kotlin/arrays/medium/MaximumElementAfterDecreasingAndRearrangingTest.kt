e65bc0a5c675781968c1dbe9ef8662952e810b7a
ikss <iksss.88@gmail.com>
2023-11-15 06:57:55 +0100

    Task: 1846. Maximum Element After Decreasing and Rearranging

---
+++ problems/src/test/kotlin/arrays/medium/MaximumElementAfterDecreasingAndRearrangingTest.kt
@@ -0,0 +1,39 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumElementAfterDecreasingAndRearrangingTest {
+    @Test
+    fun test1() {
+        val arr = intArrayOf(2, 2, 1, 2, 1)
+        val expected = 2
+
+        assertEquals(
+            expected,
+            MaximumElementAfterDecreasingAndRearranging.maximumElementAfterDecrementingAndRearranging(arr)
+        )
+    }
+
+    @Test
+    fun test2() {
+        val arr = intArrayOf(100, 1, 1000)
+        val expected = 3
+
+        assertEquals(
+            expected,
+            MaximumElementAfterDecreasingAndRearranging.maximumElementAfterDecrementingAndRearranging(arr)
+        )
+    }
+
+    @Test
+    fun test3() {
+        val arr = intArrayOf(1, 2, 3, 4, 5)
+        val expected = 5
+
+        assertEquals(
+            expected,
+            MaximumElementAfterDecreasingAndRearranging.maximumElementAfterDecrementingAndRearranging(arr)
+        )
+    }
+}
