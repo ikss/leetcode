b7ae97d889a0ec578648173a03704c0f3c320120
ikss <iksss.88@gmail.com>
2023-11-16 09:26:10 +0100

    Task: 1980. Find Unique Binary String

---
+++ problems/src/test/kotlin/strings/medium/FindUniqueBinaryStringTest.kt
@@ -0,0 +1,30 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindUniqueBinaryStringTest {
+    @Test
+    fun test1() {
+        val nums = arrayOf("01", "10")
+        val expected = "11"
+
+        assertEquals(expected, FindUniqueBinaryString.findDifferentBinaryString(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = arrayOf("00", "01")
+        val expected = "11"
+
+        assertEquals(expected, FindUniqueBinaryString.findDifferentBinaryString(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = arrayOf("111", "011", "001")
+        val expected = "101"
+
+        assertEquals(expected, FindUniqueBinaryString.findDifferentBinaryString(nums))
+    }
+}
