70ff4ae376c2b7bd89e8b0c022b843a5806a5724
ikss <iksss.88@gmail.com>
2023-11-25 09:00:28 +0100

    Task: 1685. Sum of Absolute Differences in a Sorted Array

---
+++ problems/src/test/kotlin/arrays/medium/SumOfAbsoluteDifferencesInSortedArrayTest.kt
@@ -0,0 +1,22 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class SumOfAbsoluteDifferencesInSortedArrayTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(2, 3, 5)
+        val expected = intArrayOf(4, 3, 5)
+
+        assertArrayEquals(expected, SumOfAbsoluteDifferencesInSortedArray.getSumAbsoluteDifferences(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(1, 4, 6, 8, 10)
+        val expected = intArrayOf(24, 15, 13, 15, 21)
+
+        assertArrayEquals(expected, SumOfAbsoluteDifferencesInSortedArray.getSumAbsoluteDifferences(nums))
+    }
+}
