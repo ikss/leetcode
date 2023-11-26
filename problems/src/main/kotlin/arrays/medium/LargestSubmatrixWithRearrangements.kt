9ee998121abbd9072aef97bc31214ce48318ef17
ikss <iksss.88@gmail.com>
2023-11-26 09:45:07 +0100

    Task: 1727. Largest Submatrix With Rearrangements

---
+++ problems/src/main/kotlin/arrays/medium/LargestSubmatrixWithRearrangements.kt
@@ -0,0 +1,31 @@
+package arrays.medium
+
+
+/**
+ * You are given a binary matrix _matrix_ of size m x n, and you are allowed to rearrange the columns
+ * of the matrix in any order.
+ *
+ * Return the area of the largest submatrix within matrix where every element of the submatrix
+ * is 1 after reordering the columns optimally.
+ *
+ * [URL](https://leetcode.com/problems/largest-submatrix-with-rearrangements/)
+ */
+object LargestSubmatrixWithRearrangements {
+    fun largestSubmatrix(matrix: Array<IntArray>): Int {
+        val m = matrix.size
+        val n = matrix[0].size
+        var ans = 0
+        for (row in 0 until m) {
+            for (col in 0 until n) {
+                if (matrix[row][col] != 0 && row > 0) {
+                    matrix[row][col] += matrix[row - 1][col]
+                }
+            }
+            val currRow = matrix[row].sortedDescending()
+            for (i in 0 until n) {
+                ans = maxOf(ans, currRow[i] * (i + 1))
+            }
+        }
+        return ans
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/LargestSubmatrixWithRearrangementsTest.kt
@@ -0,0 +1,39 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LargestSubmatrixWithRearrangementsTest {
+    @Test
+    fun test1() {
+        val matrix = arrayOf(
+            intArrayOf(0, 0, 1),
+            intArrayOf(1, 1, 1),
+            intArrayOf(1, 0, 1),
+        )
+        val expected = 4
+
+        assertEquals(expected, LargestSubmatrixWithRearrangements.largestSubmatrix(matrix))
+    }
+
+    @Test
+    fun test2() {
+        val matrix = arrayOf(
+            intArrayOf(1, 0, 1, 0, 1),
+        )
+        val expected = 3
+
+        assertEquals(expected, LargestSubmatrixWithRearrangements.largestSubmatrix(matrix))
+    }
+
+    @Test
+    fun test3() {
+        val matrix = arrayOf(
+            intArrayOf(1, 1, 0),
+            intArrayOf(1, 0, 1),
+        )
+        val expected = 2
+
+        assertEquals(expected, LargestSubmatrixWithRearrangements.largestSubmatrix(matrix))
+    }
+}
