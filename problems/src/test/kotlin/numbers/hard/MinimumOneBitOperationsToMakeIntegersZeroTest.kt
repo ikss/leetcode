500de96d9b0f4f78bd727512e64ac3f26166c4c7
ikss <iksss.88@gmail.com>
2023-11-30 09:44:36 +0100

    Task: 1611. Minimum One Bit Operations to Make Integers Zero

---
+++ problems/src/test/kotlin/numbers/hard/MinimumOneBitOperationsToMakeIntegersZeroTest.kt
@@ -0,0 +1,22 @@
+package numbers.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumOneBitOperationsToMakeIntegersZeroTest {
+    @Test
+    fun test1() {
+        val n = 3
+        val expected = 2
+
+        assertEquals(expected, MinimumOneBitOperationsToMakeIntegersZero.minimumOneBitOperations(n))
+    }
+
+    @Test
+    fun test2() {
+        val n = 6
+        val expected = 4
+
+        assertEquals(expected, MinimumOneBitOperationsToMakeIntegersZero.minimumOneBitOperations(n))
+    }
+}
