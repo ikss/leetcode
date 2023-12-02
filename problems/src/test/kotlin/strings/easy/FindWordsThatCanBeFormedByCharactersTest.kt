28865c61d62eeca9550fffdffbb17e48882bfad2
ikss <iksss.88@gmail.com>
2023-12-02 09:23:42 +0100

    Task: 1160. Find Words That Can Be Formed by Characters

---
+++ problems/src/test/kotlin/strings/easy/FindWordsThatCanBeFormedByCharactersTest.kt
@@ -0,0 +1,24 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindWordsThatCanBeFormedByCharactersTest {
+    @Test
+    fun test1() {
+        val words = arrayOf("cat", "bt", "hat", "tree")
+        val chars = "atach"
+        val expected = 6
+
+        assertEquals(expected, FindWordsThatCanBeFormedByCharacters.countCharacters(words, chars))
+    }
+
+    @Test
+    fun test2() {
+        val words = arrayOf("hello", "world", "leetcode")
+        val chars = "welldonehoneyr"
+        val expected = 10
+
+        assertEquals(expected, FindWordsThatCanBeFormedByCharacters.countCharacters(words, chars))
+    }
+}
