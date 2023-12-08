e062002d58a4a7c6308bf223a8e97abf66c6a739
ikss <iksss.88@gmail.com>
2023-12-08 17:35:53 +0100

    Task: 1120. Maximum Average Subtree

---
+++ problems/src/main/kotlin/trees/medium/MaximumAverageSubtree.kt
@@ -0,0 +1,36 @@
+package trees.medium
+
+import data_structures.TreeNode
+
+/**
+ * Given the root of a binary tree, return the maximum average value of a subtree of that tree.
+ * Answers within 10-5 of the actual answer will be accepted.
+ *
+ * A subtree of a tree is any node of that tree plus all its descendants.
+ *
+ * The average value of a tree is the sum of its values, divided by the number of nodes.
+ *
+ * [URL](https://leetcode.com/problems/maximum-average-subtree/)
+ */
+object MaximumAverageSubtree {
+    private var result = 0.0
+
+    fun maximumAverageSubtree(root: TreeNode?): Double {
+        result = 0.0
+        traverse(root)
+        return result
+    }
+
+    private fun traverse(root: TreeNode?): Pair<Double, Int> {
+        if (root == null) return 0.0 to 0
+        val (left, countLeft) = traverse(root.left)
+        val (right, countRight) = traverse(root.right)
+
+        val curr = left + right + root.`val`
+        val count = countLeft + countRight + 1
+
+        result = maxOf(result, curr / count)
+
+        return curr to count
+    }
+}
---
+++ problems/src/test/kotlin/trees/medium/MaximumAverageSubtreeTest.kt
@@ -0,0 +1,23 @@
+package trees.medium
+
+import data_structures.TreeBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumAverageSubtreeTest {
+    @Test
+    fun test1() {
+        val root = TreeBuilder.build(5, 6, 1)
+        val expected = 6.0
+
+        assertEquals(expected, MaximumAverageSubtree.maximumAverageSubtree(root), 0.0001)
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeBuilder.build(0, null, 1)
+        val expected = 1.0
+
+        assertEquals(expected, MaximumAverageSubtree.maximumAverageSubtree(root), 0.0001)
+    }
+}
