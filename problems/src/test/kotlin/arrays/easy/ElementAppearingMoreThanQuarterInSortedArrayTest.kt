720732895a6f779585b347d8426c51d34778e47b
ikss <iksss.88@gmail.com>
2023-12-11 11:06:47 +0100

    Task: 1287. Element Appearing More Than 25% In Sorted Array

---
+++ problems/src/test/kotlin/arrays/easy/ElementAppearingMoreThanQuarterInSortedArrayTest.kt
@@ -0,0 +1,22 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ElementAppearingMoreThanQuarterInSortedArrayTest {
+    @Test
+    fun test1() {
+        val arr = intArrayOf(1, 2, 2, 6, 6, 6, 6, 7, 10)
+        val expected = 6
+
+        assertEquals(expected, ElementAppearingMoreThanQuarterInSortedArray.findSpecialInteger(arr))
+    }
+
+    @Test
+    fun test2() {
+        val arr = intArrayOf(1, 1)
+        val expected = 1
+
+        assertEquals(expected, ElementAppearingMoreThanQuarterInSortedArray.findSpecialInteger(arr))
+    }
+}
