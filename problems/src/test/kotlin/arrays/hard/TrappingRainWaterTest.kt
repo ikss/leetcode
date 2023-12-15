92d1d0908bad9836287aa1cb9fefb9caecdfa869
ikss <iksss.88@gmail.com>
2023-12-15 14:34:23 +0100

    Task: 42. Trapping Rain Water (two pointers simplified and test)

--- problems/src/test/kotlin/arrays/hard/TrappingRainWaterTest.kt
+++ problems/src/test/kotlin/arrays/hard/TrappingRainWaterTest.kt
@@ -22,6 +22,14 @@ class TrappingRainWaterTest {
     }
 
     @Test
+    fun test3() {
+        val height = intArrayOf(4, 0, 0, 0, 0, 0)
+        val expected = 0
+
+        assertEquals(expected, TrappingRainWater.trap(height))
+    }
+
+    @Test
     fun test1twoPointers() {
         val height = intArrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)
         val expected = 6
@@ -33,6 +41,14 @@ class TrappingRainWaterTest {
     fun test2twoPointers() {
         val height = intArrayOf(4, 2, 0, 3, 2, 5)
         val expected = 9
+
+        assertEquals(expected, TrappingRainWater.trapTwoPointers(height))
+    }
+
+    @Test
+    fun test3twoPointers() {
+        val height = intArrayOf(4, 0, 0, 0, 0, 0)
+        val expected = 0
 
         assertEquals(expected, TrappingRainWater.trapTwoPointers(height))
     }
