02ffed1968d272d56a247697e45f955d194f283c
ikss <iksss.88@gmail.com>
2023-12-17 09:51:20 +0100

    Task: 2353. Design a Food Rating System

---
+++ problems/src/test/kotlin/design/medium/DesignFoodRatingSystemTest.kt
@@ -0,0 +1,30 @@
+package design.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class DesignFoodRatingSystemTest {
+    @Test
+    fun test1() {
+        val foodRatings = DesignFoodRatingSystem.FoodRatings(
+            arrayOf("kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"),
+            arrayOf("korean", "japanese", "japanese", "greek", "japanese", "korean"),
+            intArrayOf(9, 12, 8, 15, 14, 7),
+        )
+        assertEquals("kimchi", foodRatings.highestRated("korean")) // return "kimchi"
+        // "kimchi" is the highest rated korean food with a rating of 9.
+
+        assertEquals("ramen", foodRatings.highestRated("japanese")) // return "ramen"
+        // "ramen" is the highest rated japanese food with a rating of 14.
+
+        foodRatings.changeRating("sushi", 16) // "sushi" now has a rating of 16.
+        assertEquals("sushi", foodRatings.highestRated("japanese")) // return "sushi"
+        // "sushi" is the highest rated japanese food with a rating of 16.
+
+        foodRatings.changeRating("ramen", 16) // "ramen" now has a rating of 16.
+
+        assertEquals("ramen", foodRatings.highestRated("japanese")) // return "ramen"
+        // Both "sushi" and "ramen" have a rating of 16.
+        // However, "ramen" is lexicographically smaller than "sushi".
+    }
+}
