6a87a9a8814ad616d4f910808b9770f7c81e3021
ikss <iksss.88@gmail.com>
2023-12-18 13:13:18 +0100

    Task: 1913. Maximum Product Difference Between Two Pairs

---
+++ problems/src/test/kotlin/arrays/easy/MaximumProductDifferenceBetweenTwoPairsTest.kt
@@ -0,0 +1,22 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumProductDifferenceBetweenTwoPairsTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(5, 6, 2, 7, 4)
+        val expected = 34
+
+        assertEquals(expected, MaximumProductDifferenceBetweenTwoPairs.maxProductDifference(nums))
+    }
+    
+    @Test
+    fun test2() {
+        val nums = intArrayOf(4, 2, 5, 9, 7, 4, 8)
+        val expected = 64
+
+        assertEquals(expected, MaximumProductDifferenceBetweenTwoPairs.maxProductDifference(nums))
+    }
+}
