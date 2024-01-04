cb81f5e85ba916c38b3692f3e213f0c81fa5bb06
ikss <iksss.88@gmail.com>
2024-01-04 07:39:56 +0100

    Task: 2870. Minimum Number of Operations to Make Array Empty

---
+++ problems/src/test/kotlin/arrays/medium/MinimumNumberOfOperationsToMakeArrayEmptyTest.kt
@@ -0,0 +1,22 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumNumberOfOperationsToMakeArrayEmptyTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(2, 3, 3, 2, 2, 4, 2, 3, 4)
+        val expected = 4
+
+        assertEquals(expected, MinimumNumberOfOperationsToMakeArrayEmpty.minOperations(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(2, 1, 2, 2, 3, 3)
+        val expected = -1
+
+        assertEquals(expected, MinimumNumberOfOperationsToMakeArrayEmpty.minOperations(nums))
+    }
+}
