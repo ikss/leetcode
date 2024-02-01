43da83bce17d8bd11706740404ae438d4859585f
ikss <iksss.88@gmail.com>
2024-02-01 10:02:37 +0100

    Task: 2966. Divide Array Into Arrays With Max Difference

---
+++ problems/src/main/kotlin/arrays/medium/DivideArrayIntoArraysWithMaxDifference.kt
@@ -0,0 +1,29 @@
+package arrays.medium
+
+/**
+ * You are given an integer array nums of size n and a positive integer k.
+ *
+ * Divide the array into one or more arrays of size 3 satisfying the following conditions:
+ *
+ * Each element of nums should be in exactly one array.
+ * The difference between any two elements in one array is less than or equal to k.
+ * Return a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array.
+ * And if there are multiple answers, return any of them.
+ *
+ * [URL](https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/)
+ */
+object DivideArrayIntoArraysWithMaxDifference {
+    fun divideArray(nums: IntArray, k: Int): Array<IntArray> {
+        val result = ArrayList<IntArray>(nums.size / 3)
+        nums.sort()
+
+        var i = 0
+        while (i < nums.size) {
+            if (nums[i + 2] - nums[i] > k) return emptyArray()
+            result.add(intArrayOf(nums[i], nums[i + 1], nums[i + 2]))
+            i += 3
+        }
+
+        return result.toTypedArray()
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/DivideArrayIntoArraysWithMaxDifferenceTest.kt
@@ -0,0 +1,24 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class DivideArrayIntoArraysWithMaxDifferenceTest {
+    @Test
+    fun test1() {
+        val input = intArrayOf(1, 3, 4, 8, 7, 9, 3, 5, 1)
+        val k = 2
+        val expected = arrayOf(intArrayOf(1, 1, 3), intArrayOf(3, 4, 5), intArrayOf(7, 8, 9))
+
+        assertArrayEquals(expected, DivideArrayIntoArraysWithMaxDifference.divideArray(input, k))
+    }
+
+    @Test
+    fun test2() {
+        val input = intArrayOf(1, 3, 3, 2, 7, 3)
+        val k = 3
+        val expected = emptyArray<IntArray>()
+
+        assertArrayEquals(expected, DivideArrayIntoArraysWithMaxDifference.divideArray(input, k))
+    }
+}
