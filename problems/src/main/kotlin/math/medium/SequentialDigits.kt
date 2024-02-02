6d60f5831035ede652a3e756c96ef22287d83391
ikss <iksss.88@gmail.com>
2024-02-02 11:30:38 +0100

    Task: 1291. Sequential Digits

---
+++ problems/src/main/kotlin/math/medium/SequentialDigits.kt
@@ -0,0 +1,52 @@
+package math.medium
+
+
+/**
+ * An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+ *
+ * Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+ *
+ * [URL](https://leetcode.com/problems/sequential-digits/)
+ */
+object SequentialDigits {
+    fun sequentialDigitsOwn(low: Int, high: Int): List<Int> {
+        val result = ArrayList<Int>()
+
+        var start = 1
+        var next = start
+        var curr = start
+        while (true) {
+            if (start == 10) break
+            if (curr in low..high) {
+                result.add(curr)
+            }
+            if (++next == 10 || curr > high) {
+                next = ++start
+                curr = start
+                continue
+            }
+            curr = curr * 10 + next
+        }
+
+        return result.sorted()
+    }
+
+    fun sequentialDigitsSlidingWindow(low: Int, high: Int): List<Int> {
+        val sample = "123456789"
+        val n = 10
+        val nums = ArrayList<Int>()
+
+        val lowLen = low.toString().length
+        val highLen = high.toString().length
+
+        for (length in lowLen..highLen) {
+            for (start in 0 until n - length) {
+                val num = sample.substring(start, start + length).toInt()
+                if (num in low..high) {
+                    nums.add(num)
+                }
+            }
+        }
+        return nums
+    }
+}
---
+++ problems/src/test/kotlin/math/medium/SequentialDigitsTest.kt
@@ -0,0 +1,43 @@
+package math.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SequentialDigitsTest {
+    @Test
+    fun test1own() {
+        val low = 100
+        val high = 300
+        val expected = listOf(123, 234)
+
+        assertEquals(expected, SequentialDigits.sequentialDigitsOwn(low, high))
+    }
+
+    @Test
+    fun test2own() {
+        val low = 1000
+        val high = 13000
+        val expected = listOf(1234, 2345, 3456, 4567, 5678, 6789, 12345)
+
+        assertEquals(expected, SequentialDigits.sequentialDigitsOwn(low, high))
+    }
+
+    @Test
+    fun test1slidingWindow() {
+        val low = 100
+        val high = 300
+        val expected = listOf(123, 234)
+
+        assertEquals(expected, SequentialDigits.sequentialDigitsSlidingWindow(low, high))
+    }
+
+    @Test
+    fun test2slidingWindow() {
+        val low = 1000
+        val high = 13000
+        val expected = listOf(1234, 2345, 3456, 4567, 5678, 6789, 12345)
+
+        assertEquals(expected, SequentialDigits.sequentialDigitsSlidingWindow(low, high))
+    }
+
+}
