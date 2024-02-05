844c61841c1395038042740d8022c3295639c61d
ikss <iksss.88@gmail.com>
2024-02-05 10:24:35 +0100

    Task: 387. First Unique Character in a String

---
+++ problems/src/main/kotlin/strings/easy/FirstUniqueCharacterInAString.kt
@@ -0,0 +1,22 @@
+package strings.easy
+
+/**
+ * Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+ *
+ * [URL](https://leetcode.com/problems/first-unique-character-in-a-string/)
+ */
+object FirstUniqueCharacterInAString {
+    fun firstUniqChar(s: String): Int {
+        val count = IntArray(26)
+
+        for (c in s) {
+            count[c - 'a']++
+        }
+
+        for (i in s.indices) {
+            if (count[s[i] - 'a'] == 1) return i
+        }
+
+        return -1
+    }
+}
