d54f8361252a6e325f5ab83fc7f82a4f34afd549
ikss <iksss.88@gmail.com>
2024-02-08 17:26:43 +0100

    Task: 276. Paint Fence (unused import)

--- problems/src/main/kotlin/arrays/medium/PaintFence.kt
+++ problems/src/main/kotlin/arrays/medium/PaintFence.kt
@@ -1,7 +1,5 @@
 package arrays.medium
 
-import java.util.*
-
 /**
  * You are painting a fence of n posts with k different colors. You must paint the posts following these rules:
  * * Every post must be painted exactly one color.
