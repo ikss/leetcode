fc81bedd1df0f93cbf2480ebe3537123079126b5
ikss <iksss.88@gmail.com>
2024-02-09 12:02:00 +0100

    Task: 368. Largest Divisible Subset

---
+++ problems/src/test/kotlin/arrays/medium/LargestDivisibleSubsetTest.kt
@@ -0,0 +1,30 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LargestDivisibleSubsetTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, 2, 3)
+        val expected = listOf(1, 2)
+
+        assertEquals(expected, LargestDivisibleSubset.largestDivisibleSubset(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(1, 2, 4, 8)
+        val expected = listOf(1, 2, 4, 8)
+
+        assertEquals(expected, LargestDivisibleSubset.largestDivisibleSubset(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(1)
+        val expected = listOf(1)
+
+        assertEquals(expected, LargestDivisibleSubset.largestDivisibleSubset(nums))
+    }
+}
