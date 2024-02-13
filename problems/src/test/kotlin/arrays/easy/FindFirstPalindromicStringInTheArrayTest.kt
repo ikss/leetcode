bb928e818ea3a4368e55db744270a0953a36f571
ikss <iksss.88@gmail.com>
2024-02-13 10:16:20 +0100

    Task: 2108. Find First Palindromic String in the Array

---
+++ problems/src/test/kotlin/arrays/easy/FindFirstPalindromicStringInTheArrayTest.kt
@@ -0,0 +1,30 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindFirstPalindromicStringInTheArrayTest {
+    @Test
+    fun test1() {
+        val words = arrayOf("abc", "car", "ada", "racecar", "cool")
+        val expected = "ada"
+
+        assertEquals(expected, FindFirstPalindromicStringInTheArray.firstPalindrome(words))
+    }
+
+    @Test
+    fun test2() {
+        val words = arrayOf("notapalindrome", "racecar")
+        val expected = "racecar"
+
+        assertEquals(expected, FindFirstPalindromicStringInTheArray.firstPalindrome(words))
+    }
+    
+    @Test
+    fun test3() {
+        val words = arrayOf("def","ghi")
+        val expected = ""
+
+        assertEquals(expected, FindFirstPalindromicStringInTheArray.firstPalindrome(words))
+    }
+}
