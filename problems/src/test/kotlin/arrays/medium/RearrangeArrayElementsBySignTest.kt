d37d8ca9084e8d8707210d719869d69d858efa63
ikss <iksss.88@gmail.com>
2024-02-14 12:03:14 +0100

    Task: 2149. Rearrange Array Elements by Sign

---
+++ problems/src/test/kotlin/arrays/medium/RearrangeArrayElementsBySignTest.kt
@@ -0,0 +1,22 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class RearrangeArrayElementsBySignTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(3, 1, -2, -5, 2, -4)
+        val expected = intArrayOf(3, -2, 1, -5, 2, -4)
+
+        assertArrayEquals(expected, RearrangeArrayElementsBySign.rearrangeArray(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(-1, 1)
+        val expected = intArrayOf(1, -1)
+
+        assertArrayEquals(expected, RearrangeArrayElementsBySign.rearrangeArray(nums))
+    }
+}
