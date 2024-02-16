793ed4f2e1682819c55aeee356ef918375d4f85e
ikss <iksss.88@gmail.com>
2024-02-16 09:21:53 +0100

    Task: 1481. Least Number of Unique Integers after K Removals (counting sort)

--- problems/src/main/kotlin/arrays/medium/LeastNumberOfUniqueIntegersAfterKRemovals.kt
+++ problems/src/main/kotlin/arrays/medium/LeastNumberOfUniqueIntegersAfterKRemovals.kt
@@ -7,7 +7,7 @@ package arrays.medium
  * [URL](https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/)
  */
 object LeastNumberOfUniqueIntegersAfterKRemovals {
-    fun findLeastNumOfUniqueInts(arr: IntArray, k: Int): Int {
+    fun findLeastNumOfUniqueIntsSortValues(arr: IntArray, k: Int): Int {
         val map = HashMap<Int, Int>()
 
         for (n in arr) {
@@ -24,5 +24,36 @@ object LeastNumberOfUniqueIntegersAfterKRemovals {
             unique -= 1
         }
         return unique
+    }
+
+    fun findLeastNumOfUniqueIntsCountSort(arr: IntArray, k: Int): Int {
+        val map = HashMap<Int, Int>()
+
+        for (n in arr) {
+            map.merge(n, 1, Int::plus)
+        }
+
+        val n = arr.size
+        val countOfFrequencies = IntArray(n + 1)
+
+        for (count in map.values) {
+            countOfFrequencies[count]++
+        }
+
+        var unique = map.size
+        var left = k
+        for (i in 1..n) {
+            val numElementsToRemove = minOf(left / i, countOfFrequencies[i])
+
+            left -= i * numElementsToRemove
+
+            unique -= numElementsToRemove
+
+            if (left < i || unique == 0) {
+                return unique
+            }
+        }
+
+        return 0
     }
 }
--- problems/src/test/kotlin/arrays/medium/LeastNumberOfUniqueIntegersAfterKRemovalsTest.kt
+++ problems/src/test/kotlin/arrays/medium/LeastNumberOfUniqueIntegersAfterKRemovalsTest.kt
@@ -5,20 +5,37 @@ import org.junit.jupiter.api.Test
 
 class LeastNumberOfUniqueIntegersAfterKRemovalsTest {
     @Test
-    fun test1() {
+    fun test1sortValues() {
         val arr = intArrayOf(5, 5, 4)
         val k = 1
         val expected = 1
 
-        assertEquals(expected, LeastNumberOfUniqueIntegersAfterKRemovals.findLeastNumOfUniqueInts(arr, k))
+        assertEquals(expected, LeastNumberOfUniqueIntegersAfterKRemovals.findLeastNumOfUniqueIntsSortValues(arr, k))
     }
 
     @Test
-    fun test2() {
+    fun test2sortValues() {
         val arr = intArrayOf(4, 3, 1, 1, 3, 3, 2)
         val k = 3
         val expected = 2
 
-        assertEquals(expected, LeastNumberOfUniqueIntegersAfterKRemovals.findLeastNumOfUniqueInts(arr, k))
+        assertEquals(expected, LeastNumberOfUniqueIntegersAfterKRemovals.findLeastNumOfUniqueIntsSortValues(arr, k))
+    }
+    @Test
+    fun test1sortCountSort() {
+        val arr = intArrayOf(5, 5, 4)
+        val k = 1
+        val expected = 1
+
+        assertEquals(expected, LeastNumberOfUniqueIntegersAfterKRemovals.findLeastNumOfUniqueIntsCountSort(arr, k))
+    }
+
+    @Test
+    fun test2sortCountSort() {
+        val arr = intArrayOf(4, 3, 1, 1, 3, 3, 2)
+        val k = 3
+        val expected = 2
+
+        assertEquals(expected, LeastNumberOfUniqueIntegersAfterKRemovals.findLeastNumOfUniqueIntsCountSort(arr, k))
     }
 }
