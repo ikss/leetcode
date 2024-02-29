1ad44a542cb7c051d397ef520a85327a36c78b7e
ikss <iksss.88@gmail.com>
2024-02-29 13:28:22 +0100

    Task: 1973. Count Nodes Equal to Sum of Descendants

---
+++ problems/src/main/kotlin/trees/medium/CountNodesEqualToSumOfDescendants.kt
@@ -0,0 +1,36 @@
+package trees.medium
+
+import data_structures.TreeNode
+import java.util.*
+
+/**
+ * Given the root of a binary tree, return the number of nodes where the value of the node
+ * is equal to the sum of the values of its descendants.
+ *
+ * A descendant of a node x is any node that is on the path from node x to some leaf node.
+ * The sum is considered to be 0 if the node has no descendants.
+ *
+ * [URL](https://leetcode.com/problems/count-nodes-equal-to-sum-of-descendants/)
+ */
+object CountNodesEqualToSumOfDescendants {
+    private var result = 0
+
+    fun equalToDescendants(root: TreeNode?): Int {
+        result = 0
+        traverse(root)
+        return result
+    }
+
+    private fun traverse(node: TreeNode?): Int {
+        if (node == null) return 0
+
+        val left = traverse(node.left)
+        val right = traverse(node.right)
+
+        if (left + right == node.`val`) {
+            result++
+        }
+
+        return node.`val` + left + right
+    }
+}
