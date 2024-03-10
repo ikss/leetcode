fa244177e5a728f55c136305ff9dc7085de42d07
ikss <iksss.88@gmail.com>
2024-03-10 07:49:39 +0100

    Task: 349. Intersection of Two Arrays

---
+++ problems/src/main/kotlin/arrays/easy/IntersectionOfTwoArrays.kt
@@ -0,0 +1,18 @@
+package arrays.easy
+
+import java.util.*
+
+/**
+ * Given two integer arrays nums1 and nums2, return an array of their intersection.
+ * Each element in the result must be unique and you may return the result in any order.
+ *
+ * [URL](https://leetcode.com/problems/intersection-of-two-arrays/)
+ */
+object IntersectionOfTwoArrays {
+    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+        val set1 = nums1.toSet()
+        val set2 = nums2.toSet()
+
+        return set1.intersect(set2).toIntArray()
+    }
+}
