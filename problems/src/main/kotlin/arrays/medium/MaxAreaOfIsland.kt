6de28376675518c100501e3da21c78b4de8dea11
ikss <iksss.88@gmail.com>
2024-03-28 10:02:17 +0100

    Task: 695. Max Area of Island (bfs using queue)

--- problems/src/main/kotlin/arrays/medium/MaxAreaOfIsland.kt
+++ problems/src/main/kotlin/arrays/medium/MaxAreaOfIsland.kt
@@ -11,8 +11,9 @@ package arrays.medium
  * [URL](https://leetcode.com/problems/max-area-of-island/)
  */
 object MaxAreaOfIsland {
+    private val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
 
-    fun maxAreaOfIsland(grid: Array<IntArray>): Int {
+    fun maxAreaOfIslandDfs(grid: Array<IntArray>): Int {
         val seen = Array(grid.size) { BooleanArray(grid[0].size) }
         var ans = 0
         for (r in grid.indices) {
@@ -24,7 +25,7 @@ object MaxAreaOfIsland {
     }
 
     private fun area(r: Int, c: Int, seen: Array<BooleanArray>, grid: Array<IntArray>): Int {
-        if (r < 0 || r >= grid.size || c < 0 || c >= grid[0].size || seen[r][c] || grid[r][c] == 0) {
+        if (r !in grid.indices || c !in grid[0].indices || seen[r][c] || grid[r][c] == 0) {
             return 0
         }
         seen[r][c] = true
@@ -33,5 +34,39 @@ object MaxAreaOfIsland {
         val left = area(r, c - 1, seen, grid)
         val right = area(r, c + 1, seen, grid)
         return (1 + down + up + left + right)
+    }
+
+
+    fun maxAreaOfIslandBfs(grid: Array<IntArray>): Int {
+        val m = grid.size
+        val n = grid[0].size
+        val visited = Array(m) { BooleanArray(n) }
+
+        var result = 0
+        val queue = java.util.ArrayDeque<Pair<Int, Int>>()
+        for (i in grid.indices) {
+            for (j in grid[0].indices) {
+                val n = grid[i][j]
+                if (n == 0 || visited[i][j]) continue
+
+                queue.offer(i to j)
+                var area = 0
+                while (queue.isNotEmpty()) {
+                    val (curri, currj) = queue.poll()
+                    if (curri !in grid.indices || currj !in grid[0].indices || grid[curri][currj] == 0 || visited[curri][currj]) continue
+                    area++
+                    visited[curri][currj] = true
+
+                    for ((di, dj) in directions) {
+                        val nexti = curri + di
+                        val nextj = currj + dj
+                        queue.offer(nexti to nextj)
+                    }
+                }
+                result = maxOf(result, area)
+            }
+        }
+
+        return result
     }
 }
