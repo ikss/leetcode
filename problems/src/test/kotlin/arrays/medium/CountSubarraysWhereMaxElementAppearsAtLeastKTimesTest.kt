8badc4d089d6f9e84bc43e95728ff6c3404a5bc3
ikss <iksss.88@gmail.com>
2024-03-29 09:32:49 +0100

    Task: 2962. Count Subarrays Where Max Element Appears at Least K Times

---
+++ problems/src/test/kotlin/arrays/medium/CountSubarraysWhereMaxElementAppearsAtLeastKTimesTest.kt
@@ -0,0 +1,24 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CountSubarraysWhereMaxElementAppearsAtLeastKTimesTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, 3, 2, 3, 3)
+        val k = 2
+        val expected = 6L
+
+        assertEquals(expected, CountSubarraysWhereMaxElementAppearsAtLeastKTimes.countSubarrays(nums, k))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(1, 4, 2, 1)
+        val k = 3 
+        val expected = 0L
+
+        assertEquals(expected, CountSubarraysWhereMaxElementAppearsAtLeastKTimes.countSubarrays(nums, k))
+    }
+}
