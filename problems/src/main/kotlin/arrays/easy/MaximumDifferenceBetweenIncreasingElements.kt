d5d5c39f4a68b4d774c6f6ccfb2df51a27275f36
ikss <iksss.88@gmail.com>
2024-04-03 14:04:54 +0200

    Task: 2016. Maximum Difference Between Increasing Elements

---
+++ problems/src/main/kotlin/arrays/easy/MaximumDifferenceBetweenIncreasingElements.kt
@@ -0,0 +1,27 @@
+package arrays.easy
+
+/**
+ * Given a 0-indexed integer array nums of size n, find the maximum difference between `nums[i]` and `nums[j]`
+ * (i.e., `nums[j] - nums[i]`), such that 0 <= i < j < n and `nums[i] < nums[j]`.
+ *
+ * Return the maximum difference. If no such i and j exists, return -1.
+ *
+ * [URL](https://leetcode.com/problems/maximum-difference-between-increasing-elements/)
+ */
+object MaximumDifferenceBetweenIncreasingElements {
+    fun maximumDifference(nums: IntArray): Int {
+        var result = -1
+        var min = nums[0]
+
+        for (i in 1 until nums.size) {
+            val n = nums[i]
+            if (n > min) {
+                result = maxOf(result, n - min)
+            } else {
+                min = n
+            }
+        }
+        
+        return result
+    }
+}
---
+++ problems/src/test/kotlin/arrays/easy/MaximumDifferenceBetweenIncreasingElementsTest.kt
@@ -0,0 +1,30 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumDifferenceBetweenIncreasingElementsTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(7, 1, 5, 4)
+        val expected = 4
+
+        assertEquals(expected, MaximumDifferenceBetweenIncreasingElements.maximumDifference(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(9, 4, 3, 2)
+        val expected = -1
+
+        assertEquals(expected, MaximumDifferenceBetweenIncreasingElements.maximumDifference(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(1, 5, 2, 10)
+        val expected = 9
+
+        assertEquals(expected, MaximumDifferenceBetweenIncreasingElements.maximumDifference(nums))
+    }
+}
