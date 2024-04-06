38a59ba6c8b93e51152392e665e13317f6858d81
ikss <iksss.88@gmail.com>
2024-04-06 23:15:22 +0200

    Task: 1249. Minimum Remove to Make Valid Parentheses

---
+++ problems/src/test/kotlin/strings/medium/MinimumRemoveToMakeValidParenthesesTest.kt
@@ -0,0 +1,30 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumRemoveToMakeValidParenthesesTest {
+    @Test
+    fun test1() {
+        val s = "lee(t(c)o)de)"
+        val expected = "lee(t(c)o)de"
+
+        assertEquals(expected, MinimumRemoveToMakeValidParentheses.minRemoveToMakeValid(s))
+    }
+    
+    @Test
+    fun test2() {
+        val s = "a)b(c)d"
+        val expected = "ab(c)d"
+
+        assertEquals(expected, MinimumRemoveToMakeValidParentheses.minRemoveToMakeValid(s))
+    }
+    
+    @Test
+    fun test3() {
+        val s = "))(("
+        val expected = ""
+
+        assertEquals(expected, MinimumRemoveToMakeValidParentheses.minRemoveToMakeValid(s))
+    }
+}
