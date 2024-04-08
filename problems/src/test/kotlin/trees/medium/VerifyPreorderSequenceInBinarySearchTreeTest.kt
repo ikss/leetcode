dcd96c3d66367c678fcf9d6b08ebc4ef197fa90c
ikss <iksss.88@gmail.com>
2024-04-08 09:18:48 +0200

    Task: 255. Verify Preorder Sequence in Binary Search Tree

---
+++ problems/src/test/kotlin/trees/medium/VerifyPreorderSequenceInBinarySearchTreeTest.kt
@@ -0,0 +1,22 @@
+package trees.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class VerifyPreorderSequenceInBinarySearchTreeTest {
+    @Test
+    fun test1() {
+        val preorder = intArrayOf(5, 2, 1, 3, 6)
+        val expected = true
+
+        assertEquals(expected, VerifyPreorderSequenceInBinarySearchTree.verifyPreorder(preorder))
+    }
+
+    @Test
+    fun test2() {
+        val preorder = intArrayOf(5, 2, 6, 1, 3)
+        val expected = false
+
+        assertEquals(expected, VerifyPreorderSequenceInBinarySearchTree.verifyPreorder(preorder))
+    }
+}
