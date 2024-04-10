5ff3d0d85de3e43d45b4213d6cb5221c2c138780
ikss <iksss.88@gmail.com>
2024-04-10 11:26:00 +0200

    Task: 950. Reveal Cards In Increasing Order

---
+++ problems/src/main/kotlin/arrays/medium/RevealCardsInIncreasingOrder.kt
@@ -0,0 +1,72 @@
+package arrays.medium
+
+/**
+ * You are given an integer array deck. There is a deck of cards where every card has a unique integer.
+ * The integer on the ith card is `deck[i]`.
+ *
+ * You can order the deck in any order you want. Initially, all the cards start face down (unrevealed) in one deck.
+ *
+ * You will do the following steps repeatedly until all cards are revealed:
+ *
+ * * Take the top card of the deck, reveal it, and take it out of the deck.
+ * * If there are still cards in the deck then put the next top card of the deck at the bottom of the deck.
+ * * If there are still unrevealed cards, go back to step 1. Otherwise, stop.
+ *
+ * Return an ordering of the deck that would reveal the cards in increasing order.
+ *
+ * Note that the first entry in the answer is considered to be the top of the deck.
+ *
+ * [URL](https://leetcode.com/problems/reveal-cards-in-increasing-order/)
+ */
+object RevealCardsInIncreasingOrder {
+    fun deckRevealedIncreasingSimulationArray(deck: IntArray): IntArray {
+        val cards = deck.sorted()
+
+        val result = IntArray(deck.size)
+
+        var index = 0
+        result[0] = cards[0]
+
+        for (i in 1 until cards.size) {
+            val c = cards[i]
+            val nextZero = result.indexOf(0, index)
+            index = result.indexOf(0, nextZero)
+            result[index] = c
+        }
+
+        return result
+    }
+
+    private fun IntArray.indexOf(value: Int, fromIndex: Int): Int {
+        for (i in fromIndex + 1 until size) {
+            if (this[i] == value) {
+                return i
+            }
+        }
+        for (i in indices) {
+            if (this[i] == value) {
+                return i
+            }
+        }
+        throw IllegalStateException("Value $value not found in array")
+    }
+
+    fun deckRevealedIncreasingSimulationQueue(deck: IntArray): IntArray {
+        val cards = deck.sorted()
+        val result = IntArray(deck.size)
+        val queue = java.util.ArrayDeque<Int>()
+
+        for (i in cards.indices) {
+            queue.add(i)
+        }
+
+        for (card in cards) {
+            result[queue.poll()] = card
+            if (queue.isNotEmpty()) {
+                queue.offer(queue.poll())
+            }
+        }
+
+        return result
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/RevealCardsInIncreasingOrderTest.kt
@@ -0,0 +1,39 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+
+class RevealCardsInIncreasingOrderTest {
+    @Test
+    fun test1simulationArray() {
+        val deck = intArrayOf(17, 13, 11, 2, 3, 5, 7)
+        val expected = intArrayOf(2, 13, 3, 11, 5, 17, 7)
+
+        assertArrayEquals(expected, RevealCardsInIncreasingOrder.deckRevealedIncreasingSimulationArray(deck))
+    }
+
+    @Test
+    fun test2simulationArray() {
+        val deck = intArrayOf(1, 1000)
+        val expected = intArrayOf(1, 1000)
+
+        assertArrayEquals(expected, RevealCardsInIncreasingOrder.deckRevealedIncreasingSimulationArray(deck))
+    }
+
+    @Test
+    fun test1simulationQueue() {
+        val deck = intArrayOf(17, 13, 11, 2, 3, 5, 7)
+        val expected = intArrayOf(2, 13, 3, 11, 5, 17, 7)
+
+        assertArrayEquals(expected, RevealCardsInIncreasingOrder.deckRevealedIncreasingSimulationQueue(deck))
+    }
+
+    @Test
+    fun test2simulationQueue() {
+        val deck = intArrayOf(1, 1000)
+        val expected = intArrayOf(1, 1000)
+
+        assertArrayEquals(expected, RevealCardsInIncreasingOrder.deckRevealedIncreasingSimulationQueue(deck))
+    }
+}
