c92393b8a367b5903c346bece532e94affaf1c38
ikss <iksss.88@gmail.com>
2024-04-15 21:48:19 +0200

    Task: 82. Remove Duplicates from Sorted List II

---
+++ problems/src/test/kotlin/linked_lists/medium/RemoveDuplicatesFromSortedListIITest.kt
@@ -0,0 +1,23 @@
+package linked_lists.medium
+
+import data_structures.ListBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class RemoveDuplicatesFromSortedListIITest {
+    @Test
+    fun test1() {
+        val list = ListBuilder.build(1, 2, 3, 3, 4, 4, 5)
+        val expected = ListBuilder.build(1, 2, 5)
+
+        assertEquals(expected, RemoveDuplicatesFromSortedListII.deleteDuplicates(list))
+    }
+
+    @Test
+    fun test2() {
+        val list = ListBuilder.build(1, 1, 1, 2, 3)
+        val expected = ListBuilder.build(2, 3)
+
+        assertEquals(expected, RemoveDuplicatesFromSortedListII.deleteDuplicates(list))
+    }
+}
