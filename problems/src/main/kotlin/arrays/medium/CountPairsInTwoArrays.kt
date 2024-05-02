5486ed7c39dd38a19d4f73e249c2f3c9af3cd03a
ikss <iksss.88@gmail.com>
2024-05-02 17:57:48 +0200

    Task: 1885. Count Pairs in Two Arrays

---
+++ problems/src/main/kotlin/arrays/medium/CountPairsInTwoArrays.kt
@@ -0,0 +1,34 @@
+package arrays.medium
+
+import java.util.*
+
+
+/**
+ * Given two integer arrays nums1 and nums2 of length n, count the pairs of indices (i, j)
+ * such that i < j and `nums1[i] + nums1[j] > nums2[i] + nums2[j]`.
+ *
+ * Return the number of pairs satisfying the condition.
+ *
+ * [URL](https://leetcode.com/problems/count-pairs-in-two-arrays/)
+ */
+object CountPairsInTwoArrays {
+    fun countPairs(nums1: IntArray, nums2: IntArray): Long {
+        val n = nums1.size
+
+        val difference = LongArray(n) { nums1[it].toLong() - nums2[it] }
+        difference.sort()
+
+        var result = 0L
+        var left = 0
+        var right = n - 1
+        while (left < right) {
+            if (difference[left] + difference[right] > 0) {
+                result += (right - left).toLong()
+                right--
+            } else {
+                left++
+            }
+        }
+        return result
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/CountPairsInTwoArraysTest.kt
@@ -0,0 +1,24 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CountPairsInTwoArraysTest {
+    @Test
+    fun test1() {
+        val nums1 = intArrayOf(2, 1, 2, 1)
+        val nums2 = intArrayOf(1, 2, 1, 2)
+        val expected = 1L
+
+        assertEquals(expected, CountPairsInTwoArrays.countPairs(nums1, nums2))
+    }
+
+    @Test
+    fun test2() {
+        val nums1 = intArrayOf(1, 10, 6, 2)
+        val nums2 = intArrayOf(1, 4, 1, 5)
+        val expected = 5L
+
+        assertEquals(expected, CountPairsInTwoArrays.countPairs(nums1, nums2))
+    }
+}
