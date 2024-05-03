0a3a11de8e00325f0e506a383a956bf2ffb60009
ikss <iksss.88@gmail.com>
2024-05-03 09:09:58 +0200

    Task: 165. Compare Version Numbers

---
+++ problems/src/main/kotlin/strings/medium/CompareVersionNumbers.kt
@@ -0,0 +1,61 @@
+package strings.medium
+
+/**
+ * Given two version numbers, version1 and version2, compare them.
+ *
+ * Version numbers consist of one or more revisions joined by a dot '.'.
+ * Each revision consists of digits and may contain leading zeros. Every revision contains at least one character.
+ * Revisions are 0-indexed from left to right, with the leftmost revision being revision 0,
+ * the next revision being revision 1, and so on. For example 2.5.33 and 0.1 are valid version numbers.
+ *
+ * To compare version numbers, compare their revisions in left-to-right order. Revisions are compared using their
+ * integer value ignoring any leading zeros. This means that revisions 1 and 001 are considered equal. If a version
+ * number does not specify a revision at an index, then treat the revision as 0. For example, version 1.0 is less than
+ * version 1.1 because their revision 0s are the same, but their revision 1s are 0 and 1 respectively, and 0 < 1.
+ *
+ * Return the following:
+ * * If version1 < version2, return -1.
+ * * If version1 > version2, return 1.
+ * * Otherwise, return 0.
+ *
+ * [URL](https://leetcode.com/problems/compare-version-numbers)
+ */
+object CompareVersionNumbers {
+    fun compareVersion(version1: String, version2: String): Int {
+        var i1 = 0
+        var i2 = 0
+
+        while (i1 < version1.length || i2 < version2.length) {
+            val parsed1 = parseVersion(version1, i1)
+            i1 = parsed1.first
+            val v1 = parsed1.second
+            val parsed2 = parseVersion(version2, i2)
+            i2 = parsed2.first
+            val v2 = parsed2.second
+
+            if (v1 < v2) {
+                return -1
+            } else if (v1 > v2) {
+                return 1
+            }
+        }
+
+        return 0
+    }
+
+    private fun parseVersion(version: String, index: Int): Pair<Int, Int> {
+        if (index >= version.length) return index to 0
+
+        var v = 0
+        var i = index
+
+        while (i < version.length) {
+            val ch = version[i++]
+            if (ch == '.') break
+            else if (ch == '0' && v == 0) continue
+            v = v * 10 + (ch - '0')
+        }
+
+        return i to v
+    }
+}
---
+++ problems/src/test/kotlin/strings/medium/CompareVersionNumbersTest.kt
@@ -0,0 +1,51 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CompareVersionNumbersTest {
+    @Test
+    fun test1() {
+        val version1 = "1.01"
+        val version2 = "1.001"
+        val expected = 0
+
+        assertEquals(expected, CompareVersionNumbers.compareVersion(version1, version2))
+    }
+
+    @Test
+    fun test2() {
+        val version1 = "1.0"
+        val version2 = "1.0.0"
+        val expected = 0
+
+        assertEquals(expected, CompareVersionNumbers.compareVersion(version1, version2))
+    }
+
+    @Test
+    fun test3() {
+        val version1 = "0.1"
+        val version2 = "1.1"
+        val expected = -1
+
+        assertEquals(expected, CompareVersionNumbers.compareVersion(version1, version2))
+    }
+
+    @Test
+    fun test4() {
+        val version1 = "1.0.1"
+        val version2 = "1"
+        val expected = 1
+
+        assertEquals(expected, CompareVersionNumbers.compareVersion(version1, version2))
+    }
+
+    @Test
+    fun test5() {
+        val version1 = "1.1"
+        val version2 = "1.10"
+        val expected = -1
+
+        assertEquals(expected, CompareVersionNumbers.compareVersion(version1, version2))
+    }
+}
