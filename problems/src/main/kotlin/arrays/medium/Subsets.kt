ebb8f22162916ffb99bafc21c205e48d9fec4629
ikss <iksss.88@gmail.com>
2024-05-21 09:26:16 +0200

    Task: 78. Subsets

---
+++ problems/src/main/kotlin/arrays/medium/Subsets.kt
@@ -0,0 +1,31 @@
+package arrays.medium
+
+/**
+ * Given an integer array nums of unique elements, return all possible subsets (the power set).
+ *
+ * The solution set must not contain duplicate subsets. Return the solution in any order.
+ *
+ * [URL](https://leetcode.com/problems/subsets/)
+ */
+object Subsets {
+    fun subsets(nums: IntArray): List<List<Int>> {
+        val result = ArrayList<List<Int>>()
+        result.add(emptyList())
+        for (i in nums.indices) {
+            backtrack(i, nums, arrayListOf(nums[i]), result)
+        }
+        return result
+    }
+
+    private fun backtrack(i: Int, nums: IntArray, curr: ArrayList<Int>, result: ArrayList<List<Int>>) {
+        if (i == nums.size - 1) {
+            result.add(ArrayList(curr))
+            return
+        }
+
+        curr.add(nums[i + 1])
+        backtrack(i + 1, nums, curr, result)
+        curr.removeLast()
+        backtrack(i + 1, nums, curr, result)
+    }
+}
