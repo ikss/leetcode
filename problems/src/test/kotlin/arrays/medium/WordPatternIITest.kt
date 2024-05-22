bdba9ed5202a9b846163cef1b6299d344026f4d8
ikss <iksss.88@gmail.com>
2024-05-22 11:42:22 +0200

    Task: 291. Word Pattern II

---
+++ problems/src/test/kotlin/arrays/medium/WordPatternIITest.kt
@@ -0,0 +1,42 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class WordPatternIITest {
+    @Test
+    fun test1() {
+        val pattern = "abab"
+        val s = "redblueredblue"
+        val expected = true
+
+        assertEquals(expected, WordPatternII.wordPatternMatch(pattern, s))
+    }
+
+    @Test
+    fun test2() {
+        val pattern = "aaaa"
+        val s = "asdasdasdasd"
+        val expected = true
+
+        assertEquals(expected, WordPatternII.wordPatternMatch(pattern, s))
+    }
+
+    @Test
+    fun test3() {
+        val pattern = "aabb"
+        val s = "xyzabcxzyabc"
+        val expected = false
+
+        assertEquals(expected, WordPatternII.wordPatternMatch(pattern, s))
+    }
+
+    @Test
+    fun test4() {
+        val pattern = "gh"
+        val s = "i"
+        val expected = false
+
+        assertEquals(expected, WordPatternII.wordPatternMatch(pattern, s))
+    }
+}
