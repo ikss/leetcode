180a83d17b5262dc7419647700042efb684b9549
ikss <iksss.88@gmail.com>
2024-05-29 21:45:01 +0200

    Task: 2802. Find The K-th Lucky Number

---
+++ problems/src/main/kotlin/numbers/medium/FindTheKthLuckyNumber.kt
@@ -0,0 +1,55 @@
+package numbers.medium
+
+/**
+ * We know that 4 and 7 are lucky digits. Also, a number is called lucky if it contains only lucky digits.
+ *
+ * You are given an integer k, return the kth lucky number represented as a string.
+ *
+ * [URL](https://leetcode.com/problems/find-the-k-th-lucky-number/)
+ */
+object FindTheKthLuckyNumber {
+    // TLE
+    fun kthLuckyNumberGenerate(k: Int): String {
+        val result = StringBuilder("4")
+
+        for (i in 1 until k) {
+            var toReplace = -1
+
+            for (i in result.length - 1 downTo 0) {
+                if (result[i] == '4') {
+                    toReplace = i
+                    break
+                }
+            }
+
+            if (toReplace != -1) {
+                result[toReplace] = '7'
+                for (i in toReplace + 1 until result.length) {
+                    result[i] = '4'
+                }
+            } else {
+                for (i in 0 until result.length) {
+                    result[i] = '4'
+                }
+                result.append('4')
+            }
+
+        }
+
+        return result.toString()
+    }
+
+    fun kthLuckyNumberBitManipulation(k: Int): String {
+        var k = k + 1
+
+        val binaryString = Integer.toBinaryString(k).substring(1)
+
+        val kthLuckyNum = binaryString.toCharArray()
+
+        for (i in kthLuckyNum.indices) {
+            kthLuckyNum[i] = if (kthLuckyNum[i] == '1') '7' else '4'
+        }
+
+        return String(kthLuckyNum)
+    }
+}
---
+++ problems/src/test/kotlin/numbers/medium/FindTheKthLuckyNumberTest.kt
@@ -0,0 +1,54 @@
+package numbers.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindTheKthLuckyNumberTest {
+    @Test
+    fun test1generate() {
+        val k = 4
+        val expected = "47"
+
+        assertEquals(expected, FindTheKthLuckyNumber.kthLuckyNumberGenerate(k))
+    }
+
+    @Test
+    fun test2generate() {
+        val k = 10
+        val expected = "100"
+
+        assertEquals(expected, FindTheKthLuckyNumber.kthLuckyNumberGenerate(k))
+    }
+
+    @Test
+    fun test3generate() {
+        val k = 1000
+        val expected = "777747447"
+
+        assertEquals(expected, FindTheKthLuckyNumber.kthLuckyNumberGenerate(k))
+    }
+
+    @Test
+    fun test1bitManipulation() {
+        val k = 4
+        val expected = "47"
+
+        assertEquals(expected, FindTheKthLuckyNumber.kthLuckyNumberBitManipulation(k))
+    }
+
+    @Test
+    fun test2bitManipulation() {
+        val k = 10
+        val expected = "100"
+
+        assertEquals(expected, FindTheKthLuckyNumber.kthLuckyNumberBitManipulation(k))
+    }
+
+    @Test
+    fun test3bitManipulation() {
+        val k = 1000
+        val expected = "777747447"
+
+        assertEquals(expected, FindTheKthLuckyNumber.kthLuckyNumberBitManipulation(k))
+    }
+}
