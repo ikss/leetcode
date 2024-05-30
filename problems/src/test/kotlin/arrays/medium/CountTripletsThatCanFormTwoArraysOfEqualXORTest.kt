a38f414cc9001547c70452e0a277dc35e61e806c
ikss <iksss.88@gmail.com>
2024-05-30 22:19:17 +0200

    Task: 1442. Count Triplets That Can Form Two Arrays of Equal XOR

---
+++ problems/src/test/kotlin/arrays/medium/CountTripletsThatCanFormTwoArraysOfEqualXORTest.kt
@@ -0,0 +1,22 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CountTripletsThatCanFormTwoArraysOfEqualXORTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(2, 3, 1, 6, 7)
+        val expected = 4
+
+        assertEquals(expected, CountTripletsThatCanFormTwoArraysOfEqualXOR.countTriplets(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(1, 1, 1, 1, 1)
+        val expected = 10
+
+        assertEquals(expected, CountTripletsThatCanFormTwoArraysOfEqualXOR.countTriplets(nums))
+    }
+}
