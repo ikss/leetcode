5e89c1b68a6dcbd45e7f09ebd0ce0c6cf41796b2
ikss <iksss.88@gmail.com>
2024-05-31 14:15:18 +0200

    Task: 260. Single Number III

---
+++ problems/src/test/kotlin/arrays/medium/SingleNumberIIITest.kt
@@ -0,0 +1,30 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class SingleNumberIIITest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, 2, 1, 3, 2, 5)
+        val expected = intArrayOf(3, 5)
+
+        assertArrayEquals(expected, SingleNumberIII.singleNumber(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(-1, 0)
+        val expected = intArrayOf(-1, 0)
+
+        assertArrayEquals(expected, SingleNumberIII.singleNumber(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(0, 1)
+        val expected = intArrayOf(1, 0)
+
+        assertArrayEquals(expected, SingleNumberIII.singleNumber(nums))
+    }
+}
