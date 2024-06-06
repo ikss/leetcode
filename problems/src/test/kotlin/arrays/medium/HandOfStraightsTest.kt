f4313bb3992733e35f0605219f3e135e6b34364a
ikss <iksss.88@gmail.com>
2024-06-06 09:57:57 +0200

    Task: 846. Hand of Straights

---
+++ problems/src/test/kotlin/arrays/medium/HandOfStraightsTest.kt
@@ -0,0 +1,33 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class HandOfStraightsTest {
+    @Test
+    fun test1() {
+        val hand = intArrayOf(1, 2, 3, 6, 2, 3, 4, 7, 8)
+        val groupSize = 3
+        val expected = true
+
+        assertEquals(expected, HandOfStraights.isNStraightHand(hand, groupSize))
+    }
+
+    @Test
+    fun test2() {
+        val hand = intArrayOf(1, 2, 3, 4, 5)
+        val groupSize = 4
+        val expected = false
+
+        assertEquals(expected, HandOfStraights.isNStraightHand(hand, groupSize))
+    }
+    
+    @Test
+    fun test3() {
+        val hand = intArrayOf(1, 2, 3)
+        val groupSize = 1
+        val expected = true
+
+        assertEquals(expected, HandOfStraights.isNStraightHand(hand, groupSize))
+    }
+}
