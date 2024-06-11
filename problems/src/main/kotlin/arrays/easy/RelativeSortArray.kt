faddffaadb9115158cd0f65284a5a56bec9afb22
ikss <iksss.88@gmail.com>
2024-06-11 09:05:57 +0200

    Task: 1122. Relative Sort Array (counting sort)

---
+++ problems/src/main/kotlin/arrays/easy/RelativeSortArray.kt
@@ -0,0 +1,39 @@
+package arrays.easy
+
+
+/**
+ * Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+ *
+ * Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.
+ * Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+ *
+ * [URL](https://leetcode.com/problems/relative-sort-array/)
+ */
+object RelativeSortArray {
+
+    fun relativeSortArray(arr1: IntArray, arr2: IntArray): IntArray {
+        val maxElement = arr1.max()
+        val count = IntArray(maxElement + 1)
+
+        for (element in arr1) {
+            count[element]++
+        }
+
+        val result = ArrayList<Int>(arr1.size)
+        for (value in arr2) {
+            while (count[value] > 0) {
+                result.add(value)
+                count[value]--
+            }
+        }
+
+        for (num in 0..maxElement) {
+            while (count[num] > 0) {
+                result.add(num)
+                count[num]--
+            }
+        }
+
+        return result.toIntArray()
+    }
+}
---
+++ problems/src/test/kotlin/arrays/easy/RelativeSortArrayTest.kt
@@ -0,0 +1,25 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class RelativeSortArrayTest {
+    @Test
+    fun test1() {
+        val arr1 = intArrayOf(2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19)
+        val arr2 = intArrayOf(2, 1, 4, 3, 9, 6)
+        val expected = intArrayOf(2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19)
+
+        assertArrayEquals(expected, RelativeSortArray.relativeSortArray(arr1, arr2))
+    }
+    
+    
+    @Test
+    fun test2() {
+        val arr1 = intArrayOf(28, 6, 22, 8, 44, 17)
+        val arr2 = intArrayOf(22, 28, 8, 6)
+        val expected = intArrayOf(22, 28, 8, 6, 17, 44)
+
+        assertArrayEquals(expected, RelativeSortArray.relativeSortArray(arr1, arr2))
+    }
+}
