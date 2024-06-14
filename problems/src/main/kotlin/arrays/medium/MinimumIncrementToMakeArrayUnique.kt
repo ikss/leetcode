e3019bdb0699436752eb3a694d8b225ae985baed
ikss <iksss.88@gmail.com>
2024-06-14 14:19:25 +0200

    Task: 945. Minimum Increment to Make Array Unique

---
+++ problems/src/main/kotlin/arrays/medium/MinimumIncrementToMakeArrayUnique.kt
@@ -0,0 +1,40 @@
+package arrays.medium
+
+import java.util.*
+
+/**
+ * You are given an integer array nums. In one move, you can pick an index i where 0 <= i < nums.length
+ * and increment `nums[i]` by 1.
+ *
+ * Return the minimum number of moves to make every value in nums unique.
+ *
+ * The test cases are generated so that the answer fits in a 32-bit integer.
+ *
+ * [URL](https://leetcode.com/problems/minimum-increment-to-make-array-unique/)
+ */
+object MinimumIncrementToMakeArrayUnique {
+    fun minIncrementForUnique(nums: IntArray): Int {
+        val counts = TreeMap<Int, Int>()
+
+        for (n in nums) {
+            counts.merge(n, 1, Int::plus)
+        }
+
+        var result = 0
+        while (counts.isNotEmpty()){
+            var (k, v) = counts.firstEntry()
+            var move = 1
+            while (v-- > 1) {
+                counts.merge(k + move, 1, Int::plus)
+                result += move++
+            }
+            counts.remove(k)
+        }
+
+        return result
+    }
+}
+
+fun main() {
+    println(MinimumIncrementToMakeArrayUnique.minIncrementForUnique(intArrayOf(3,2,1,2,1,7)) == 6)
+}
---
+++ problems/src/test/kotlin/arrays/medium/MinimumIncrementToMakeArrayUniqueTest.kt
@@ -0,0 +1,20 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumIncrementToMakeArrayUniqueTest {
+    @Test
+    fun test1() {
+        val input = intArrayOf(1, 2, 2)
+        val output = 1
+        assertEquals(output, MinimumIncrementToMakeArrayUnique.minIncrementForUnique(input))
+    }
+    
+    @Test
+    fun test2() {
+        val input = intArrayOf(3, 2, 1, 2, 1, 7)
+        val output = 6
+        assertEquals(output, MinimumIncrementToMakeArrayUnique.minIncrementForUnique(input))
+    }
+}
