14037bb3fc1037033cdaeb2a1f4ad4b639729c28
skuznetsov <iksss.88@gmail.com>
2024-06-27 15:20:32 +0200

    Task: 1791. Find Center of Star Graph

---
+++ problems/src/test/kotlin/graphs/easy/FindCenterOfStarGraphTest.kt
@@ -0,0 +1,22 @@
+package graphs.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindCenterOfStarGraphTest {
+    @Test
+    fun test1() {
+        val edges = arrayOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(4, 2))
+        val expected = 2
+
+        assertEquals(expected, FindCenterOfStarGraph.findCenter(edges))
+    }
+
+    @Test
+    fun test2() {
+        val edges = arrayOf(intArrayOf(1, 2), intArrayOf(5, 1), intArrayOf(1, 3), intArrayOf(1, 4))
+        val expected = 1
+
+        assertEquals(expected, FindCenterOfStarGraph.findCenter(edges))
+    }
+}
