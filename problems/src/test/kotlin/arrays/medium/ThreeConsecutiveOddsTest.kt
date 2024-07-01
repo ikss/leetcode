edb2a688b1e57b871dc29d54cfe0fc47ecd9b3b9
ikss <iksss.88@gmail.com>
2024-07-01 10:08:32 +0200

    Task: 1550. Three Consecutive Odds

---
+++ problems/src/test/kotlin/arrays/medium/ThreeConsecutiveOddsTest.kt
@@ -0,0 +1,22 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ThreeConsecutiveOddsTest {
+    @Test
+    fun test1() {
+        val arr = intArrayOf(2, 6, 4, 1)
+        val expected = false
+
+        assertEquals(expected, ThreeConsecutiveOdds.threeConsecutiveOdds(arr))
+    }
+
+    @Test
+    fun test2() {
+        val arr = intArrayOf(1, 2, 34, 3, 4, 5, 7, 23, 12)
+        val expected = true
+
+        assertEquals(expected, ThreeConsecutiveOdds.threeConsecutiveOdds(arr))
+    }
+}
