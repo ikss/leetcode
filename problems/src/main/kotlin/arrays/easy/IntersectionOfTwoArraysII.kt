7d8e5671c897a0583336fb98ef7dad68a1b720ac
ikss <iksss.88@gmail.com>
2024-07-02 09:15:10 +0200

    Task: 350. Intersection of Two Arrays II (map solution)

--- problems/src/main/kotlin/arrays/easy/IntersectionOfTwoArraysII.kt
+++ problems/src/main/kotlin/arrays/easy/IntersectionOfTwoArraysII.kt
@@ -9,7 +9,7 @@ import java.util.*
  * [URL](https://leetcode.com/problems/intersection-of-two-arrays-ii/)
  */
 object IntersectionOfTwoArraysII {
-    fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
+    fun intersectSort(nums1: IntArray, nums2: IntArray): IntArray {
         val result = mutableListOf<Int>()
         Arrays.sort(nums1)
         Arrays.sort(nums2)
@@ -29,6 +29,26 @@ object IntersectionOfTwoArraysII {
                 index2++
             }
         } while (index1 < nums1.size && index2 < nums2.size)
+        return result.toIntArray()
+    }
+
+    fun intersectMap(nums1: IntArray, nums2: IntArray): IntArray {
+        if (nums1.size > nums2.size) return intersectMap(nums2, nums1)
+
+        val map = HashMap<Int, Int>(nums1.size, 1F)
+        for (sn in nums1) {
+            map[sn] = map.getOrDefault(sn, 0) + 1
+        }
+
+        val result = ArrayList<Int>()
+
+        for (ln in nums2) {
+            val count = map.getOrDefault(ln, 0)
+            if (count <= 0) continue
+            result.add(ln)
+            map[ln] = count - 1
+        }
+
         return result.toIntArray()
     }
 }
