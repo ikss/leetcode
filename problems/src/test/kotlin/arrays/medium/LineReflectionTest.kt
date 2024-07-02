b01d4f2a150a9928153e7142d8477ade5fa13e35
ikss <iksss.88@gmail.com>
2024-07-02 19:56:30 +0200

    Task: 356. Line Reflection

---
+++ problems/src/test/kotlin/arrays/medium/LineReflectionTest.kt
@@ -0,0 +1,30 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LineReflectionTest {
+    @Test
+    fun test1() {
+        val points = arrayOf(intArrayOf(1, 1), intArrayOf(-1, 1))
+        val expected = true
+
+        assertEquals(expected, LineReflection.isReflected(points))
+    }
+
+    @Test
+    fun test2() {
+        val points = arrayOf(intArrayOf(1, 1), intArrayOf(-1, -1))
+        val expected = false
+
+        assertEquals(expected, LineReflection.isReflected(points))
+    }
+
+    @Test
+    fun test3() {
+        val points = arrayOf(intArrayOf(0, 0), intArrayOf(1, 0), intArrayOf(3, 0))
+        val expected = false
+
+        assertEquals(expected, LineReflection.isReflected(points))
+    }
+}
