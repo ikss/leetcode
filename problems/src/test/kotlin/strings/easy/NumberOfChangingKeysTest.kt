4078fa84f7ac749df19f92c38f7615dd9a3d3ed9
ikss <iksss.88@gmail.com>
2024-07-09 10:17:52 +0200

    Task: 3019. Number of Changing Keys

---
+++ problems/src/test/kotlin/strings/easy/NumberOfChangingKeysTest.kt
@@ -0,0 +1,22 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NumberOfChangingKeysTest {
+    @Test
+    fun test1() {
+        val s = "aAbBcC"
+        val expected = 2
+
+        assertEquals(expected, NumberOfChangingKeys.countKeyChanges(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "AaAaAaaA"
+        val expected = 0
+
+        assertEquals(expected, NumberOfChangingKeys.countKeyChanges(s))
+    }
+}
