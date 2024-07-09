f0977144b97f55124ef2774e88812bac1a24d715
ikss <iksss.88@gmail.com>
2024-07-09 09:18:24 +0200

    Task: 680. Valid Palindrome II

---
+++ problems/src/test/kotlin/strings/easy/ValidPalindromeIITest.kt
@@ -0,0 +1,30 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ValidPalindromeIITest {
+    @Test
+    fun test1() {
+        val s = "aba"
+        val expected = true
+
+        assertEquals(expected, ValidPalindromeII.validPalindrome(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "abca"
+        val expected = true
+
+        assertEquals(expected, ValidPalindromeII.validPalindrome(s))
+    }
+
+    @Test
+    fun test3() {
+        val s = "abc"
+        val expected = false
+
+        assertEquals(expected, ValidPalindromeII.validPalindrome(s))
+    }
+}
