c411703f2a00043c7bb59b65ca5ef8f2996ca6f3
ikss <iksss.88@gmail.com>
2024-07-09 09:42:58 +0200

    Task: 213. House Robber II

---
+++ problems/src/test/kotlin/arrays/medium/HouseRobberIITest.kt
@@ -0,0 +1,30 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class HouseRobberIITest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(2, 3, 2)
+        val expected = 3
+
+        assertEquals(expected, HouseRobberII.rob(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(1, 2, 3, 1)
+        val expected = 4
+
+        assertEquals(expected, HouseRobberII.rob(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(1, 2, 3)
+        val expected = 3
+
+        assertEquals(expected, HouseRobberII.rob(nums))
+    }
+}
