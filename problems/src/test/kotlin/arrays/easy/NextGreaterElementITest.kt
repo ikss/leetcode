d9e4f408ae42777871ba22b812e1bb1f1b7291c1
ikss <iksss.88@gmail.com>
2024-07-09 10:28:59 +0200

    Task: 496. Next Greater Element I

---
+++ problems/src/test/kotlin/arrays/easy/NextGreaterElementITest.kt
@@ -0,0 +1,24 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class NextGreaterElementITest {
+    @Test
+    fun test1() {
+        val nums1 = intArrayOf(4, 1, 2)
+        val nums2 = intArrayOf(1, 3, 4, 2)
+        val expected = intArrayOf(-1, 3, -1)
+
+        assertArrayEquals(expected, NextGreaterElementI.nextGreaterElement(nums1, nums2))
+    }
+
+    @Test
+    fun test2() {
+        val nums1 = intArrayOf(2, 4)
+        val nums2 = intArrayOf(1, 2, 3, 4)
+        val expected = intArrayOf(3, -1)
+
+        assertArrayEquals(expected, NextGreaterElementI.nextGreaterElement(nums1, nums2))
+    }
+}
