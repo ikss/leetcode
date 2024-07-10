98de6f02f4d68035198500589c9f5039bbfb2412
ikss <iksss.88@gmail.com>
2024-07-10 09:09:21 +0200

    Task: 532. K-diff Pairs in an Array

---
+++ problems/src/main/kotlin/arrays/medium/KdiffPairsInArray.kt
@@ -0,0 +1,33 @@
+package arrays.medium
+
+/**
+ * Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
+ *
+ * A k-diff pair is an integer pair `(nums[i], nums[j])`, where the following are true:
+ * * 0 <= i, j < nums.length
+ * * i != j
+ * * `|nums[i] - nums[j]| == k`
+ *
+ * Notice that |val| denotes the absolute value of val.
+ *
+ * [URL](https://leetcode.com/problems/k-diff-pairs-in-an-array/)
+ */
+object KdiffPairsInArray {
+    fun findPairs(nums: IntArray, k: Int): Int {
+        var result = 0
+
+        val map = HashMap<Int, Int>()
+        for (n in nums) {
+            map.merge(n, 1, Int::plus)
+        }
+
+        for ((num, freq) in map.entries) {
+            if (k > 0 && map.containsKey(num + k)) {
+                result++
+            } else if (k == 0 && freq > 1) {
+                result++
+            }
+        }
+        return result
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/KdiffPairsInArrayTest.kt
@@ -0,0 +1,33 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class KdiffPairsInArrayTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(3, 1, 4, 1, 5)
+        val k = 2
+        val expected = 2
+
+        assertEquals(expected, KdiffPairsInArray.findPairs(nums, k))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(1, 2, 3, 4, 5)
+        val k = 1
+        val expected = 4
+
+        assertEquals(expected, KdiffPairsInArray.findPairs(nums, k))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(1, 3, 1, 5, 4)
+        val k = 0
+        val expected = 1
+
+        assertEquals(expected, KdiffPairsInArray.findPairs(nums, k))
+    }
+}
