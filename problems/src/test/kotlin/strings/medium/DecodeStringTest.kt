d58264ac595dc153ca9a9db2b28e8329297ae081
ikss <iksss.88@gmail.com>
2024-07-10 10:12:15 +0200

    Task: 394. Decode String (recursive)

---
+++ problems/src/test/kotlin/strings/medium/DecodeStringTest.kt
@@ -0,0 +1,22 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DecodeStringTest {
+    @Test
+    fun test1() {
+        val s = "3[a]2[bc]"
+        val expected = "aaabcbc"
+        
+        assertEquals(expected, DecodeString.decodeString(s))
+    }
+    
+    @Test
+    fun test2() {
+        val s = "3[a2[c]]"
+        val expected = "accaccacc"
+        
+        assertEquals(expected, DecodeString.decodeString(s))
+    }
+}
