1b425d8280102b0d863e5652cfc22b06ddf75d2e
ikss <iksss.88@gmail.com>
2024-07-21 08:34:04 +0200

    Task: 2392. Build a Matrix With Conditions

---
+++ problems/src/test/kotlin/arrays/hard/BuildMatrixWithConditionsTest.kt
@@ -0,0 +1,26 @@
+package arrays.hard
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class BuildMatrixWithConditionsTest {
+    @Test
+    fun test1() {
+        val k = 3
+        val rowConditions = arrayOf(intArrayOf(1, 2), intArrayOf(3, 2))
+        val colConditions = arrayOf(intArrayOf(2, 1), intArrayOf(3, 2))
+        val expected = arrayOf(intArrayOf(0, 0, 1), intArrayOf(3, 0, 0), intArrayOf(0, 2, 0))
+
+        assertArrayEquals(expected, BuildMatrixWithConditions.buildMatrix(k, rowConditions, colConditions))
+    }
+
+    @Test
+    fun test2() {
+        val k = 3
+        val rowConditions = arrayOf(intArrayOf(1, 1), intArrayOf(1, 1))
+        val colConditions = arrayOf(intArrayOf(2, 1))
+        val expected = emptyArray<IntArray>()
+
+        assertArrayEquals(expected, BuildMatrixWithConditions.buildMatrix(k, rowConditions, colConditions))
+    }
+}
