d63c3a55ff6d9bfa121e4bba2052756a8073cab1
ikss <iksss.88@gmail.com>
2024-07-31 09:24:02 +0200

    Task: 1105. Filling Bookcase Shelves

---
+++ problems/src/test/kotlin/arrays/medium/FillingBookcaseShelvesTest.kt
@@ -0,0 +1,36 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class FillingBookcaseShelvesTest {
+    @Test
+    fun test1() {
+        val books = arrayOf(
+            intArrayOf(1, 1),
+            intArrayOf(2, 3),
+            intArrayOf(2, 3),
+            intArrayOf(1, 1),
+            intArrayOf(1, 1),
+            intArrayOf(1, 1),
+            intArrayOf(1, 2),
+        )
+        val shelfWidth = 4
+        val result = 6
+        
+        assertEquals(result, FillingBookcaseShelves.minHeightShelves(books, shelfWidth))
+    }
+    
+    @Test
+    fun test2() {
+        val books = arrayOf(
+            intArrayOf(1, 3),
+            intArrayOf(2, 4),
+            intArrayOf(3, 2),
+        )
+        val shelfWidth = 6
+        val result = 4
+        
+        assertEquals(result, FillingBookcaseShelves.minHeightShelves(books, shelfWidth))
+    }
+}
