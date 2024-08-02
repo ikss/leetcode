9a934ac3dac267dbdee128cca99227383904099b
ikss <iksss.88@gmail.com>
2024-08-02 17:15:03 +0200

    Task: 2134. Minimum Swaps to Group All 1's Together II

---
+++ problems/src/main/kotlin/arrays/medium/MinimumSwapsToGroupAll1sTogetherII.kt
@@ -0,0 +1,43 @@
+package arrays.medium
+
+/**
+ * A swap is defined as taking two distinct positions in an array and swapping the values in them.
+ *
+ * A circular array is defined as an array where we consider the first element and the last element to be adjacent.
+ *
+ * Given a binary circular array nums, return the minimum number of swaps required to group all 1's present in the array together at any location.
+ *
+ * [URL](https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii/)
+ */
+object MinimumSwapsToGroupAll1sTogetherII {
+    fun minSwaps(nums: IntArray): Int {
+        var totalOnes = 0
+
+        for (num in nums) {
+            totalOnes += num
+        }
+        var result = totalOnes
+
+        var onesCount = 0
+        var end = 0
+        var start = 0
+        
+        while (end < totalOnes) {
+            onesCount += nums[end++]
+        }
+        end--
+
+        while (start < nums.size) {
+            if (start > 0) {
+                onesCount -= nums[start - 1]
+                onesCount += nums[end % nums.size]
+            }
+
+            result = minOf(result, totalOnes - onesCount)
+            start++
+            end++
+        }
+
+        return result
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/MinimumSwapsToGroupAll1sTogetherIITest.kt
@@ -0,0 +1,30 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumSwapsToGroupAll1sTogetherIITest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(0, 1, 0, 1, 1, 0, 0)
+        val expected = 1
+
+        assertEquals(expected, MinimumSwapsToGroupAll1sTogetherII.minSwaps(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(0, 1, 1, 1, 0, 0, 1, 1, 0)
+        val expected = 2
+
+        assertEquals(expected, MinimumSwapsToGroupAll1sTogetherII.minSwaps(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(1, 1, 0, 0, 1)
+        val expected = 0
+
+        assertEquals(expected, MinimumSwapsToGroupAll1sTogetherII.minSwaps(nums))
+    }
+}
