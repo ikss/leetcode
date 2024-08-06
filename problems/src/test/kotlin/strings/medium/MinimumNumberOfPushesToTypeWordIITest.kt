c5fd483d0430ebb318b92985d51b3a0670c881ae
ikss <iksss.88@gmail.com>
2024-08-06 06:56:24 +0200

    Task: 3016. Minimum Number of Pushes to Type Word II

---
+++ problems/src/test/kotlin/strings/medium/MinimumNumberOfPushesToTypeWordIITest.kt
@@ -0,0 +1,30 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumNumberOfPushesToTypeWordIITest {
+    @Test
+    fun test1() {
+        val word = "abcde"
+        val expected = 5
+
+        assertEquals(expected, MinimumNumberOfPushesToTypeWordII.minimumPushes(word))
+    }
+
+    @Test
+    fun test2() {
+        val word = "xyzxyzxyzxyz"
+        val expected = 12
+
+        assertEquals(expected, MinimumNumberOfPushesToTypeWordII.minimumPushes(word))
+    }
+
+    @Test
+    fun test3() {
+        val word = "aabbccddeeffgghhiiiiii"
+        val expected = 24
+
+        assertEquals(expected, MinimumNumberOfPushesToTypeWordII.minimumPushes(word))
+    }
+}
