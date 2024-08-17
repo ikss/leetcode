13cdde89af8a20454774d614d67544a54e3203d5
ikss <iksss.88@gmail.com>
2024-08-17 21:37:02 +0200

    Task: 2057. Smallest Index With Equal Value

---
+++ problems/src/main/kotlin/arrays/easy/SmallestIndexWithEqualValue.kt
@@ -0,0 +1,22 @@
+package arrays.easy
+
+/**
+ * Given a 0-indexed integer array nums, return the smallest index i of nums such that `i mod 10 == nums[i]`,
+ * or -1 if such index does not exist.
+ *
+ * x mod y denotes the remainder when x is divided by y.
+ *
+ * [URL](https://leetcode.com/problems/smallest-index-with-equal-value/)
+ */
+object SmallestIndexWithEqualValue {
+    fun smallestEqual(nums: IntArray): Int {
+        for (i in nums.indices) {
+            if (i % 10 == nums[i]) return i
+        }
+        return -1
+    }
+
+    fun smallestEqualOneLine(nums: IntArray): Int {
+        return nums.withIndex().firstOrNull { it.index % 10 == it.value }?.index ?: -1
+    }
+}
---
+++ problems/src/test/kotlin/arrays/easy/SmallestIndexWithEqualValueTest.kt
@@ -0,0 +1,54 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SmallestIndexWithEqualValueTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(0, 1, 2)
+        val expected = 0
+
+        assertEquals(expected, SmallestIndexWithEqualValue.smallestEqual(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(4, 3, 2, 1)
+        val expected = 2
+
+        assertEquals(expected, SmallestIndexWithEqualValue.smallestEqual(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(7, 8, 3, 5, 2, 6, 3, 1, 1, 4, 5, 4, 8, 7, 2, 0, 9, 9, 0, 5, 7, 1, 6)
+        val expected = 21
+
+        assertEquals(expected, SmallestIndexWithEqualValue.smallestEqual(nums))
+    }
+
+    @Test
+    fun test1OneLine() {
+        val nums = intArrayOf(0, 1, 2)
+        val expected = 0
+
+        assertEquals(expected, SmallestIndexWithEqualValue.smallestEqualOneLine(nums))
+    }
+
+    @Test
+    fun test2OneLine() {
+        val nums = intArrayOf(4, 3, 2, 1)
+        val expected = 2
+
+        assertEquals(expected, SmallestIndexWithEqualValue.smallestEqualOneLine(nums))
+    }
+
+    @Test
+    fun test3OneLine() {
+        val nums = intArrayOf(7, 8, 3, 5, 2, 6, 3, 1, 1, 4, 5, 4, 8, 7, 2, 0, 9, 9, 0, 5, 7, 1, 6)
+        val expected = 21
+
+        assertEquals(expected, SmallestIndexWithEqualValue.smallestEqualOneLine(nums))
+    }
+}
