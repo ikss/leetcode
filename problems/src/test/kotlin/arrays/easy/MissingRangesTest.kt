a64d5e0a909b698cb4fa95009d7f64d0fe51ccf8
ikss <iksss.88@gmail.com>
2024-08-18 09:39:52 +0200

    Task: 163. Missing Ranges

---
+++ problems/src/test/kotlin/arrays/easy/MissingRangesTest.kt
@@ -0,0 +1,26 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MissingRangesTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(0, 1, 3, 50, 75)
+        val lower = 0
+        val upper = 99
+        val expected = listOf(listOf(2, 2), listOf(4, 49), listOf(51, 74), listOf(76, 99))
+
+        assertEquals(expected, MissingRanges.findMissingRanges(nums, lower, upper))
+    }
+    
+    @Test
+    fun test2() {
+        val nums = intArrayOf(-1)
+        val lower = -1
+        val upper = -1
+        val expected = emptyList<List<Int>>()
+
+        assertEquals(expected, MissingRanges.findMissingRanges(nums, lower, upper))
+    }
+}
