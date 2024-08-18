07e465dfad79cd15d2accdf4bfe0f51d51ce7ca1
ikss <iksss.88@gmail.com>
2024-08-18 09:12:09 +0200

    Task: 264. Ugly Number II (dynamic programming)

--- problems/src/main/kotlin/numbers/medium/UglyNumberII.kt
+++ problems/src/main/kotlin/numbers/medium/UglyNumberII.kt
@@ -10,7 +10,7 @@ import java.util.*
  * [URL](https://leetcode.com/problems/ugly-number-ii/)
  */
 object UglyNumberII {
-    fun nthUglyNumber(n: Int): Int {
+    fun nthUglyNumberTreeSet(n: Int): Int {
         val set = TreeSet<Long>()
         var currentUgly = 1L
         set.add(currentUgly)
@@ -24,5 +24,23 @@ object UglyNumberII {
         }
 
         return set.pollFirst()!!.toInt()
+    }
+
+    fun nthUglyNumberDp(n: Int): Int {
+        val dp = IntArray(n)
+        dp[0] = 1
+
+        var i2 = 0
+        var i3 = 0
+        var i5 = 0
+
+        for (i in 1 until n) {
+            dp[i] = minOf(dp[i2] * 2, dp[i3] * 3, dp[i5] * 5)
+            if (dp[i] == dp[i2] * 2) i2++
+            if (dp[i] == dp[i3] * 3) i3++
+            if (dp[i] == dp[i5] * 5) i5++
+        }
+
+        return dp[n - 1]
     }
 }
--- problems/src/test/kotlin/numbers/medium/UglyNumberIITest.kt
+++ problems/src/test/kotlin/numbers/medium/UglyNumberIITest.kt
@@ -5,18 +5,34 @@ import org.junit.jupiter.api.Test
 
 class UglyNumberIITest {
     @Test
-    fun test1() {
+    fun test1TreeSet() {
         val n = 10
         val expected = 12
 
-        assertEquals(expected, UglyNumberII.nthUglyNumber(n))
+        assertEquals(expected, UglyNumberII.nthUglyNumberTreeSet(n))
     }
-    
+
     @Test
-    fun test2() {
+    fun test2TreeSet() {
         val n = 1
         val expected = 1
 
-        assertEquals(expected, UglyNumberII.nthUglyNumber(n))
+        assertEquals(expected, UglyNumberII.nthUglyNumberTreeSet(n))
+    }
+
+    @Test
+    fun test1Dp() {
+        val n = 10
+        val expected = 12
+
+        assertEquals(expected, UglyNumberII.nthUglyNumberDp(n))
+    }
+
+    @Test
+    fun test2Dp() {
+        val n = 1
+        val expected = 1
+
+        assertEquals(expected, UglyNumberII.nthUglyNumberDp(n))
     }
 }
