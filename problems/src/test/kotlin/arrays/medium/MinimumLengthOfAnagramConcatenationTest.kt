93aad689a2a7bb9d023606fd0054199e50166d31
ikss <iksss.88@gmail.com>
2024-08-23 21:07:56 +0200

    Task: 3138. Minimum Length of Anagram Concatenation

---
+++ problems/src/test/kotlin/arrays/medium/MinimumLengthOfAnagramConcatenationTest.kt
@@ -0,0 +1,30 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumLengthOfAnagramConcatenationTest {
+    @Test
+    fun test1() {
+        val s = "abba"
+        val expected = 2
+
+        assertEquals(expected, MinimumLengthOfAnagramConcatenation.minAnagramLength(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "cdef"
+        val expected = 4
+
+        assertEquals(expected, MinimumLengthOfAnagramConcatenation.minAnagramLength(s))
+    }
+
+    @Test
+    fun test3() {
+        val s = "aabb"
+        val expected = 4
+
+        assertEquals(expected, MinimumLengthOfAnagramConcatenation.minAnagramLength(s))
+    }
+}
