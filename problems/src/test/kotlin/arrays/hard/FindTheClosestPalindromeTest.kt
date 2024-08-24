6443701c71741bd1e06453b09ca86aa2bb9ca4df
ikss <iksss.88@gmail.com>
2024-08-24 09:06:51 +0200

    Task: 564. Find the Closest Palindrome

---
+++ problems/src/test/kotlin/arrays/hard/FindTheClosestPalindromeTest.kt
@@ -0,0 +1,46 @@
+package arrays.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindTheClosestPalindromeTest {
+    @Test
+    fun test1() {
+        val input = "123"
+        val output = "121"
+
+        assertEquals(output, FindTheClosestPalindrome.nearestPalindromic(input))
+    }
+
+    @Test
+    fun test2() {
+        val input = "1"
+        val output = "0"
+
+        assertEquals(output, FindTheClosestPalindrome.nearestPalindromic(input))
+    }
+
+    @Test
+    fun test3() {
+        val input = "10"
+        val output = "9"
+
+        assertEquals(output, FindTheClosestPalindrome.nearestPalindromic(input))
+    }
+
+    @Test
+    fun test4() {
+        val input = "100"
+        val output = "99"
+
+        assertEquals(output, FindTheClosestPalindrome.nearestPalindromic(input))
+    }
+
+    @Test
+    fun test5() {
+        val input = "9999"
+        val output = "10001"
+
+        assertEquals(output, FindTheClosestPalindrome.nearestPalindromic(input))
+    }
+}
