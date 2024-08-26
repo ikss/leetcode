2b78b62dc3dea2fe7aaaab6a6f6ed40109a509c7
ikss <iksss.88@gmail.com>
2024-08-26 09:41:19 +0200

    Add NaryTreeBuilder to utils

--- shared/src/main/kotlin/data_structures/NaryTreeNode.kt
+++ shared/src/main/kotlin/data_structures/NaryTreeNode.kt
@@ -1,3 +1,33 @@
 package data_structures
 
 data class NaryTreeNode(val `val`: Int, var children: List<NaryTreeNode?> = emptyList())
+
+object NaryTreeBuilder {
+    fun build(vararg values: Int?): NaryTreeNode? {
+        return build(values.toList())
+    }
+
+    private fun build(values: List<Int?>): NaryTreeNode? {
+        val rootVal = values.firstOrNull() ?: return null
+        val root = NaryTreeNode(rootVal)
+        var index = 2
+        val queue = java.util.ArrayDeque<NaryTreeNode>()
+        queue.offer(root)
+
+        while (index < values.size && queue.isNotEmpty()) {
+            val currNode = queue.poll()!!
+            
+            // add current level
+            val currLevel = ArrayList<NaryTreeNode>()
+            while (index < values.size && values[index] != null) {
+                val childNode = NaryTreeNode(values[index++]!!)
+                currLevel.add(childNode)
+                queue.offer(childNode)
+            }
+            currNode.children = currLevel
+            // skip null
+            index++
+        }
+        return root
+    }
+}
