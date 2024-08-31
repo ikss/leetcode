deecb7e8f2978e25d64cb9bdc6b242bc69fcc8a2
ikss <iksss.88@gmail.com>
2024-08-31 06:55:51 +0200

    Task: 922. Sort Array By Parity II

---
+++ problems/src/main/kotlin/arrays/easy/SortArrayByParityII.kt
@@ -0,0 +1,30 @@
+package arrays.easy
+
+/**
+ * Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+ *
+ * Sort the array so that whenever `nums[i]` is odd, i is odd, and whenever `nums[i]` is even, i is even.
+ *
+ * Return any answer array that satisfies this condition.
+ *
+ * [URL](https://leetcode.com/problems/sort-array-by-parity-ii/)
+ */
+object SortArrayByParityII {
+    fun sortArrayByParityII(nums: IntArray): IntArray {
+        val result = IntArray(nums.size)
+        var evenIndex = 0
+        var oddIndex = 1
+        
+        for (n in nums) {
+            if (n % 2 == 0) {
+                result[evenIndex] = n
+                evenIndex += 2
+            } else {
+                result[oddIndex] = n
+                oddIndex += 2
+            }
+        }
+
+        return result
+    }
+}
---
+++ problems/src/test/kotlin/arrays/easy/SortArrayByParityIITest.kt
@@ -0,0 +1,22 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class SortArrayByParityIITest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(4, 2, 5, 7)
+        val expected = intArrayOf(4, 5, 2, 7)
+
+        assertArrayEquals(expected, SortArrayByParityII.sortArrayByParityII(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(2, 3)
+        val expected = intArrayOf(2, 3)
+
+        assertArrayEquals(expected, SortArrayByParityII.sortArrayByParityII(nums))
+    }
+}
