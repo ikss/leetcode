3e53d0a22fd21a5b49ca4f544beedfbfe3d582b8
ikss <iksss.88@gmail.com>
2024-09-02 09:31:39 +0200

    Task: 1894. Find the Student that Will Replace the Chalk

---
+++ problems/src/main/kotlin/arrays/medium/FindTheStudentThatWillReplaceTheChalk.kt
@@ -0,0 +1,33 @@
+package arrays.medium
+
+/**
+ * There are n students in a class numbered from 0 to n - 1. The teacher will give each student a problem starting with
+ * the student number 0, then the student number 1, and so on until the teacher reaches the student number n - 1.
+ * After that, the teacher will restart the process, starting with the student number 0 again.
+ *
+ * You are given a 0-indexed integer array chalk and an integer k. There are initially k pieces of chalk. When the
+ * student number i is given a problem to solve, they will use `chalk[i]` pieces of chalk to solve that problem.
+ * However, if the current number of chalk pieces is strictly less than `chalk[i]`, then the student number i will be
+ * asked to replace the chalk.
+ *
+ * Return the index of the student that will replace the chalk pieces.
+ *
+ * [URL](https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/)
+ */
+object FindTheStudentThatWillReplaceTheChalk {
+    fun chalkReplacer(chalk: IntArray, k: Int): Int {
+        val n = chalk.size
+        val sum = chalk.sumOf { it.toLong() }
+
+        var curr = 0
+        var k = k % sum
+
+        while (k > 0) {
+            k -= chalk[curr % n]
+            if (k < 0) break
+            curr++
+        }
+
+        return curr % n
+    }
+}
