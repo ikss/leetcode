86eb4ca09973b142550724c4cb48a6f9935b5370
ikss <iksss.88@gmail.com>
2024-09-03 18:18:51 +0200

    Task: 628. Maximum Product of Three Numbers (single pass, min max)

--- problems/src/main/kotlin/arrays/easy/MaximumProductOfThreeNumbers.kt
+++ problems/src/main/kotlin/arrays/easy/MaximumProductOfThreeNumbers.kt
@@ -6,10 +6,37 @@ package arrays.easy
  * [URL](https://leetcode.com/problems/maximum-product-of-three-numbers/)
  */
 object MaximumProductOfThreeNumbers {
-    fun maximumProduct(nums: IntArray): Int {
+    fun maximumProductSort(nums: IntArray): Int {
         nums.sort()
         val n = nums.size
 
         return maxOf(nums[0] * nums[1] * nums[n - 1], nums[n - 3] * nums[n - 2] * nums[n - 1])
+    }
+
+    fun maximumProductMinMax(nums: IntArray): Int {
+        var min1 = Int.MAX_VALUE
+        var min2 = Int.MAX_VALUE
+        var max1 = Int.MIN_VALUE
+        var max2 = Int.MIN_VALUE
+        var max3 = Int.MIN_VALUE
+        for (n in nums) {
+            if (n <= min1) {
+                min2 = min1
+                min1 = n
+            } else if (n <= min2) {     // n lies between min1 and min2
+                min2 = n
+            }
+            if (n >= max1) {            // n is greater than max1, max2 and max3
+                max3 = max2
+                max2 = max1
+                max1 = n
+            } else if (n >= max2) {     // n lies betweeen max1 and max2
+                max3 = max2
+                max2 = n
+            } else if (n >= max3) {     // n lies betwen max2 and max3
+                max3 = n
+            }
+        }
+        return maxOf(min1 * min2 * max1, max1 * max2 * max3)
     }
 }
--- problems/src/test/kotlin/arrays/easy/MaximumProductOfThreeNumbersTest.kt
+++ problems/src/test/kotlin/arrays/easy/MaximumProductOfThreeNumbersTest.kt
@@ -5,26 +5,50 @@ import org.junit.jupiter.api.Test
 
 class MaximumProductOfThreeNumbersTest {
     @Test
-    fun test1() {
+    fun test1Sort() {
         val nums = intArrayOf(1, 2, 3)
         val expected = 6
 
-        assertEquals(expected, MaximumProductOfThreeNumbers.maximumProduct(nums))
+        assertEquals(expected, MaximumProductOfThreeNumbers.maximumProductSort(nums))
     }
 
     @Test
-    fun test2() {
+    fun test2Sort() {
         val nums = intArrayOf(1, 2, 3, 4)
         val expected = 24
 
-        assertEquals(expected, MaximumProductOfThreeNumbers.maximumProduct(nums))
+        assertEquals(expected, MaximumProductOfThreeNumbers.maximumProductSort(nums))
     }
 
     @Test
-    fun test3() {
+    fun test3Sort() {
         val nums = intArrayOf(-1, -2, -3)
         val expected = -6
 
-        assertEquals(expected, MaximumProductOfThreeNumbers.maximumProduct(nums))
+        assertEquals(expected, MaximumProductOfThreeNumbers.maximumProductSort(nums))
+    }
+
+    @Test
+    fun test1MinMax() {
+        val nums = intArrayOf(1, 2, 3)
+        val expected = 6
+
+        assertEquals(expected, MaximumProductOfThreeNumbers.maximumProductMinMax(nums))
+    }
+
+    @Test
+    fun test2MinMax() {
+        val nums = intArrayOf(1, 2, 3, 4)
+        val expected = 24
+
+        assertEquals(expected, MaximumProductOfThreeNumbers.maximumProductMinMax(nums))
+    }
+
+    @Test
+    fun test3MinMax() {
+        val nums = intArrayOf(-1, -2, -3)
+        val expected = -6
+
+        assertEquals(expected, MaximumProductOfThreeNumbers.maximumProductMinMax(nums))
     }
 }
