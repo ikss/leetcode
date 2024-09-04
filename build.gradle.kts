04a29db3b7577373e534597da269121a5f7ef4bd
ikss <iksss.88@gmail.com>
2024-09-04 09:56:00 +0200

    Bump kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "2.0.0"
+    id("org.jetbrains.kotlin.jvm") version "2.0.20"
 }
 
 allprojects {
