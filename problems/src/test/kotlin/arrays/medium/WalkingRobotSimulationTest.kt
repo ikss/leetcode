761feba1ee931494901e4f64cef9b1e9534baea0
ikss <iksss.88@gmail.com>
2024-09-04 09:27:17 +0200

    Task: 874. Walking Robot Simulation

---
+++ problems/src/test/kotlin/arrays/medium/WalkingRobotSimulationTest.kt
@@ -0,0 +1,33 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class WalkingRobotSimulationTest {
+    @Test
+    fun test1() {
+        val commands = intArrayOf(4, -1, 3)
+        val obstacles = emptyArray<IntArray>()
+        val expected = 25
+
+        assertEquals(expected, WalkingRobotSimulation.robotSim(commands, obstacles))
+    }
+    
+    @Test
+    fun test2() {
+        val commands = intArrayOf(4, -1, 4, -2, 4)
+        val obstacles = arrayOf(intArrayOf(2, 4))
+        val expected = 65
+
+        assertEquals(expected, WalkingRobotSimulation.robotSim(commands, obstacles))
+    }
+    
+    @Test
+    fun test3() {
+        val commands = intArrayOf(6,-1,-1,6)
+        val obstacles = emptyArray<IntArray>()
+        val expected = 36
+
+        assertEquals(expected, WalkingRobotSimulation.robotSim(commands, obstacles))
+    }
+}
