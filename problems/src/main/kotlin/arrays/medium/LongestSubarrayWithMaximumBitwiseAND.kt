804e7e9baf484ee06c64cd268fcc05dd8f2f8a56
ikss <iksss.88@gmail.com>
2024-09-14 23:16:32 +0200

    Task: 2419. Longest Subarray With Maximum Bitwise AND

---
+++ problems/src/main/kotlin/arrays/medium/LongestSubarrayWithMaximumBitwiseAND.kt
@@ -0,0 +1,42 @@
+package arrays.medium
+
+/**
+ * You are given an integer array nums of size n.
+ *
+ * Consider a non-empty subarray from nums that has the maximum possible bitwise AND.
+ *
+ * * In other words, let k be the maximum value of the bitwise AND of any subarray of nums
+ * Then, only subarrays with a bitwise AND equal to k should be considered.
+ *
+ * Return the length of the longest such subarray.
+ *
+ * The bitwise AND of an array is the bitwise AND of all the numbers in it.
+ *
+ * A subarray is a contiguous sequence of elements within an array.
+ *
+ * [URL](https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/)
+ */
+object LongestSubarrayWithMaximumBitwiseAND {
+    fun longestSubarray(nums: IntArray): Int {
+        var result = 0
+        var maxVal = 0
+        var currentStreak = 0
+
+        for (num in nums) {
+            if (maxVal < num) {
+                maxVal = num
+                currentStreak = 0
+                result = currentStreak
+            }
+
+            if (maxVal == num) {
+                currentStreak++
+            } else {
+                currentStreak = 0
+            }
+
+            result = maxOf(result, currentStreak)
+        }
+        return result
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/LongestSubarrayWithMaximumBitwiseANDTest.kt
@@ -0,0 +1,22 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LongestSubarrayWithMaximumBitwiseANDTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, 2, 3, 3, 2, 2)
+        val expected = 2
+
+        assertEquals(expected, LongestSubarrayWithMaximumBitwiseAND.longestSubarray(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(1, 2, 3, 4)
+        val expected = 1
+
+        assertEquals(expected, LongestSubarrayWithMaximumBitwiseAND.longestSubarray(nums))
+    }
+}
