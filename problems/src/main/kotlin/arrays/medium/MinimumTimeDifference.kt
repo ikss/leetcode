3268440bee2d051ebc8187b21e6f597ed8a6123f
ikss <iksss.88@gmail.com>
2024-09-16 13:03:20 +0200

    Task: 539. Minimum Time Difference (bucket sort)

--- problems/src/main/kotlin/arrays/medium/MinimumTimeDifference.kt
+++ problems/src/main/kotlin/arrays/medium/MinimumTimeDifference.kt
@@ -8,7 +8,7 @@ package arrays.medium
  */
 object MinimumTimeDifference {
     fun findMinDifferenceSort(timePoints: List<String>): Int {
-        val sorted = timePoints.map { it ->
+        val sorted = timePoints.map {
             val (hours, minutes) = it.split(":")
             60 * hours.toInt() + minutes.toInt()
         }.sorted()
@@ -25,5 +25,35 @@ object MinimumTimeDifference {
         val diff = Math.abs(t1 - t2)
 
         return minOf(diff, 1440 - diff)
+    }
+
+    fun findMinDifferenceBucketSort(timePoints: List<String>): Int {
+        val minutes = BooleanArray(24 * 60)
+        for (t in timePoints) {
+            val mapped = t.substring(0, 2).toInt() * 60 + t.substring(3).toInt()
+            if (minutes[mapped]) {
+                return 0
+            }
+            minutes[mapped] = true
+        }
+        var prevIndex = Int.MAX_VALUE
+        var firstIndex = Int.MAX_VALUE
+        var lastIndex = Int.MAX_VALUE
+        var result = Int.MAX_VALUE
+
+        for (i in 0 until 24 * 60) {
+            if (!minutes[i]) continue
+            
+            if (prevIndex != Int.MAX_VALUE) {
+                result = minOf(result, i - prevIndex)
+            }
+            prevIndex = i
+            if (firstIndex == Int.MAX_VALUE) {
+                firstIndex = i
+            }
+            lastIndex = i
+        }
+
+        return minOf(result, (24 * 60 - (lastIndex - firstIndex)))
     }
 }
--- problems/src/test/kotlin/arrays/medium/MinimumTimeDifferenceTest.kt
+++ problems/src/test/kotlin/arrays/medium/MinimumTimeDifferenceTest.kt
@@ -27,4 +27,28 @@ class MinimumTimeDifferenceTest {
 
         assertEquals(expected, MinimumTimeDifference.findMinDifferenceSort(timePoints))
     }
+
+    @Test
+    fun test1BucketSort() {
+        val timePoints = listOf("23:59", "00:00")
+        val expected = 1
+
+        assertEquals(expected, MinimumTimeDifference.findMinDifferenceBucketSort(timePoints))
+    }
+
+    @Test
+    fun test2BucketSort() {
+        val timePoints = listOf("00:00", "23:59", "00:00")
+        val expected = 0
+
+        assertEquals(expected, MinimumTimeDifference.findMinDifferenceBucketSort(timePoints))
+    }
+
+    @Test
+    fun test3BucketSort() {
+        val timePoints = listOf("16:28", "10:33", "19:36", "11:30", "12:04")
+        val expected = 34
+
+        assertEquals(expected, MinimumTimeDifference.findMinDifferenceBucketSort(timePoints))
+    }
 }
