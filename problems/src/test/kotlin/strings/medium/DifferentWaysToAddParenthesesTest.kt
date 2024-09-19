247c56339393eb2887e26d50da77c3ebb42d347c
ikss <iksss.88@gmail.com>
2024-09-19 15:18:58 +0200

    Task: 241. Different Ways to Add Parentheses

---
+++ problems/src/test/kotlin/strings/medium/DifferentWaysToAddParenthesesTest.kt
@@ -0,0 +1,22 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DifferentWaysToAddParenthesesTest {
+    @Test
+    fun test1() {
+        val input = "2-1-1"
+        val expected = listOf(2, 0)
+        
+        assertEquals(expected, DifferentWaysToAddParentheses.diffWaysToCompute(input))
+    }
+    
+    @Test
+    fun test2() {
+        val input = "2*3-4*5"
+        val expected = listOf(-34, -10, -14, -10, 10)
+        
+        assertEquals(expected, DifferentWaysToAddParentheses.diffWaysToCompute(input))
+    }
+}
