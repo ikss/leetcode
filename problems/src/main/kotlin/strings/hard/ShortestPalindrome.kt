33ad3c936b987256516b91b2f96882b859d0c52d
ikss <iksss.88@gmail.com>
2024-09-20 09:53:46 +0200

    214. Shortest Palindrome

---
+++ problems/src/main/kotlin/strings/hard/ShortestPalindrome.kt
@@ -0,0 +1,33 @@
+package strings.hard
+
+import jdk.internal.org.jline.utils.Colors
+
+
+/**
+ * You are given a string s. You can convert s to a palindrome by adding characters in front of it.
+ *
+ * Return the shortest palindrome you can find by performing this transformation.
+ *
+ * [URL](https://leetcode.com/problems/shortest-palindrome/)
+ */
+object ShortestPalindrome {
+    fun shortestPalindrome(s: String): String {
+        return shortestPalindrome(s, s.length)
+    }
+
+    private fun shortestPalindrome(s: String, end: Int): String {
+        var left = 0
+
+        for (right in end - 1 downTo 0) {
+            if (s[right] == s[left]) {
+                left++
+            }
+        }
+
+        if (left == end) return s
+        
+        val builder = StringBuilder(end).append(s, left, end).reverse()
+
+        return builder.append(shortestPalindrome(s, left)).toString()
+    }
+}
---
+++ problems/src/test/kotlin/strings/hard/ShortestPalindromeTest.kt
@@ -0,0 +1,22 @@
+package strings.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ShortestPalindromeTest {
+    @Test
+    fun test1() {
+        val s = "aacecaaa"
+        val expected = "aaacecaaa"
+
+        assertEquals(expected, ShortestPalindrome.shortestPalindrome(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "abcd"
+        val expected = "dcbabcd"
+
+        assertEquals(expected, ShortestPalindrome.shortestPalindrome(s))
+    }
+}
