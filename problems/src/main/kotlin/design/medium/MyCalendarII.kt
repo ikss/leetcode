9152a501ee1b852d81a5591cfb00f6ff87fd2e19
ikss <iksss.88@gmail.com>
2024-09-27 12:08:16 +0200

    731. My Calendar II (fix javadoc)

--- problems/src/main/kotlin/design/medium/MyCalendarII.kt
+++ problems/src/main/kotlin/design/medium/MyCalendarII.kt
@@ -11,9 +11,8 @@ package design.medium
  * `[start, end)`, the range of real numbers x such that start <= x < end.
  *
  * Implement the MyCalendarTwo class:
- *
- * MyCalendarTwo() Initializes the calendar object.
- * boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing
+ * * MyCalendarTwo() Initializes the calendar object.
+ * * boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing
  * a triple booking. Otherwise, return false and do not add the event to the calendar.
  *
  * [URL](https://leetcode.com/problems/my-calendar-ii/)
