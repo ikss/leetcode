e5af7cb02be32f71f4098cb791ed7a2d8715de6f
ikss <iksss.88@gmail.com>
2024-10-16 12:27:46 +0200

    2840. Check if Strings Can be Made Equal With Operations II

---
+++ problems/src/main/kotlin/strings/medium/CheckIfStringsCanBeMadeEqualWithOperationsII.kt
@@ -0,0 +1,38 @@
+package strings.medium
+
+/**
+ * You are given two strings s1 and s2, both of length n, consisting of lowercase English letters.
+ *
+ * You can apply the following operation on any of the two strings any number of times:
+ * * Choose any two indices i and j such that i < j and the difference j - i is even,
+ * then swap the two characters at those indices in the string.
+ *
+ * Return true if you can make the strings s1 and s2 equal, and false otherwise.
+ *
+ * [URL](https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-ii/)
+ */
+object CheckIfStringsCanBeMadeEqualWithOperationsII {
+    fun checkStrings(s1: String, s2: String): Boolean {
+        val hashEven = Array(2) { IntArray(26) }
+        val hashOdd = Array(2) { IntArray(26) }
+
+        for (i in s1.indices) {
+            if (i % 2 == 0) {
+                hashEven[0][s1[i] - 'a']++
+                hashEven[1][s2[i] - 'a']++
+            } else {
+                hashOdd[0][s1[i] - 'a']++
+                hashOdd[1][s2[i] - 'a']++
+            }
+        }
+        return areSame(hashEven[0], hashEven[1]) && areSame(hashOdd[0], hashOdd[1])
+    }
+
+    private fun areSame(arr1: IntArray, arr2: IntArray): Boolean {
+        for (i in arr1.indices) {
+            if (arr1[i] != arr2[i]) return false
+        }
+
+        return true
+    }
+}
