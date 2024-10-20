5834b27d5f4a91c1b98fdc88bee42d1123b041fe
ikss <iksss.88@gmail.com>
2024-10-20 17:19:27 +0200

    1106. Parsing A Boolean Expression

---
+++ problems/src/test/kotlin/strings/hard/ParsingABooleanExpressionTest.kt
@@ -0,0 +1,30 @@
+package strings.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ParsingABooleanExpressionTest {
+    @Test
+    fun test1() {
+        val input = "&(|(f))"
+        val expected = false
+
+        assertEquals(expected, ParsingABooleanExpression.parseBoolExpr(input))
+    }
+
+    @Test
+    fun test2() {
+        val input = "|(f,f,f,t)"
+        val expected = true
+
+        assertEquals(expected, ParsingABooleanExpression.parseBoolExpr(input))
+    }
+
+    @Test
+    fun test3() {
+        val input = "!(&(f,t))"
+        val expected = true
+
+        assertEquals(expected, ParsingABooleanExpression.parseBoolExpr(input))
+    }
+}
