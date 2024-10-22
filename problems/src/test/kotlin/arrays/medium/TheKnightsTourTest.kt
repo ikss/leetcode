3c3cbc5d0e557441aeb75f8d9da64e906efdc249
ikss <iksss.88@gmail.com>
2024-10-22 09:54:36 +0200

    2664. The Knight’s Tour (dfs)

---
+++ problems/src/test/kotlin/arrays/medium/TheKnightsTourTest.kt
@@ -0,0 +1,32 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class TheKnightsTourTest {
+    @Test
+    fun test1() {
+        val m = 1
+        val n = 1
+        val r = 0
+        val c = 0
+        val expected = arrayOf(intArrayOf(0))
+
+        assertArrayEquals(expected, TheKnightsTour.tourOfKnight(m, n, r, c))
+    }
+
+    @Test
+    fun test2() {
+        val m = 3
+        val n = 4
+        val r = 0
+        val c = 0
+        val expected = arrayOf(
+            intArrayOf(0, 3, 6, 9),
+            intArrayOf(7, 10, 1, 4),
+            intArrayOf(2, 5, 8, 11),
+        )
+
+        assertArrayEquals(expected, TheKnightsTour.tourOfKnight(m, n, r, c))
+    }
+}
