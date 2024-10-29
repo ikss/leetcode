bbc8f24fffa078d78f0f6ce6c6038b33f95e3503
ikss <iksss.88@gmail.com>
2024-10-29 09:51:50 +0100

    265. Paint House II

---
+++ problems/src/main/kotlin/arrays/hard/PaintHousesII.kt
@@ -0,0 +1,42 @@
+package arrays.hard
+
+/**
+ * There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with
+ * a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+ *
+ * The cost of painting each house with a certain color is represented by an n x k cost matrix costs.
+ *
+ * For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1
+ * with color 2, and so on...
+ * Return the minimum cost to paint all houses.
+ *
+ * [URL](https://leetcode.com/problems/paint-house-ii/)
+ */
+object PaintHousesII {
+    fun minCostII(costs: Array<IntArray>): Int {
+        val n = costs.size
+        val k = costs[0].size
+
+        val dp = Array(n) { IntArray(k) { Int.MAX_VALUE } }
+
+        for (i in 0 until k) {
+            dp[0][i] = costs[0][i]
+        }
+
+        for (house in 1 until n) {
+            for (paint in 0 until k) {
+                for (prevPaint in 0 until k) {
+                    if (paint == prevPaint) continue
+
+                    dp[house][paint] = minOf(dp[house][paint], dp[house - 1][prevPaint] + costs[house][paint])
+                }
+            }
+        }
+        var result = Int.MAX_VALUE
+        for (paint in costs[0].indices) {
+            result = minOf(result, dp[n - 1][paint])
+        }
+
+        return result
+    }
+}
---
+++ problems/src/test/kotlin/arrays/hard/PaintHousesIITest.kt
@@ -0,0 +1,28 @@
+package arrays.hard
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class PaintHousesIITest {
+    @Test
+    fun test1() {
+        val costs = arrayOf(
+            intArrayOf(1, 5, 3),
+            intArrayOf(2, 9, 4),
+        )
+        val expected = 5
+        
+        assertEquals(expected, PaintHousesII.minCostII(costs))
+    }
+    
+    @Test
+    fun test2() {
+        val costs = arrayOf(
+            intArrayOf(1, 3),
+            intArrayOf(2, 4),
+        )
+        val expected = 5
+        
+        assertEquals(expected, PaintHousesII.minCostII(costs))
+    }
+}
