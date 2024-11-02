a0b47f5dc14d22b98bca0f8942659f5488e6ef85
ikss <iksss.88@gmail.com>
2024-11-02 16:33:03 +0100

    2490. Circular Sentence

---
+++ problems/src/test/kotlin/strings/easy/CircularSentenceTest.kt
@@ -0,0 +1,30 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CircularSentenceTest {
+    @Test
+    fun test1() {
+        val sentence = "leetcode exercises sound delightful"
+        val expected = true
+
+        assertEquals(expected, CircularSentence.isCircularSentence(sentence))
+    }
+
+    @Test
+    fun test2() {
+        val sentence = "eetcode"
+        val expected = true
+
+        assertEquals(expected, CircularSentence.isCircularSentence(sentence))
+    }
+
+    @Test
+    fun test3() {
+        val sentence = "Leetcode is cool"
+        val expected = false
+
+        assertEquals(expected, CircularSentence.isCircularSentence(sentence))
+    }
+}
