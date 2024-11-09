14e134ee45313ffe4dc538ef2b7cb379db9f1026
ikss <iksss.88@gmail.com>
2024-11-09 18:28:42 +0100

    3133. Minimum Array End

---
+++ problems/src/test/kotlin/numbers/medium/MinimumArrayEndTest.kt
@@ -0,0 +1,24 @@
+package numbers.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumArrayEndTest {
+    @Test
+    fun test1() {
+        val n = 3
+        val x = 4
+        val expected = 6
+
+        assertEquals(expected, MinimumArrayEnd.minEnd(n, x))
+    }
+
+    @Test
+    fun test2() {
+        val n = 2
+        val x = 7
+        val expected = 15
+
+        assertEquals(expected, MinimumArrayEnd.minEnd(n, x))
+    }
+}
