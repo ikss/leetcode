6fbed27c79aaf8b96d252dbdbae4f87540ed1807
ikss <iksss.88@gmail.com>
2024-11-10 10:12:53 +0100

    Fix tests

--- problems/src/test/kotlin/arrays/medium/FindIfArrayCanBeSortedTest.kt
+++ problems/src/test/kotlin/arrays/medium/FindIfArrayCanBeSortedTest.kt
@@ -6,24 +6,16 @@ import org.junit.jupiter.api.Test
 class FindIfArrayCanBeSortedTest {
     @Test
     fun test1() {
-        val input = intArrayOf(2, 1)
-        val output = false
+        val input = intArrayOf(1, 2, 3, 4, 5)
+        val output = true
 
         assertEquals(output, FindIfArrayCanBeSorted.canSortArray(input))
     }
 
     @Test
     fun test2() {
-        val input = intArrayOf(3, 4, 2, 3)
+        val input = intArrayOf(3,16,8,4,2)
         val output = false
-
-        assertEquals(output, FindIfArrayCanBeSorted.canSortArray(input))
-    }
-
-    @Test
-    fun test3() {
-        val input = intArrayOf(4, 2, 3)
-        val output = true
 
         assertEquals(output, FindIfArrayCanBeSorted.canSortArray(input))
     }
--- problems/src/test/kotlin/arrays/medium/MaximumXORForEachQueryTest.kt
+++ problems/src/test/kotlin/arrays/medium/MaximumXORForEachQueryTest.kt
@@ -1,6 +1,6 @@
 package arrays.medium
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
 
 class MaximumXORForEachQueryTest {
@@ -8,7 +8,7 @@ class MaximumXORForEachQueryTest {
     fun test1() {
         val nums = intArrayOf(0, 1, 1, 3)
         val maximumBit = 2
-        val expected = intArrayOf(3, 3, 3, 3, 7)
+        val expected = intArrayOf(0, 3, 2, 3)
 
         assertArrayEquals(expected, MaximumXORForEachQuery.getMaximumXor(nums, maximumBit))
     }
@@ -17,7 +17,7 @@ class MaximumXORForEachQueryTest {
     fun test2() {
         val nums = intArrayOf(2, 3, 4, 7)
         val maximumBit = 3
-        val expected = intArrayOf(5, 2, 3, 4, 7)
+        val expected = intArrayOf(5, 2, 6, 5)
 
         assertArrayEquals(expected, MaximumXORForEachQuery.getMaximumXor(nums, maximumBit))
     }
@@ -26,7 +26,7 @@ class MaximumXORForEachQueryTest {
     fun test3() {
         val nums = intArrayOf(0, 1, 2, 2, 5, 7)
         val maximumBit = 3
-        val expected = intArrayOf(7, 5, 2, 2, 5, 7, 7)
+        val expected = intArrayOf(4, 3, 6, 4, 6, 7)
 
         assertArrayEquals(expected, MaximumXORForEachQuery.getMaximumXor(nums, maximumBit))
     }
