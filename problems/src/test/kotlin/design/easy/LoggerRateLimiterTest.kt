e460ddb2241aa1e7f066b45b5f0bec99b487b462
ikss <iksss.88@gmail.com>
2024-11-18 15:44:28 +0100

    359. Logger Rate Limiter

---
+++ problems/src/test/kotlin/design/easy/LoggerRateLimiterTest.kt
@@ -0,0 +1,33 @@
+package design.easy
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class LoggerRateLimiterTest {
+    @Test
+    fun test1LoggerMap() {
+        val loggerRateLimiter = LoggerRateLimiter.LoggerMap()
+        assertTrue(loggerRateLimiter.shouldPrintMessage(1, "foo"))
+        assertTrue(loggerRateLimiter.shouldPrintMessage(2, "bar"))
+
+        assertFalse(loggerRateLimiter.shouldPrintMessage(3, "foo"))
+        assertFalse(loggerRateLimiter.shouldPrintMessage(8, "bar"))
+        assertFalse(loggerRateLimiter.shouldPrintMessage(10, "foo"))
+
+        assertTrue(loggerRateLimiter.shouldPrintMessage(11, "foo"))
+    }
+
+    @Test
+    fun test1LoggerQueue() {
+        val loggerRateLimiter = LoggerRateLimiter.LoggerQueue()
+        assertTrue(loggerRateLimiter.shouldPrintMessage(1, "foo"))
+        assertTrue(loggerRateLimiter.shouldPrintMessage(2, "bar"))
+
+        assertFalse(loggerRateLimiter.shouldPrintMessage(3, "foo"))
+        assertFalse(loggerRateLimiter.shouldPrintMessage(8, "bar"))
+        assertFalse(loggerRateLimiter.shouldPrintMessage(10, "foo"))
+
+        assertTrue(loggerRateLimiter.shouldPrintMessage(11, "foo"))
+    }
+}
