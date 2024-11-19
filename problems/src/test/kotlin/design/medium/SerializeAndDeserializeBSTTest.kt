7e264edf4472e7d66e6fbb5becb2086d30eb5405
ikss <iksss.88@gmail.com>
2024-11-19 09:06:50 +0100

    449. Serialize and Deserialize BST

---
+++ problems/src/test/kotlin/design/medium/SerializeAndDeserializeBSTTest.kt
@@ -0,0 +1,27 @@
+package design.medium
+
+import data_structures.TreeBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SerializeAndDeserializeBSTTest {
+    @Test
+    fun test1() {
+        val root = TreeBuilder.build(2, 1, 3)
+        val codec = SerializeAndDeserializeBST.Codec()
+        val serialized = codec.serialize(root)
+
+        assertEquals("2,1,3,null,null,null,null", serialized)
+        assertEquals(root, codec.deserialize(serialized))
+    }
+
+    @Test
+    fun test2() {
+        val root = TreeBuilder.build()
+        val codec = SerializeAndDeserializeBST.Codec()
+        val serialized = codec.serialize(root)
+
+        assertEquals("", serialized)
+        assertEquals(root, codec.deserialize(serialized))
+    }
+}
