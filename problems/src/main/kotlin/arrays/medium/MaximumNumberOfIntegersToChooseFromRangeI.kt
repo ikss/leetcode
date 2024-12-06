03e1be13925c0e627d0d54dca7e9cd349afa4bea
ikss <iksss.88@gmail.com>
2024-12-06 17:45:54 +0100

    2554. Maximum Number of Integers to Choose From a Range I

---
+++ problems/src/main/kotlin/arrays/medium/MaximumNumberOfIntegersToChooseFromRangeI.kt
@@ -0,0 +1,28 @@
+package arrays.medium
+
+/**
+ * You are given an integer array banned and two integers n and maxSum.
+ * You are choosing some number of integers following the below rules:
+ * The chosen integers have to be in the range `[1, n]`.
+ * Each integer can be chosen at most once.
+ * The chosen integers should not be in the array banned.
+ * The sum of the chosen integers should not exceed maxSum.
+ *
+ * Return the maximum number of integers you can choose following the mentioned rules.
+ *
+ * [URL](https://leetcode.com/problems/maximum-number-of-integers-to-choose-from-a-range-i/)
+ */
+object MaximumNumberOfIntegersToChooseFromRangeI {
+    fun maxCount(banned: IntArray, n: Int, maxSum: Int): Int {
+        val bannedNums = banned.toSet()
+        var result = 0
+        var sum = 0
+        for (i in 1..n) {
+            if (i in bannedNums) continue
+            sum += i
+            if (sum > maxSum) break
+            result++
+        }
+        return result
+    }
+}
