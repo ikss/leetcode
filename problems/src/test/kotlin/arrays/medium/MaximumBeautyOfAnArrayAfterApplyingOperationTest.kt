b74940d064d4e70c9d6020d9896ff9e1a6e53c6b
ikss <iksss.88@gmail.com>
2024-12-11 18:55:52 +0100

    2779. Maximum Beauty of an Array After Applying Operation

---
+++ problems/src/test/kotlin/arrays/medium/MaximumBeautyOfAnArrayAfterApplyingOperationTest.kt
@@ -0,0 +1,24 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumBeautyOfAnArrayAfterApplyingOperationTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(4, 6, 1, 2)
+        val k = 2
+        val expected = 3
+
+        assertEquals(expected, MaximumBeautyOfAnArrayAfterApplyingOperation.maximumBeauty(nums, k))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(1, 1, 1, 1)
+        val k = 10
+        val expected = 4
+
+        assertEquals(expected, MaximumBeautyOfAnArrayAfterApplyingOperation.maximumBeauty(nums, k))
+    }
+}
