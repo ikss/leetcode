4f8ea4608cc132680df7c381a9e50c1bd73fefb1
ikss <iksss.88@gmail.com>
2025-01-03 09:57:10 +0100

    2270. Number of Ways to Split Array

---
+++ problems/src/test/kotlin/arrays/medium/NumberOfWaysToSplitArrayTest.kt
@@ -0,0 +1,22 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NumberOfWaysToSplitArrayTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(10, 4, -8, 7)
+        val expected = 2
+
+        assertEquals(expected, NumberOfWaysToSplitArray.waysToSplitArray(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(2, 3, 1, 0)
+        val expected = 2
+
+        assertEquals(expected, NumberOfWaysToSplitArray.waysToSplitArray(nums))
+    }
+}
