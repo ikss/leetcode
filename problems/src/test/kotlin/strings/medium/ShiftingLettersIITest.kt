69a006de87da5e0503c1d6a90e61ec96ee6abea6
ikss <iksss.88@gmail.com>
2025-01-05 20:43:31 +0100

    2381. Shifting Letters II

---
+++ problems/src/test/kotlin/strings/medium/ShiftingLettersIITest.kt
@@ -0,0 +1,24 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ShiftingLettersIITest {
+    @Test
+    fun test1() {
+        val s = "abc"
+        val shifts = arrayOf(intArrayOf(0, 1, 0), intArrayOf(1, 2, 1), intArrayOf(0, 2, 1))
+
+        val expected = "ace"
+        assertEquals(expected, ShiftingLettersII.shiftingLetters(s, shifts))
+    }
+
+    @Test
+    fun test2() {
+        val s = "dztz"
+        val shifts = arrayOf(intArrayOf(0, 0, 0), intArrayOf(1, 1, 1))
+
+        val expected = "catz"
+        assertEquals(expected, ShiftingLettersII.shiftingLetters(s, shifts))
+    }
+}
