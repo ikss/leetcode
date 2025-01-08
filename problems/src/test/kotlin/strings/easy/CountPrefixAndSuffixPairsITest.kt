4c02e1c471d360f1a3d595c34027f1ab7c894084
ikss <iksss.88@gmail.com>
2025-01-08 17:26:23 +0100

    3042. Count Prefix and Suffix Pairs I

---
+++ problems/src/test/kotlin/strings/easy/CountPrefixAndSuffixPairsITest.kt
@@ -0,0 +1,30 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CountPrefixAndSuffixPairsITest {
+    @Test
+    fun test1() {
+        val words = arrayOf("a", "aba", "ababa", "aa")
+        val expected = 4
+
+        assertEquals(expected, CountPrefixAndSuffixPairsI.countPrefixSuffixPairs(words))
+    }
+
+    @Test
+    fun test2() {
+        val words = arrayOf("pa", "papa", "ma", "mama")
+        val expected = 2
+
+        assertEquals(expected, CountPrefixAndSuffixPairsI.countPrefixSuffixPairs(words))
+    }
+
+    @Test
+    fun test3() {
+        val words = arrayOf("abab", "ab")
+        val expected = 0
+
+        assertEquals(expected, CountPrefixAndSuffixPairsI.countPrefixSuffixPairs(words))
+    }
+}
