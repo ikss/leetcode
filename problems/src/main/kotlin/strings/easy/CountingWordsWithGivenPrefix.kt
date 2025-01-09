21903b00e5cc783d0e5e88a38fc01ec50b04ce31
ikss <iksss.88@gmail.com>
2025-01-09 14:05:55 +0100

    2185. Counting Words With a Given Prefix (trie)

--- problems/src/main/kotlin/strings/easy/CountingWordsWithGivenPrefix.kt
+++ problems/src/main/kotlin/strings/easy/CountingWordsWithGivenPrefix.kt
@@ -10,7 +10,42 @@ package strings.easy
  * [URL](https://leetcode.com/problems/counting-words-with-a-given-prefix/)
  */
 object CountingWordsWithGivenPrefix {
-    fun prefixCount(words: Array<String>, pref: String): Int {
+    fun prefixCountBuildIn(words: Array<String>, pref: String): Int {
         return words.count { it.startsWith(pref) }
+    }
+
+    fun prefixCountTrie(words: Array<String>, pref: String): Int {
+        val trie = Trie()
+
+        for (word in words) {
+            trie.insert(word)
+        }
+
+        return trie.countPrefix(pref)
+    }
+
+    private class Trie {
+        private class TrieNode {
+            val children: MutableMap<Char, TrieNode> = mutableMapOf()
+            var count: Int = 0
+        }
+
+        private val root = TrieNode()
+
+        fun insert(word: String) {
+            var node = root
+            for (char in word) {
+                node = node.children.computeIfAbsent(char) { TrieNode() }
+                node.count++
+            }
+        }
+
+        fun countPrefix(prefix: String): Int {
+            var node = root
+            for (char in prefix) {
+                node = node.children[char] ?: return 0
+            }
+            return node.count
+        }
     }
 }
