52083bc9712055723919044959c3f9c7c1cd212d
ikss <iksss.88@gmail.com>
2025-01-19 17:34:39 +0100

    1368. Minimum Cost to Make at Least One Valid Path in a Grid (priority queue)

--- problems/src/main/kotlin/arrays/hard/TrappingRainWaterII.kt
+++ problems/src/main/kotlin/arrays/hard/TrappingRainWaterII.kt
@@ -1,5 +1,8 @@
 package arrays.hard
 
+import java.util.*
+
+
 /**
  * Given an m x n integer matrix heightMap representing the height of each unit cell in a 2D elevation map,
  * return the volume of water it can trap after raining.
@@ -22,7 +25,7 @@ object TrappingRainWaterII {
         intArrayOf(1, 0, 0),
     )
 
-    fun trapRainWater(heightMap: Array<IntArray>): Int {
+    fun trapRainWaterBfs(heightMap: Array<IntArray>): Int {
         val m = heightMap.size
         val n = heightMap[0].size
         val maxHeight = heightMap.maxOf { it.max() }
@@ -127,5 +130,79 @@ object TrappingRainWaterII {
             }
         }
         return count
+    }
+
+    fun trapRainWaterPriorityQueue(heightMap: Array<IntArray>): Int {
+        // Direction arrays
+        val dRow = intArrayOf(0, 0, -1, 1)
+        val dCol = intArrayOf(-1, 1, 0, 0)
+
+        val numOfRows = heightMap.size
+        val numOfCols = heightMap[0].size
+
+        val visited = Array(numOfRows) { BooleanArray(numOfCols) }
+
+        // Priority queue (min-heap) to process boundary cells in increasing height order
+        val boundary = PriorityQueue<Cell>()
+
+        // Add the first and last column cells to the boundary and mark them as visited
+        for (i in 0 until numOfRows) {
+            boundary.offer(Cell(heightMap[i][0], i, 0))
+            boundary.offer(Cell(heightMap[i][numOfCols - 1], i, numOfCols - 1))
+            // Mark left and right boundary cells as visited
+            visited[i][numOfCols - 1] = true
+            visited[i][0] = visited[i][numOfCols - 1]
+        }
+
+        // Add the first and last row cells to the boundary and mark them as visited
+        for (i in 0 until numOfCols) {
+            boundary.offer(Cell(heightMap[0][i], 0, i))
+            boundary.offer(Cell(heightMap[numOfRows - 1][i], numOfRows - 1, i))
+            // Mark top and bottom boundary cells as visited
+            visited[numOfRows - 1][i] = true
+            visited[0][i] = visited[numOfRows - 1][i]
+        }
+
+        var result = 0
+
+        while (!boundary.isEmpty()) {
+            val currentCell = boundary.poll()
+
+            val currentRow = currentCell.row
+            val currentCol = currentCell.col
+            val minBoundaryHeight = currentCell.height
+
+            for (direction in 0..3) {
+                val nrow = currentRow + dRow[direction]
+                val ncol = currentCol + dCol[direction]
+
+                if (nrow in heightMap.indices && ncol in heightMap[0].indices && !visited[nrow][ncol]) {
+                    // Get the height of the neighbor cell
+                    val neighborHeight = heightMap[nrow][ncol]
+
+                    // If the neighbor's height is less than the current boundary height, water can be trapped
+                    if (neighborHeight < minBoundaryHeight) {
+                        // Add the trapped water volume
+                        result += minBoundaryHeight - neighborHeight
+                    }
+
+                    // Push the neighbor into the boundary with updated height (to prevent water leakage)
+                    boundary.offer(Cell(maxOf(neighborHeight, minBoundaryHeight), nrow, ncol))
+                    visited[nrow][ncol] = true
+                }
+            }
+        }
+
+        // Return the total amount of trapped water
+        return result
+    }
+
+    // Class to store the height and coordinates of a cell in the grid
+    private data class Cell(var height: Int, var row: Int, var col: Int) : Comparable<Cell> {
+        // Overload the compareTo method to make the priority queue a min-heap based on height
+        override fun compareTo(other: Cell): Int {
+            // Min-heap comparison
+            return this.height.compareTo(other.height)
+        }
     }
 }
