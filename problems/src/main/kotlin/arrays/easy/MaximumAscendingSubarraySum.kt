ff480552389178836dcda40e569f2e01ed212774
ikss <iksss.88@gmail.com>
2025-02-04 22:21:33 +0100

    1800. Maximum Ascending Subarray Sum

---
+++ problems/src/main/kotlin/arrays/easy/MaximumAscendingSubarraySum.kt
@@ -0,0 +1,30 @@
+package arrays.easy
+
+/**
+ * Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
+ *
+ * A subarray is defined as a contiguous sequence of numbers in an array.
+ *
+ * A subarray `[numsl, numsl+1, ..., numsr-1, numsr]` is ascending if for all i where l <= i < r, numsi  < numsi+1.
+ * Note that a subarray of size 1 is ascending.
+ *
+ * [URL](https://leetcode.com/problems/maximum-ascending-subarray-sum/)
+ */
+object MaximumAscendingSubarraySum {
+    fun maxAscendingSum(nums: IntArray): Int {
+        var curr = nums[0]
+        var result = nums[0]
+
+        for (i in 1 until nums.size) {
+            if (nums[i] > nums[i - 1]) {
+                curr += nums[i]
+            } else {
+                curr = nums[i]
+            }
+
+            result = maxOf(result, curr)
+        }
+
+        return result
+    }
+}
---
+++ problems/src/test/kotlin/arrays/easy/MaximumAscendingSubarraySumTest.kt
@@ -0,0 +1,30 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumAscendingSubarraySumTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(10, 20, 30, 5, 10, 50)
+        val expected = 65
+
+        assertEquals(expected, MaximumAscendingSubarraySum.maxAscendingSum(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(10, 20, 30, 40, 50)
+        val expected = 150
+
+        assertEquals(expected, MaximumAscendingSubarraySum.maxAscendingSum(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(12, 17, 15, 13, 10, 11, 12)
+        val expected = 33
+
+        assertEquals(expected, MaximumAscendingSubarraySum.maxAscendingSum(nums))
+    }
+}
