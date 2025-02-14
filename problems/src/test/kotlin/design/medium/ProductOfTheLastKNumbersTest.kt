889e32284fa963ae30c1fe4ec9f5bc00a61b07b9
ikss <iksss.88@gmail.com>
2025-02-14 17:28:49 +0100

    1352. Product of the Last K Numbers

---
+++ problems/src/test/kotlin/design/medium/ProductOfTheLastKNumbersTest.kt
@@ -0,0 +1,21 @@
+package design.medium
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ProductOfTheLastKNumbersTest {
+    @Test
+    fun test1() {
+        val productOfTheLastKNumbers = ProductOfTheLastKNumbers.ProductOfNumbers()
+        productOfTheLastKNumbers.add(3)
+        productOfTheLastKNumbers.add(0)
+        productOfTheLastKNumbers.add(2)
+        productOfTheLastKNumbers.add(5)
+        productOfTheLastKNumbers.add(4)
+        assertEquals(20, productOfTheLastKNumbers.getProduct(2))
+        assertEquals(40, productOfTheLastKNumbers.getProduct(3))
+        assertEquals(0, productOfTheLastKNumbers.getProduct(4))
+        productOfTheLastKNumbers.add(8)
+        assertEquals(32, productOfTheLastKNumbers.getProduct(2))
+    }
+}
