18ebd5ecc5f091906b1688f2c1bd9afa1b77a9d4
ikss <iksss.88@gmail.com>
2025-02-16 20:35:34 +0100

    1718. Construct the Lexicographically Largest Valid Sequence

---
+++ problems/src/test/kotlin/arrays/medium/ConstructTheLexicographicallyLargestValidSequenceTest.kt
@@ -0,0 +1,22 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class ConstructTheLexicographicallyLargestValidSequenceTest {
+    @Test
+    fun test1() {
+        val n = 3
+        val expected = intArrayOf(3, 1, 2, 3, 2)
+
+        assertArrayEquals(expected, ConstructTheLexicographicallyLargestValidSequence.constructDistancedSequence(n))
+    }
+
+    @Test
+    fun test2() {
+        val n = 5
+        val expected = intArrayOf(5, 3, 1, 4, 3, 5, 2, 4, 2)
+
+        assertArrayEquals(expected, ConstructTheLexicographicallyLargestValidSequence.constructDistancedSequence(n))
+    }
+}
