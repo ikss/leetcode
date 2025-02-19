fbfe1e296a982ddd51d9f8bc5aec28e724ead81c
ikss <iksss.88@gmail.com>
2025-02-19 11:50:06 +0100

    1415. The k-th Lexicographical String of All Happy Strings of Length n

---
+++ problems/src/test/kotlin/strings/medium/TheKthLexicographicalStringOfAllHappyStringsOfLengthNTest.kt
@@ -0,0 +1,33 @@
+package strings.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TheKthLexicographicalStringOfAllHappyStringsOfLengthNTest {
+    @Test
+    fun test1() {
+        val n = 1
+        val k = 3
+        val expected = "c"
+
+        assertEquals(expected, TheKthLexicographicalStringOfAllHappyStringsOfLengthN.getHappyString(n, k))
+    }
+
+    @Test
+    fun test2() {
+        val n = 1
+        val k = 4
+        val expected = ""
+
+        assertEquals(expected, TheKthLexicographicalStringOfAllHappyStringsOfLengthN.getHappyString(n, k))
+    }
+
+    @Test
+    fun test3() {
+        val n = 3
+        val k = 9
+        val expected = "cab"
+
+        assertEquals(expected, TheKthLexicographicalStringOfAllHappyStringsOfLengthN.getHappyString(n, k))
+    }
+}
