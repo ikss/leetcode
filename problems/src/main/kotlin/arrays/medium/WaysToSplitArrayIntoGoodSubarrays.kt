bf939793416afba583506b2726c1fe7f6aadb66d
ikss <iksss.88@gmail.com>
2025-02-20 18:49:36 +0100

    2750. Ways to Split Array Into Good Subarrays

---
+++ problems/src/main/kotlin/arrays/medium/WaysToSplitArrayIntoGoodSubarrays.kt
@@ -0,0 +1,36 @@
+package arrays.medium
+
+/**
+ * You are given a binary array nums.
+ *
+ * A subarray of an array is good if it contains exactly one element with the value 1.
+ *
+ * Return an integer denoting the number of ways to split the array nums into good subarrays.
+ * As the number may be too large, return it modulo 109 + 7.
+ *
+ * A subarray is a contiguous non-empty sequence of elements within an array.
+ *
+ * [URL](https://leetcode.com/problems/ways-to-split-array-into-good-subarrays/)
+ */
+object WaysToSplitArrayIntoGoodSubarrays {
+    fun numberOfGoodSubarraySplits(nums: IntArray): Int {
+        var zeroesBetween = 0
+        var oneSeen = false
+        var result = 1L
+
+        for (n in nums) {
+            if (n == 0) {
+                if (oneSeen) {
+                    zeroesBetween++
+                }
+                continue
+            }
+            oneSeen = true
+            result *= zeroesBetween + 1
+            result %= 1_000_000_007
+            zeroesBetween = 0
+        }
+
+        return if (oneSeen) result.toInt() else 0
+    }
+}
---
+++ problems/src/test/kotlin/arrays/medium/WaysToSplitArrayIntoGoodSubarraysTest.kt
@@ -0,0 +1,22 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class WaysToSplitArrayIntoGoodSubarraysTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(0, 1, 0, 0, 1)
+        val expected = 3
+
+        assertEquals(expected, WaysToSplitArrayIntoGoodSubarrays.numberOfGoodSubarraySplits(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(0, 1, 0)
+        val expected = 1
+
+        assertEquals(expected, WaysToSplitArrayIntoGoodSubarrays.numberOfGoodSubarraySplits(nums))
+    }
+}
