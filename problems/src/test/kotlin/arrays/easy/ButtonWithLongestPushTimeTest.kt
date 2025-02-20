5a01a29be577d13c537cb018073db44b6d37806b
ikss <iksss.88@gmail.com>
2025-02-20 18:26:07 +0100

    3386. Button with Longest Push Time

---
+++ problems/src/test/kotlin/arrays/easy/ButtonWithLongestPushTimeTest.kt
@@ -0,0 +1,36 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ButtonWithLongestPushTimeTest {
+    @Test
+    fun test1() {
+        val events = arrayOf(intArrayOf(1, 2), intArrayOf(2, 5), intArrayOf(3, 9), intArrayOf(1, 15))
+        val expected = 1
+
+        assertEquals(expected, ButtonWithLongestPushTime.buttonWithLongestTime(events))
+    }
+
+    @Test
+    fun test2() {
+        val events = arrayOf(intArrayOf(10, 25), intArrayOf(1, 7))
+        val expected = 10
+
+        assertEquals(expected, ButtonWithLongestPushTime.buttonWithLongestTime(events))
+    }
+
+    @Test
+    fun test3() {
+        val events = arrayOf(
+            intArrayOf(9, 4),
+            intArrayOf(19, 5),
+            intArrayOf(2, 8),
+            intArrayOf(3, 11),
+            intArrayOf(2, 15),
+        )
+        val expected = 2
+
+        assertEquals(expected, ButtonWithLongestPushTime.buttonWithLongestTime(events))
+    }
+}
