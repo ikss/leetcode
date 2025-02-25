6860d3d4ce5d2f05ce31b866ad5d7cf860474327
ikss <iksss.88@gmail.com>
2025-02-25 10:30:31 +0100

    1524. Number of Sub-arrays With Odd Sum

---
+++ problems/src/test/kotlin/arrays/medium/NumberOfSubArraysWithOddSumTest.kt
@@ -0,0 +1,30 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NumberOfSubArraysWithOddSumTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, 3, 5)
+        val expected = 4
+
+        assertEquals(expected, NumberOfSubArraysWithOddSum.numOfSubarrays(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(2, 4, 6)
+        val expected = 0
+
+        assertEquals(expected, NumberOfSubArraysWithOddSum.numOfSubarrays(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+        val expected = 16
+
+        assertEquals(expected, NumberOfSubArraysWithOddSum.numOfSubarrays(nums))
+    }
+}
