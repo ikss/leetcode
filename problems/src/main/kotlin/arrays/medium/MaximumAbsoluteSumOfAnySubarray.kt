908cb150725c9e44cc3a7c8af41eb5ca030fbe8a
ikss <iksss.88@gmail.com>
2025-02-26 22:37:06 +0100

    1749. Maximum Absolute Sum of Any Subarray (shorter)

--- problems/src/main/kotlin/arrays/medium/MaximumAbsoluteSumOfAnySubarray.kt
+++ problems/src/main/kotlin/arrays/medium/MaximumAbsoluteSumOfAnySubarray.kt
@@ -19,19 +19,13 @@ object MaximumAbsoluteSumOfAnySubarray {
         var min = Int.MAX_VALUE
         var max = Int.MIN_VALUE
         var cur = 0
-        var res = 0
 
         for (n in nums) {
             cur += n
             min = minOf(min, cur)
             max = maxOf(max, cur)
-            res = if (cur >= 0) {
-                maxOf(res, cur, cur - min)
-            } else {
-                maxOf(res, abs(cur), abs(cur - max))
-            }
         }
 
-        return res
+        return max - min
     }
 }
