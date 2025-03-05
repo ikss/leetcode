a7cd2d11240407cf142d3082792685d435374732
ikss <iksss.88@gmail.com>
2025-03-05 10:09:59 +0100

    2579. Count Total Number of Colored Cells

---
+++ problems/src/test/kotlin/numbers/medium/CountTotalNumberOfColoredCellsTest.kt
@@ -0,0 +1,38 @@
+package numbers.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CountTotalNumberOfColoredCellsTest {
+    @Test
+    fun test1Simulation() {
+        val n = 1
+        val expected = 1L
+
+        assertEquals(expected, CountTotalNumberOfColoredCells.coloredCellsSimulation(n))
+    }
+
+    @Test
+    fun test2Simulation() {
+        val n = 2
+        val expected = 5L
+
+        assertEquals(expected, CountTotalNumberOfColoredCells.coloredCellsSimulation(n))
+    }
+
+    @Test
+    fun test1Numeric() {
+        val n = 1
+        val expected = 1L
+
+        assertEquals(expected, CountTotalNumberOfColoredCells.coloredCellsNumeric(n))
+    }
+
+    @Test
+    fun test2Numeric() {
+        val n = 2
+        val expected = 5L
+
+        assertEquals(expected, CountTotalNumberOfColoredCells.coloredCellsNumeric(n))
+    }
+}
