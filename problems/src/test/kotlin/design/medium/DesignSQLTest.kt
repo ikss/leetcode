e0f1a19fcd236701c826a2188a1b2a9e708c1170
ikss <iksss.88@gmail.com>
2025-03-12 12:12:21 +0100

    2408. Design SQL

---
+++ problems/src/test/kotlin/design/medium/DesignSQLTest.kt
@@ -0,0 +1,22 @@
+package design.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class DesignSQLTest {
+    @Test
+    fun test1() {
+        val sql = DesignSQL.SQL(
+            listOf("one", "two", "three"),
+            listOf(2, 3, 1),
+        )
+        assertEquals(true, sql.ins("two", listOf("first", "second", "third")))
+        assertEquals("third", sql.sel("two", 1, 3))
+        assertEquals(true, sql.ins("two", listOf("fourth", "fifth", "sixth")))
+        assertEquals(listOf("1,first,second,third", "2,fourth,fifth,sixth"), sql.exp("two"))
+        sql.rmv("two", 1)
+        assertEquals("fifth", sql.sel("two", 2, 2))
+        assertEquals(listOf("2,fourth,fifth,sixth"), sql.exp("two"))
+
+    }
+}
