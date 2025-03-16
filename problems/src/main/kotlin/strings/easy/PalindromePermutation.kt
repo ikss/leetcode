8f8dc9574a0c52534462824ae93e32b24d8a55b1
ikss <iksss.88@gmail.com>
2025-03-16 07:16:35 +0100

    266. Palindrome Permutation

---
+++ problems/src/main/kotlin/strings/easy/PalindromePermutation.kt
@@ -0,0 +1,29 @@
+package strings.easy
+
+/**
+ * Given a string s, return true if a permutation of the string could form a palindrome and false otherwise.
+ *
+ * [URL](https://leetcode.com/problems/buddy-strings/)
+ */
+object PalindromePermutation {
+    fun canPermutePalindrome(s: String): Boolean {
+        val hash = IntArray(26)
+
+        for (c in s) {
+            hash[c - 'a']++
+        }
+
+        var oddSeen = false
+
+        for (count in hash) {
+            if (count % 2 != 0) {
+                if (oddSeen) {
+                    return false
+                }
+                oddSeen = true
+            }
+        }
+
+        return true
+    }
+}
---
+++ problems/src/test/kotlin/strings/easy/PalindromePermutationTest.kt
@@ -0,0 +1,30 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PalindromePermutationTest {
+    @Test
+    fun test1() {
+        val s = "code"
+        val expected = false
+
+        assertEquals(expected, PalindromePermutation.canPermutePalindrome(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "aab"
+        val expected = true
+
+        assertEquals(expected, PalindromePermutation.canPermutePalindrome(s))
+    }
+
+    @Test
+    fun test3() {
+        val s = "carerac"
+        val expected = true
+
+        assertEquals(expected, PalindromePermutation.canPermutePalindrome(s))
+    }
+}
