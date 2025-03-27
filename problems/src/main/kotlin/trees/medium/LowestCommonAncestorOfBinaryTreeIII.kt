2e2b75e0c383659a2ba2e1c96befdbe60387b728
ikss <iksss.88@gmail.com>
2025-03-27 05:47:24 +0100

    1650. Lowest Common Ancestor of a Binary Tree III

---
+++ problems/src/main/kotlin/trees/medium/LowestCommonAncestorOfBinaryTreeIII.kt
@@ -0,0 +1,54 @@
+package trees.medium
+
+/**
+ * Given two nodes of a binary tree p and q, return their lowest common ancestor (LCA).
+ *
+ * Each node will have a reference to its parent node. The definition for Node is below:
+ *
+ * class Node {
+ *     public int val;
+ *     public Node left;
+ *     public Node right;
+ *     public Node parent;
+ * }
+ *
+ * According to the definition of LCA on Wikipedia: "The lowest common ancestor of two nodes p and q in a tree T is
+ * the lowest node that has both p and q as descendants (where we allow a node to be a descendant of itself)."
+ *
+ * [URL](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/)
+ */
+object LowestCommonAncestorOfBinaryTreeIII {
+    data class Node(
+        val `val`: Int,
+        var left: Node? = null,
+        var right: Node? = null,
+        var parent: Node? = null,
+    )
+
+    fun lowestCommonAncestor(p: Node?, q: Node?): Node? {
+        if (p == null || q == null) return null
+
+        val parents1 = getParents(p)
+        val parents2 = getParents(q)
+
+        var i = 0
+        while (i < parents1.size && i < parents2.size && parents1[i] == parents2[i]) {
+            i++
+        }
+
+        return parents1[i - 1]
+    }
+
+    private fun getParents(node: Node): List<Node> {
+        val result = ArrayList<Node>()
+        result.add(node)
+
+        var node = node
+        while (node.parent != null) {
+            result.add(node.parent!!)
+            node = node.parent!!
+        }
+
+        return result.reversed()
+    }
+}
