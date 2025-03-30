610b5b680277abfd4ea1d47619e6ee7d0d0b4df3
ikss <iksss.88@gmail.com>
2025-03-30 19:13:08 +0200

    2818. Apply Operations to Maximize Score

---
+++ problems/src/test/kotlin/arrays/hard/ApplyOperationsToMaximizeScoreTest.kt
@@ -0,0 +1,24 @@
+package arrays.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ApplyOperationsToMaximizeScoreTest {
+    @Test
+    fun test1() {
+        val nums = listOf(8, 3, 9, 3, 8)
+        val k = 2
+        val expected = 81
+
+        assertEquals(expected, ApplyOperationsToMaximizeScore.maximumScore(nums, k))
+    }
+
+    @Test
+    fun test2() {
+        val nums = listOf(19, 12, 14, 6, 10, 18)
+        val k = 3
+        val expected = 4788
+
+        assertEquals(expected, ApplyOperationsToMaximizeScore.maximumScore(nums, k))
+    }
+}
