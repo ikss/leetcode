a8f2cb56ebe009e548d996bbd935fc48945c21fd
ikss <iksss.88@gmail.com>
2025-04-14 20:36:00 +0200

    1534. Count Good Triplets (optimized)

--- problems/src/main/kotlin/arrays/easy/CountGoodTriplets.kt
+++ problems/src/main/kotlin/arrays/easy/CountGoodTriplets.kt
@@ -1,5 +1,7 @@
 package arrays.easy
 
+import kotlin.math.abs
+
 /**
  * Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
  *
@@ -16,7 +18,7 @@ package arrays.easy
  * [URL](https://leetcode.com/problems/count-good-triplets/)
  */
 object CountGoodTriplets {
-    fun countGoodTriplets(arr: IntArray, a: Int, b: Int, c: Int): Int {
+    fun countGoodTripletsBruteForce(arr: IntArray, a: Int, b: Int, c: Int): Int {
         var result = 0
 
         for (i in 0 until arr.size - 2) {
@@ -30,6 +32,40 @@ object CountGoodTriplets {
                         result++
                     }
                 }
+            }
+        }
+        return result
+    }
+
+    fun countGoodTripletsOptimized(arr: IntArray, a: Int, b: Int, c: Int): Int {
+        var result = 0
+        val n = arr.size
+        val sum = IntArray(1001)
+
+        for (num in arr[0]..1000) {
+            ++sum[num]
+        }
+
+        for (j in 1..<n-1) {
+            for (k in j + 1..<n) {
+                if (abs(arr[j] - arr[k]) > b) continue
+                val lj = arr[j] - a
+                val rj = arr[j] + a
+                val lk = arr[k] - c
+                val rk = arr[k] + c
+                val l = maxOf(0, lj, lk)
+                val r = minOf(1000, rj, rk)
+
+                if (l <= r) {
+                    result += if (l == 0) {
+                        sum[r]
+                    } else {
+                        sum[r] - sum[l - 1]
+                    }
+                }
+            }
+            for (k in arr[j]..1000) {
+                ++sum[k]
             }
         }
         return result
