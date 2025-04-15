500ccfdb1229ba180dea44877d38a1c537c7b805
ikss <iksss.88@gmail.com>
2025-04-15 18:53:00 +0200

    325. Maximum Size Subarray Sum Equals k

---
+++ problems/src/test/kotlin/arrays/medium/MaximumSizeSubarraySumEqualsKTest.kt
@@ -0,0 +1,24 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MaximumSizeSubarraySumEqualsKTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, -1, 5, -2, 3)
+        val k = 3
+        val expected = 4
+
+        assertEquals(expected, MaximumSizeSubarraySumEqualsK.maxSubArrayLen(nums, k))
+    }
+    
+    @Test
+    fun test2() {
+        val nums = intArrayOf(-2, -1, 2, 1)
+        val k = 1
+        val expected = 2
+
+        assertEquals(expected, MaximumSizeSubarraySumEqualsK.maxSubArrayLen(nums, k))
+    }
+}
