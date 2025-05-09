cf5a25ae2f41582d8554928aeba484c468394bf6
ikss <iksss.88@gmail.com>
2025-05-09 09:37:56 +0200

    3343. Count Number of Balanced Permutations

---
+++ problems/src/test/kotlin/arrays/hard/CountNumberOfBalancedPermutationsTest.kt
@@ -0,0 +1,22 @@
+package arrays.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CountNumberOfBalancedPermutationsTest {
+    @Test
+    fun test1() {
+        val num = "123"
+        val expected = 2
+
+        assertEquals(expected, CountNumberOfBalancedPermutations.countBalancedPermutations(num))
+    }
+
+    @Test
+    fun test2() {
+        val num = "112"
+        val expected = 1
+
+        assertEquals(expected, CountNumberOfBalancedPermutations.countBalancedPermutations(num))
+    }
+}
