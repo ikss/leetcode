9a63cbc2baae255881ad26651302cc0b5053c2a6
ikss <iksss.88@gmail.com>
2025-05-15 06:17:58 +0200

    361. Bomb Enemy

---
+++ problems/src/test/kotlin/arrays/medium/BombEnemyTest.kt
@@ -0,0 +1,31 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class BombEnemyTest {
+    @Test
+    fun test1() {
+        val grid = arrayOf(
+            charArrayOf('0', 'E', '0', '0'),
+            charArrayOf('0', '0', '0', '0'),
+            charArrayOf('0', '0', 'B', '0'),
+            charArrayOf('0', '0', '0', '0'),
+        )
+        val expected = 3
+
+        assertEquals(expected, BombEnemy.maxKilledEnemies(grid))
+    }
+
+    @Test
+    fun test2() {
+        val grid = arrayOf(
+            charArrayOf('W', 'W', 'W'),
+            charArrayOf('0', '0', '0'),
+            charArrayOf('E', 'E', 'E'),
+        )
+        val expected = 1
+
+        assertEquals(expected, BombEnemy.maxKilledEnemies(grid))
+    }
+}
