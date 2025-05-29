37615e4fa9728f5d61a8105f4df04d063797aaf9
ikss <iksss.88@gmail.com>
2025-05-29 09:02:04 +0200

    3373. Maximize the Number of Target Nodes After Connecting Trees II (DFS, TLE)

---
+++ problems/src/main/kotlin/trees/hard/MaximizeTheNumberOfTargetNodesAfterConnectingTreesII.kt
@@ -0,0 +1,63 @@
+package trees.hard
+
+/**
+ * There exist two undirected trees with n and m nodes, labeled from `[0, n - 1]` and `[0, m - 1]`, respectively.
+ *
+ * You are given two 2D integer arrays edges1 and edges2 of lengths n - 1 and m - 1, respectively,
+ * where `edges1[i] = [ai, bi]` indicates that there is an edge between nodes ai and bi in the first tree and
+ * `edges2[i] = [ui, vi]` indicates that there is an edge between nodes ui and vi in the second tree.
+ *
+ * Node u is target to node v if the number of edges on the path from u to v is even.
+ * Note that a node is always target to itself.
+ *
+ * Return an array of n integers answer, where `answer[i]` is the maximum possible number of nodes that are target to
+ * node i of the first tree if you had to connect one node from the first tree to another node in the second tree.
+ *
+ * Note that queries are independent from each other.
+ * That is, for every query you will remove the added edge before proceeding to the next query.
+ *
+ * [URL](https://leetcode.com/problems/maximize-the-number-of-target-nodes-after-connecting-trees-ii/)
+ */
+object MaximizeTheNumberOfTargetNodesAfterConnectingTreesII {
+    fun maxTargetNodesDfsTLE(edges1: Array<IntArray>, edges2: Array<IntArray>): IntArray {
+        val n = edges1.size + 1
+        val count1 = build(edges1, isEven = true)
+        val count2 = build(edges2, isEven = false)
+        var maxCount2 = 0
+        for (c in count2) {
+            if (c > maxCount2) {
+                maxCount2 = c
+            }
+        }
+        val result = IntArray(n)
+        for (i in 0..<n) {
+            result[i] = count1[i] + maxCount2
+        }
+        return result
+    }
+
+    private fun build(edges: Array<IntArray>, isEven: Boolean): IntArray {
+        val n = edges.size + 1
+        val children = Array<ArrayList<Int>>(n) { ArrayList() }
+        for (edge in edges) {
+            children[edge[0]].add(edge[1])
+            children[edge[1]].add(edge[0])
+        }
+        val res = IntArray(n)
+        for (i in 0..<n) {
+            res[i] = dfs(i, -1, children, isEven, 0)
+        }
+        return res
+    }
+
+    private fun dfs(node: Int, parent: Int, children: Array<ArrayList<Int>>, isEven: Boolean, index: Int): Int {
+        var result = if (isEven && index % 2 == 0) 1 else if (!isEven && index % 2 == 1) 1 else 0
+        for (child in children[node]) {
+            if (child == parent) {
+                continue
+            }
+            result += dfs(child, node, children, isEven, index + 1)
+        }
+        return result
+    }
+}
