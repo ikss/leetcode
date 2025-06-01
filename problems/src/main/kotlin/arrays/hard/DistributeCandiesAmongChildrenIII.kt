1c1b5e601fbee9df335da48bb8a732038c524040
ikss <iksss.88@gmail.com>
2025-06-01 08:03:03 +0200

    2927. Distribute Candies Among Children III

--- problems/src/main/kotlin/arrays/hard/DistributeCandiesAmongChildrenIII.kt
+++ problems/src/main/kotlin/arrays/hard/DistributeCandiesAmongChildrenIII.kt
@@ -12,7 +12,7 @@ object DistributeCandiesAmongChildrenIII {
     fun distributeCandies(n: Int, limit: Int): Long {
         return cal(n + 2) -
                 3 * cal(n - limit + 1) +
-                3 * cal(n - (limit + 1) * 2 + 2) -
+                3 * cal(n - 2 * (limit + 1) + 2) -
                 cal(n - 3 * (limit + 1) + 2)
 
     }
