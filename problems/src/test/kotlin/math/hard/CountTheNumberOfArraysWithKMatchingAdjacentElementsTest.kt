66d4a67b97badfe4ac41b24f43ebd6bd4c166e88
ikss <iksss.88@gmail.com>
2025-06-17 18:26:30 +0200

    3405. Count the Number of Arrays with K Matching Adjacent Elements

---
+++ problems/src/test/kotlin/math/hard/CountTheNumberOfArraysWithKMatchingAdjacentElementsTest.kt
@@ -0,0 +1,36 @@
+package math.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CountTheNumberOfArraysWithKMatchingAdjacentElementsTest {
+    @Test
+    fun test1() {
+        val n = 3
+        val m = 2
+        val k = 1
+        val expected = 4
+
+        assertEquals(expected, CountTheNumberOfArraysWithKMatchingAdjacentElements.countGoodArrays(n, m, k))
+    }
+
+    @Test
+    fun test2() {
+        val n = 4
+        val m = 2
+        val k = 2
+        val expected = 6
+
+        assertEquals(expected, CountTheNumberOfArraysWithKMatchingAdjacentElements.countGoodArrays(n, m, k))
+    }
+
+    @Test
+    fun test3() {
+        val n = 5
+        val m = 2
+        val k = 0
+        val expected = 2
+
+        assertEquals(expected, CountTheNumberOfArraysWithKMatchingAdjacentElements.countGoodArrays(n, m, k))
+    }
+}
