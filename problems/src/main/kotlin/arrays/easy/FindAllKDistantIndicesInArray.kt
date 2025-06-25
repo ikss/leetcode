e1631a33eff4820191fe4ced0e6e5782fd604f42
skuznetsov <iksss.88@gmail.com>
2025-06-25 20:24:42 +0200

    2200. Find All K-Distant Indices in an Array

---
+++ problems/src/main/kotlin/arrays/easy/FindAllKDistantIndicesInArray.kt
@@ -0,0 +1,28 @@
+package arrays.easy
+
+/**
+ * You are given a 0-indexed integer array nums and two integers key and k. A k-distant index is an index i of nums
+ * for which there exists at least one index j such that |i - j| <= k and `nums[j] == key`.
+ *
+ * Return a list of all k-distant indices sorted in increasing order.
+ *
+ * [URL](https://leetcode.com/problems/find-all-k-distant-indices-in-an-array/)
+ */
+object FindAllKDistantIndicesInArray {
+    fun findKDistantIndices(nums: IntArray, key: Int, k: Int): List<Int> {
+        val result = ArrayList<Int>()
+
+        for (i in nums.indices) {
+            if (nums[i] != key) continue
+
+            val left = maxOf((result.lastOrNull() ?: -1) + 1, i - k)
+            val right = minOf(nums.size - 1, i + k)
+            for (j in left .. right) {
+                result.add(j)
+            }
+        }
+
+
+        return result.toList()
+    }
+}
---
+++ problems/src/test/kotlin/arrays/easy/FindAllKDistantIndicesInArrayTest.kt
@@ -0,0 +1,26 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindAllKDistantIndicesInArrayTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(3, 4, 9, 1, 3, 9, 5)
+        val key = 9
+        val k = 1
+        val expected = listOf(1, 2, 3, 4, 5, 6)
+
+        assertEquals(expected, FindAllKDistantIndicesInArray.findKDistantIndices(nums, key, k))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(2, 2, 2, 2, 2)
+        val key = 2
+        val k = 2
+        val expected = listOf(0, 1, 2, 3, 4)
+
+        assertEquals(expected, FindAllKDistantIndicesInArray.findKDistantIndices(nums, key, k))
+    }
+}
