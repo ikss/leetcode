015cdb08491a10f99dcba91938f24f29b4e7bf1e
ikss <iksss.88@gmail.com>
2025-08-08 08:49:21 +0200

    3363. Find the Maximum Number of Fruits Collected

---
+++ problems/src/test/kotlin/arrays/hard/FindTheMaximumNumberOfFruitsCollectedTest.kt
@@ -0,0 +1,30 @@
+package arrays.hard
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindTheMaximumNumberOfFruitsCollectedTest {
+    @Test
+    fun test1() {
+        val fruits = arrayOf(
+            intArrayOf(1, 2, 3, 4),
+            intArrayOf(5, 6, 8, 7),
+            intArrayOf(9, 10, 11, 12),
+            intArrayOf(13, 14, 15, 16),
+        )
+        val expected = 100
+
+        assertEquals(expected, FindTheMaximumNumberOfFruitsCollected.maxCollectedFruits(fruits))
+    }
+
+    @Test
+    fun test2() {
+        val fruits = arrayOf(
+            intArrayOf(1, 1),
+            intArrayOf(1, 1),
+        )
+        val expected = 4
+
+        assertEquals(expected, FindTheMaximumNumberOfFruitsCollected.maxCollectedFruits(fruits))
+    }
+}
