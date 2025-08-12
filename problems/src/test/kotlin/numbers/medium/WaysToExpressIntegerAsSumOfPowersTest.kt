578227146b26d5e547d34b519bb6f4c9bbc069e0
ikss <iksss.88@gmail.com>
2025-08-12 09:32:06 +0200

    2787. Ways to Express an Integer as Sum of Powers

---
+++ problems/src/test/kotlin/numbers/medium/WaysToExpressIntegerAsSumOfPowersTest.kt
@@ -0,0 +1,24 @@
+package numbers.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class WaysToExpressIntegerAsSumOfPowersTest {
+    @Test
+    fun test1() {
+        val n = 10
+        val x = 2
+        val expected = 1
+
+        assertEquals(expected, WaysToExpressIntegerAsSumOfPowers.numberOfWays(n, x))
+    }
+
+    @Test
+    fun test2() {
+        val n = 4
+        val x = 1
+        val expected = 2
+
+        assertEquals(expected, WaysToExpressIntegerAsSumOfPowers.numberOfWays(n, x))
+    }
+}
