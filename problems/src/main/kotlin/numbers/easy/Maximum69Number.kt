8f18a42f95adc63bb500d1b07590a527d04b3a34
ikss <iksss.88@gmail.com>
2025-08-16 22:18:14 +0200

    1323. Maximum 69 Number

--- problems/src/main/kotlin/numbers/easy/Maximum69Number.kt
+++ problems/src/main/kotlin/numbers/easy/Maximum69Number.kt
@@ -4,10 +4,32 @@ package numbers.easy
  * You are climbing a staircase. It takes n steps to reach the top.
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
  *
- * [URL](https://leetcode.com/problems/climbing-stairs/)
+ * [URL](https://leetcode.com/problems/maximum-69-number/)
  */
 object Maximum69Number {
-    fun maximum69Number(num: Int): Int {
+    fun maximum69NumberStringConversion(num: Int): Int {
         return num.toString().replaceFirst('6', '9').toInt()
+    }
+
+    fun maximum69Number(num: Int): Int {
+        val left6 = findLeft6(num)
+        return num + left6
+    }
+
+    private fun findLeft6(num: Int): Int {
+        var pos = 0
+        var pos6 = -1
+        var num = num
+        while (num > 0) {
+            val rem = num % 10
+            num /= 10
+            if (rem == 6) {
+                pos6 = pos
+            }
+            pos++
+        }
+        if (pos6 == -1) return 0
+
+        return 3 * Math.pow(10.0, pos6.toDouble()).toInt()
     }
 }
--- problems/src/test/kotlin/numbers/easy/Maximum69NumberTest.kt
+++ problems/src/test/kotlin/numbers/easy/Maximum69NumberTest.kt
@@ -6,6 +6,30 @@ import org.junit.jupiter.api.Test
 class Maximum69NumberTest {
 
     @Test
+    fun test1StringConversion() {
+        val num = 9669
+        val expected = 9969
+
+        assertEquals(expected, Maximum69Number.maximum69NumberStringConversion(num))
+    }
+
+    @Test
+    fun test2StringConversion() {
+        val num = 9669
+        val expected = 9969
+
+        assertEquals(expected, Maximum69Number.maximum69NumberStringConversion(num))
+    }
+
+    @Test
+    fun test3StringConversion() {
+        val num = 9999
+        val expected = 9999
+
+        assertEquals(expected, Maximum69Number.maximum69NumberStringConversion(num))
+    }
+
+    @Test
     fun test1() {
         val num = 9669
         val expected = 9969
