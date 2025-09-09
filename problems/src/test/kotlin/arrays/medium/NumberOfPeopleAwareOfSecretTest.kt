19c2ba59a46118c15e9c193a8d49b261e6739450
ikss <iksss.88@gmail.com>
2025-09-09 08:45:37 +0200

    2327. Number of People Aware of a Secret

---
+++ problems/src/test/kotlin/arrays/medium/NumberOfPeopleAwareOfSecretTest.kt
@@ -0,0 +1,26 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NumberOfPeopleAwareOfSecretTest {
+    @Test
+    fun test1() {
+        val n = 6
+        val delay = 2
+        val forget = 4
+        val expected = 5
+
+        assertEquals(expected, NumberOfPeopleAwareOfSecret.peopleAwareOfSecret(n, delay, forget))
+    }
+
+    @Test
+    fun test2() {
+        val n = 4
+        val delay = 1
+        val forget = 3
+        val expected = 6
+
+        assertEquals(expected, NumberOfPeopleAwareOfSecret.peopleAwareOfSecret(n, delay, forget))
+    }
+}
