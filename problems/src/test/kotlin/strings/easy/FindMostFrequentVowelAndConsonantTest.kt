0ab4e49488938718e6d0ab9e52514ebe539fcb01
ikss <iksss.88@gmail.com>
2025-09-13 07:04:42 +0200

    3541. Find Most Frequent Vowel and Consonant

---
+++ problems/src/test/kotlin/strings/easy/FindMostFrequentVowelAndConsonantTest.kt
@@ -0,0 +1,22 @@
+package strings.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FindMostFrequentVowelAndConsonantTest {
+    @Test
+    fun test1() {
+        val s = "successes"
+        val expected = 6
+
+        assertEquals(expected, FindMostFrequentVowelAndConsonant.maxFreqSum(s))
+    }
+
+    @Test
+    fun test2() {
+        val s = "aeiaeia"
+        val expected = 3
+
+        assertEquals(expected, FindMostFrequentVowelAndConsonant.maxFreqSum(s))
+    }
+}
