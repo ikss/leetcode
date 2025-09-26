28eeb1fc7d72064bb2d418e439db69a31ed975b1
ikss <iksss.88@gmail.com>
2025-09-26 12:58:03 +0200

    611. Valid Triangle Number (linear)

--- problems/src/main/kotlin/arrays/medium/ValidTriangleNumber.kt
+++ problems/src/main/kotlin/arrays/medium/ValidTriangleNumber.kt
@@ -1,5 +1,6 @@
 package arrays.medium
 
+
 /**
  * Given an integer array nums, return the number of triplets chosen from the array that can make triangles if
  * we take them as side lengths of a triangle.
@@ -7,7 +8,7 @@ package arrays.medium
  * [URL](https://leetcode.com/problems/valid-triangle-number/)
  */
 object ValidTriangleNumber {
-    fun triangleNumber(nums: IntArray): Int {
+    fun triangleNumberCubic(nums: IntArray): Int {
         var result = 0
 
         for (i in 0 until nums.size - 2) {
@@ -25,5 +26,21 @@ object ValidTriangleNumber {
 
     private fun valid(i: Int, j: Int, k: Int): Boolean {
         return i + j > k && i + k > j && j + k > i
+    }
+
+    fun triangleNumberLinear(nums: IntArray): Int {
+        var result = 0
+        nums.sort()
+
+        for (i in 0..<nums.size - 2) {
+            var k = i + 2
+            var j = i + 1
+            while (j < nums.size - 1 && nums[i] != 0) {
+                while (k < nums.size && nums[i] + nums[j] > nums[k]) k++
+                result += k - j - 1
+                j++
+            }
+        }
+        return result
     }
 }
--- problems/src/test/kotlin/arrays/medium/ValidTriangleNumberTest.kt
+++ problems/src/test/kotlin/arrays/medium/ValidTriangleNumberTest.kt
@@ -5,18 +5,34 @@ import org.junit.jupiter.api.Test
 
 class ValidTriangleNumberTest {
     @Test
-    fun test1() {
+    fun test1Cubic() {
         val nums = intArrayOf(2, 2, 3, 4)
         val expected = 3
 
-        assertEquals(expected, ValidTriangleNumber.triangleNumber(nums))
+        assertEquals(expected, ValidTriangleNumber.triangleNumberCubic(nums))
     }
 
     @Test
-    fun test2() {
+    fun test2Cubic() {
         val nums = intArrayOf(4, 2, 3, 4)
         val expected = 4
 
-        assertEquals(expected, ValidTriangleNumber.triangleNumber(nums))
+        assertEquals(expected, ValidTriangleNumber.triangleNumberCubic(nums))
+    }
+
+    @Test
+    fun test1Linear() {
+        val nums = intArrayOf(2, 2, 3, 4)
+        val expected = 3
+
+        assertEquals(expected, ValidTriangleNumber.triangleNumberLinear(nums))
+    }
+
+    @Test
+    fun test2Linear() {
+        val nums = intArrayOf(4, 2, 3, 4)
+        val expected = 4
+
+        assertEquals(expected, ValidTriangleNumber.triangleNumberLinear(nums))
     }
 }
