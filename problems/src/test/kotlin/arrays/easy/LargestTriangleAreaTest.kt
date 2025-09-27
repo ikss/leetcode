1386b991f877a5ea7a14d8e16abd2ce6772211ff
ikss <iksss.88@gmail.com>
2025-09-27 08:16:55 +0200

    812. Largest Triangle Area

---
+++ problems/src/test/kotlin/arrays/easy/LargestTriangleAreaTest.kt
@@ -0,0 +1,22 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LargestTriangleAreaTest {
+    @Test
+    fun test1() {
+        val points = arrayOf(intArrayOf(0, 0), intArrayOf(0, 1), intArrayOf(1, 0), intArrayOf(0, 2), intArrayOf(2, 0))
+        val expected = 2.00000
+
+        assertEquals(expected, LargestTriangleArea.largestTriangleArea(points), 0.00001)
+    }
+
+    @Test
+    fun test2() {
+        val points = arrayOf(intArrayOf(1, 0), intArrayOf(0, 0), intArrayOf(0, 1))
+        val expected = 0.50000
+
+        assertEquals(expected, LargestTriangleArea.largestTriangleArea(points), 0.00001)
+    }
+}
