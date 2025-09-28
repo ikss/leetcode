6c4034c95926bb4f004b6b30447a48715f103965
ikss <iksss.88@gmail.com>
2025-09-28 08:01:46 +0200

    976. Largest Perimeter Triangle (fix link)

--- problems/src/main/kotlin/arrays/easy/LargestPerimeterTriangle.kt
+++ problems/src/main/kotlin/arrays/easy/LargestPerimeterTriangle.kt
@@ -4,7 +4,7 @@ package arrays.easy
  * Given an integer array nums, return the largest perimeter of a triangle with a non-zero area,
  * formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
  *
- * [URL](https://leetcode.com/problems/two-sum/)
+ * [URL](https://leetcode.com/problems/largest-perimeter-triangle/)
  */
 object LargestPerimeterTriangle {
     fun largestPerimeter(nums: IntArray): Int {
