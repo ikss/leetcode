12d936bd79f26ff6da40fd82478d10debb0414cf
ikss <iksss.88@gmail.com>
2025-10-05 22:12:08 +0200

    417. Pacific Atlantic Water Flow

--- problems/src/main/kotlin/arrays/medium/PacificAtlanticWaterFlow.kt
+++ problems/src/main/kotlin/arrays/medium/PacificAtlanticWaterFlow.kt
@@ -19,7 +19,7 @@ import java.util.*
  * [URL](https://leetcode.com/problems/pacific-atlantic-water-flow/)
  */
 object PacificAtlanticWaterFlow {
-    fun pacificAtlantic(heights: Array<IntArray>): List<List<Int>> {
+    fun pacificAtlanticStack(heights: Array<IntArray>): List<List<Int>> {
         val rows = heights.size
         val cols = heights[0].size
         val stack = Stack<Pair<Int, Int>>()
@@ -71,6 +71,62 @@ object PacificAtlanticWaterFlow {
             }
             if (j < heights[0].size - 1 && heights[i][j + 1] >= height) {
                 stack.add(i to j + 1)
+            }
+        }
+    }
+
+    val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
+
+    fun pacificAtlanticQueue(heights: Array<IntArray>): List<List<Int>> {
+        val marker = Array(heights.size) { IntArray(heights[0].size) }
+
+        val queue = ArrayDeque<Pair<Int, Int>>()
+        // add pacific
+        for (r in marker.indices) {
+            queue.offer(r to -1)
+        }
+        for (c in marker[0].indices) {
+            queue.offer(-1 to c)
+        }
+        mark(heights, marker, queue, 1)
+
+        // add atlantic
+        for (r in marker.indices) {
+            queue.offer(r to marker[0].size)
+        }
+        for (c in marker[0].indices) {
+            queue.offer(marker.size to c)
+        }
+        mark(heights, marker, queue, 2)
+
+        // collect result
+        val result = ArrayList<List<Int>>()
+        for (r in marker.indices) {
+            for (c in marker[r].indices) {
+                if (marker[r][c] == 3) {
+                    result.add(listOf(r, c))
+                }
+            }
+        }
+        return result
+    }
+
+
+    private fun mark(heights: Array<IntArray>, marker: Array<IntArray>, queue: ArrayDeque<Pair<Int, Int>>, mark: Int) {
+        while (queue.isNotEmpty()) {
+            val (curRow, curCol) = queue.poll()
+            val oldHeight = if (curRow !in heights.indices || curCol !in heights[0].indices) 0 else heights[curRow][curCol]
+
+            for ((dr, dc) in directions) {
+                val newRow = curRow + dr
+                val newCol = curCol + dc
+
+                if (newRow !in heights.indices || newCol !in heights[0].indices || oldHeight > heights[newRow][newCol]) {
+                    continue
+                }
+                if (marker[newRow][newCol] and mark > 0) continue
+                marker[newRow][newCol] = marker[newRow][newCol] or mark
+                queue.offer(newRow to newCol)
             }
         }
     }
