c9f9a6936d9189b719495b8e00acb2cc991e242a
ikss <iksss.88@gmail.com>
2025-10-28 10:50:10 +0100

    3354. Make Array Elements Equal to Zero

---
+++ problems/src/test/kotlin/arrays/easy/MakeArrayElementsEqualToZeroTest.kt
@@ -0,0 +1,30 @@
+package arrays.easy
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MakeArrayElementsEqualToZeroTest {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(1, 0, 2, 0, 3)
+        val expected = 2
+
+        assertEquals(expected, MakeArrayElementsEqualToZero.countValidSelections(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(2, 3, 4, 0, 4, 1, 0)
+        val expected = 0
+
+        assertEquals(expected, MakeArrayElementsEqualToZero.countValidSelections(nums))
+    }
+
+    @Test
+    fun test3() {
+        val nums = intArrayOf(0, 0, 0, 0, 0)
+        val expected = 10
+
+        assertEquals(expected, MakeArrayElementsEqualToZero.countValidSelections(nums))
+    }
+}
