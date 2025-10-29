8d9c0b27d8d7ae7ae9d0be7a3e41e6f09bbed654
ikss <iksss.88@gmail.com>
2025-10-29 08:43:25 +0100

    3370. Smallest Number With All Set Bits

---
+++ problems/src/main/kotlin/numbers/easy/SmallestNumberWithAllSetBits.kt
@@ -0,0 +1,18 @@
+package numbers.easy
+
+/**
+ * You are given a positive number n.
+ *
+ * Return the smallest number x greater than or equal to n, such that the binary representation of x contains only set bits
+ *
+ * [URL](https://leetcode.com/problems/smallest-number-with-all-set-bits/)
+ */
+object SmallestNumberWithAllSetBits {
+    fun smallestNumber(n: Int): Int {
+        var result = 1
+        while (result <= n) {
+            result *= 2
+        }
+        return result - 1
+    }
+}
