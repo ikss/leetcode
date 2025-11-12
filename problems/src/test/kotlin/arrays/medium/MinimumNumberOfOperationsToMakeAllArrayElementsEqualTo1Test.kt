dbc44d4648c3301c142919b4b61c2ac3c40a61aa
ikss <iksss.88@gmail.com>
2025-11-12 09:04:12 +0100

    2654. Minimum Number of Operations to Make All Array Elements Equal to 1

---
+++ problems/src/test/kotlin/arrays/medium/MinimumNumberOfOperationsToMakeAllArrayElementsEqualTo1Test.kt
@@ -0,0 +1,20 @@
+package arrays.medium
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MinimumNumberOfOperationsToMakeAllArrayElementsEqualTo1Test {
+    @Test
+    fun test1() {
+        val nums = intArrayOf(2, 6, 3, 4)
+        val expected = 4
+        assertEquals(expected, MinimumNumberOfOperationsToMakeAllArrayElementsEqualTo1.minOperations(nums))
+    }
+
+    @Test
+    fun test2() {
+        val nums = intArrayOf(2,10,6,14)
+        val expected = -1
+        assertEquals(expected, MinimumNumberOfOperationsToMakeAllArrayElementsEqualTo1.minOperations(nums))
+    }
+}
