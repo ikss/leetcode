6d049cabc0a573192f77d00e154cd6c56ec696b6
ikss <iksss.88@gmail.com>
2026-01-08 10:30:48 +0100

    1458. Max Dot Product of Two Subsequences (bottom-up dp)

--- problems/src/main/kotlin/hard/MaxDotProductOfTwoSubsequences.kt
+++ problems/src/main/kotlin/hard/MaxDotProductOfTwoSubsequences.kt
@@ -12,7 +12,7 @@ package hard
  * [URL](https://leetcode.com/problems/max-dot-product-of-two-subsequences/)
  */
 object MaxDotProductOfTwoSubsequences {
-    fun maxDotProduct(nums1: IntArray, nums2: IntArray): Int {
+    fun maxDotProductDpTopDown(nums1: IntArray, nums2: IntArray): Int {
         var nums1Max = Int.MIN_VALUE
         var nums1Min = Int.MAX_VALUE
         var nums2Max = Int.MIN_VALUE
@@ -35,10 +35,10 @@ object MaxDotProductOfTwoSubsequences {
         }
 
         val memo = Array(nums1.size) { IntArray(nums2.size) { Int.MIN_VALUE } }
-        return dp(0, 0, nums1, nums2, memo)
+        return dpTopDown(0, 0, nums1, nums2, memo)
     }
 
-    private fun dp(i: Int, j: Int, nums1: IntArray, nums2: IntArray, memo: Array<IntArray>): Int {
+    private fun dpTopDown(i: Int, j: Int, nums1: IntArray, nums2: IntArray, memo: Array<IntArray>): Int {
         if (i == nums1.size || j == nums2.size) {
             return 0
         }
@@ -47,13 +47,47 @@ object MaxDotProductOfTwoSubsequences {
             return memo[i][j]
         }
 
-        val use = nums1[i] * nums2[j] + dp(i + 1, j + 1, nums1, nums2, memo)
+        val use = nums1[i] * nums2[j] + dpTopDown(i + 1, j + 1, nums1, nums2, memo)
 
         memo[i][j] = maxOf(
             use,
-            dp(i + 1, j, nums1, nums2, memo),
-            dp(i, j + 1, nums1, nums2, memo),
+            dpTopDown(i + 1, j, nums1, nums2, memo),
+            dpTopDown(i, j + 1, nums1, nums2, memo),
         )
         return memo[i][j]
+    }
+
+    fun maxDotProductDpBottomUp(nums1: IntArray, nums2: IntArray): Int {
+        var max1 = Int.MIN_VALUE
+        var min1 = Int.MAX_VALUE
+        var max2 = Int.MIN_VALUE
+        var min2 = Int.MAX_VALUE
+
+        for (n in nums1) {
+            max1 = maxOf(max1, n)
+            min1 = minOf(min1, n)
+        }
+        for (n in nums2) {
+            max2 = maxOf(max2, n)
+            min2 = minOf(min2, n)
+        }
+
+        if (max1 < 0 && min2 > 0) {
+            return max1 * min2
+        }
+
+        if (max2 < 0 && min1 > 0) {
+            return max2 * min1
+        }
+
+        val dp = Array(nums1.size + 1) { IntArray(nums2.size + 1) }
+
+        for (i1 in nums1.size - 1 downTo 0) {
+            for (i2 in nums2.size - 1 downTo 0) {
+                dp[i1][i2] = maxOf(dp[i1 + 1][i2 + 1] + nums1[i1] * nums2[i2], dp[i1 + 1][i2], dp[i1][i2 + 1])
+            }
+        }
+
+        return dp[0][0]
     }
 }
--- problems/src/test/kotlin/hard/MaxDotProductOfTwoSubsequencesTest.kt
+++ problems/src/test/kotlin/hard/MaxDotProductOfTwoSubsequencesTest.kt
@@ -5,29 +5,55 @@ import org.junit.jupiter.api.Test
 
 class MaxDotProductOfTwoSubsequencesTest {
     @Test
-    fun test1() {
+    fun test1DpTopDown() {
         val nums1 = intArrayOf(2, 1, -2, 5)
         val nums2 = intArrayOf(3, 0, -6)
         val expected = 18
 
-        assertEquals(expected, MaxDotProductOfTwoSubsequences.maxDotProduct(nums1, nums2))
+        assertEquals(expected, MaxDotProductOfTwoSubsequences.maxDotProductDpTopDown(nums1, nums2))
     }
 
     @Test
-    fun test2() {
+    fun test2DpTopDown() {
         val nums1 = intArrayOf(3, -2)
         val nums2 = intArrayOf(2, -6, 7)
         val expected = 21
 
-        assertEquals(expected, MaxDotProductOfTwoSubsequences.maxDotProduct(nums1, nums2))
+        assertEquals(expected, MaxDotProductOfTwoSubsequences.maxDotProductDpTopDown(nums1, nums2))
     }
 
     @Test
-    fun test3() {
+    fun test3DpTopDown() {
         val nums1 = intArrayOf(-1, -1)
         val nums2 = intArrayOf(1, 1)
         val expected = -1
 
-        assertEquals(expected, MaxDotProductOfTwoSubsequences.maxDotProduct(nums1, nums2))
+        assertEquals(expected, MaxDotProductOfTwoSubsequences.maxDotProductDpTopDown(nums1, nums2))
+    }
+    @Test
+    fun test1DpBottomUp() {
+        val nums1 = intArrayOf(2, 1, -2, 5)
+        val nums2 = intArrayOf(3, 0, -6)
+        val expected = 18
+
+        assertEquals(expected, MaxDotProductOfTwoSubsequences.maxDotProductDpBottomUp(nums1, nums2))
+    }
+
+    @Test
+    fun test2DpBottomUp() {
+        val nums1 = intArrayOf(3, -2)
+        val nums2 = intArrayOf(2, -6, 7)
+        val expected = 21
+
+        assertEquals(expected, MaxDotProductOfTwoSubsequences.maxDotProductDpBottomUp(nums1, nums2))
+    }
+
+    @Test
+    fun test3DpBottomUp() {
+        val nums1 = intArrayOf(-1, -1)
+        val nums2 = intArrayOf(1, 1)
+        val expected = -1
+
+        assertEquals(expected, MaxDotProductOfTwoSubsequences.maxDotProductDpBottomUp(nums1, nums2))
     }
 }
