c5622709688f8a45a2c1b769d0d449b40e92f491
ikss <iksss.88@gmail.com>
2026-02-02 09:02:10 +0100

    3013. Divide an Array Into Subarrays With Minimum Cost II (two heaps)

--- problems/src/main/kotlin/hard/DivideArrayIntoSubarraysWithMinimumCostII.kt
+++ problems/src/main/kotlin/hard/DivideArrayIntoSubarraysWithMinimumCostII.kt
@@ -1,6 +1,7 @@
 package hard
 
-import java.util.PriorityQueue
+import java.util.*
+
 
 /**
  * You are given a 0-indexed array of integers nums of length n, and two positive integers k and dist.
@@ -18,7 +19,7 @@ import java.util.PriorityQueue
  * [URL](https://leetcode.com/problems/adding-spaces-to-a-string/)
  */
 object DivideArrayIntoSubarraysWithMinimumCostII {
-    fun minimumCost(nums: IntArray, k: Int, dist: Int): Long {
+    fun minimumCostMaxHeap(nums: IntArray, k: Int, dist: Int): Long {
         var result = Long.MAX_VALUE
 
         for (i in 1 until nums.size - (k - 2)) {
@@ -46,5 +47,92 @@ object DivideArrayIntoSubarraysWithMinimumCostII {
         }
 
         return result
+    }
+
+    class Container(private val k: Int) {
+        private var st1Size = 0
+        private var st2Size = 0
+
+        private val st1 = TreeMap<Int, Int>()
+        private val st2 = TreeMap<Int, Int>()
+        internal var sm = 0L
+
+        private fun removeOne(map: TreeMap<Int, Int>, key: Int) {
+            val count = map[key]!!
+            if (count == 1) {
+                map.remove(key)
+            } else {
+                map[key] = count - 1
+            }
+        }
+
+        private fun addOne(map: TreeMap<Int, Int>, key: Int) {
+            map[key] = map.getOrDefault(key, 0) + 1
+        }
+
+        private fun adjust() {
+            while (st1Size < k && st2.isNotEmpty()) {
+                val x = st2.firstKey()!!
+                addOne(st1, x)
+                st1Size++
+                sm += x.toLong()
+                removeOne(st2, x)
+                st2Size--
+            }
+            while (st1Size > k) {
+                val x = st1.lastKey()!!
+                addOne(st2, x)
+                st2Size++
+                removeOne(st1, x)
+                st1Size--
+                sm -= x.toLong()
+            }
+        }
+
+        // insert element x
+        fun add(x: Int) {
+            if (st2.isNotEmpty() && x >= st2.firstKey()!!) {
+                addOne(st2, x)
+                st2Size++
+            } else {
+                addOne(st1, x)
+                st1Size++
+                sm += x
+            }
+            adjust()
+        }
+
+        // delete element x
+        fun erase(x: Int) {
+            if (st1.containsKey(x)) {
+                removeOne(st1, x)
+                st1Size--
+                sm -= x
+            } else if (st2.containsKey(x)) {
+                removeOne(st2, x)
+                st2Size--
+            }
+            adjust()
+        }
+    }
+
+    fun minimumCostTwoHeaps(nums: IntArray, k: Int, dist: Int): Long {
+        val n = nums.size
+        val cnt = Container(k - 2)
+        for (i in 1..<k - 1) {
+            cnt.add(nums[i])
+        }
+
+        var result = cnt.sm + nums[k - 1]
+        for (i in k..<n) {
+            val j = i - dist - 1
+            if (j > 0) {
+                cnt.erase(nums[j])
+            }
+            cnt.add(nums[i - 1])
+            result = minOf(result, cnt.sm + nums[i])
+        }
+
+        return result + nums[0]
     }
 }
--- problems/src/test/kotlin/hard/DivideArrayIntoSubarraysWithMinimumCostIITest.kt
+++ problems/src/test/kotlin/hard/DivideArrayIntoSubarraysWithMinimumCostIITest.kt
@@ -5,42 +5,82 @@ import org.junit.jupiter.api.Test
 
 class DivideArrayIntoSubarraysWithMinimumCostIITest {
     @Test
-    fun test1() {
+    fun test1MaxHeap() {
         val nums = intArrayOf(1, 3, 2, 6, 4, 2)
         val k = 3
         val dist = 3
         val expected = 5L
 
-        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCost(nums, k, dist))
+        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCostMaxHeap(nums, k, dist))
     }
 
     @Test
-    fun test2() {
+    fun test2MaxHeap() {
         val nums = intArrayOf(10, 1, 2, 2, 2, 1)
         val k = 4
         val dist = 3
         val expected = 15L
 
-        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCost(nums, k, dist))
+        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCostMaxHeap(nums, k, dist))
     }
 
     @Test
-    fun test3() {
+    fun test3MaxHeap() {
         val nums = intArrayOf(10, 8, 18, 9)
         val k = 3
         val dist = 1
         val expected = 36L
 
-        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCost(nums, k, dist))
+        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCostMaxHeap(nums, k, dist))
     }
 
     @Test
-    fun test4() {
-        val nums = intArrayOf(2,6,3,8,3,1,1)
+    fun test4MaxHeap() {
+        val nums = intArrayOf(2, 6, 3, 8, 3, 1, 1)
         val k = 3
         val dist = 4
         val expected = 4L
 
-        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCost(nums, k, dist))
+        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCostMaxHeap(nums, k, dist))
+    }
+
+    @Test
+    fun test1TwoHeaps() {
+        val nums = intArrayOf(1, 3, 2, 6, 4, 2)
+        val k = 3
+        val dist = 3
+        val expected = 5L
+
+        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCostTwoHeaps(nums, k, dist))
+    }
+
+    @Test
+    fun test2TwoHeaps() {
+        val nums = intArrayOf(10, 1, 2, 2, 2, 1)
+        val k = 4
+        val dist = 3
+        val expected = 15L
+
+        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCostTwoHeaps(nums, k, dist))
+    }
+
+    @Test
+    fun test3TwoHeaps() {
+        val nums = intArrayOf(10, 8, 18, 9)
+        val k = 3
+        val dist = 1
+        val expected = 36L
+
+        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCostTwoHeaps(nums, k, dist))
+    }
+
+    @Test
+    fun test4TwoHeaps() {
+        val nums = intArrayOf(2, 6, 3, 8, 3, 1, 1)
+        val k = 3
+        val dist = 4
+        val expected = 4L
+
+        assertEquals(expected, DivideArrayIntoSubarraysWithMinimumCostII.minimumCostTwoHeaps(nums, k, dist))
     }
 }
