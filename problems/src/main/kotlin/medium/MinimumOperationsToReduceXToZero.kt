d08dca5e21fd3514524d931c1a2c78f863636574
ikss <iksss.88@gmail.com>
2026-09-23 09:23:17 +0200

    1658. Minimum Operations to Reduce X to Zero

--- problems/src/main/kotlin/medium/MinimumOperationsToReduceXToZero.kt
+++ problems/src/main/kotlin/medium/MinimumOperationsToReduceXToZero.kt
@@ -1,6 +1,6 @@
 package medium
 
-import java.util.*
+import kotlin.math.min
 
 /**
  * You are given an integer array nums and an integer x. In one operation,
@@ -13,14 +13,14 @@ import java.util.*
  * [URL](https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/)
  */
 object MinimumOperationsToReduceXToZero {
-    fun minOperations(nums: IntArray, x: Int): Int {
+    fun minOperationsDp(nums: IntArray, x: Int): Int {
         val n = nums.size
 
         var dp = IntArray(n + 1)
         var result = n + 1
-        for (l in 0 .. n) {
+        for (l in 0..n) {
             val newDp = IntArray(n + 1)
-            for (r in 0 .. n - l) {
+            for (r in 0..n - l) {
                 if (l == 0 && r == 0) continue
 
                 if (l == 0) {
@@ -40,5 +40,27 @@ object MinimumOperationsToReduceXToZero {
         }
 
         return if (result == n + 1) -1 else result
+    }
+
+    fun minOperationsSlidingWindow(nums: IntArray, x: Int): Int {
+        var current = 0
+        for (num in nums) {
+            current += num
+        }
+        val n = nums.size
+        var result = Int.MAX_VALUE
+        var left = 0
+
+        for (right in 0..<n) {
+            current -= nums[right]
+            while (current < x && left <= right) {
+                current += nums[left]
+                left += 1
+            }
+            if (current == x) {
+                result = minOf(result, (n - 1 - right) + left)
+            }
+        }
+        return if (result != Int.MAX_VALUE) result else -1
     }
 }
--- problems/src/test/kotlin/medium/MinimumOperationsToReduceXToZeroTest.kt
+++ problems/src/test/kotlin/medium/MinimumOperationsToReduceXToZeroTest.kt
@@ -5,30 +5,56 @@ import org.junit.jupiter.api.Test
 
 class MinimumOperationsToReduceXToZeroTest {
     @Test
-    fun test1() {
+    fun test1Dp() {
         val nums = intArrayOf(1, 1, 4, 2, 3)
         val x = 5
         val expected = 2
 
-        assertEquals(expected, MinimumOperationsToReduceXToZero.minOperations(nums, x))
+        assertEquals(expected, MinimumOperationsToReduceXToZero.minOperationsDp(nums, x))
     }
 
     @Test
-    fun test2() {
+    fun test2Dp() {
         val nums = intArrayOf(5, 6, 7, 8, 9)
         val x = 4
         val expected = -1
 
-        assertEquals(expected, MinimumOperationsToReduceXToZero.minOperations(nums, x))
+        assertEquals(expected, MinimumOperationsToReduceXToZero.minOperationsDp(nums, x))
     }
 
     @Test
-    fun test3() {
+    fun test3Dp() {
         val nums = intArrayOf(3, 2, 20, 1, 1, 3)
         val x = 10
         val expected = 5
 
-        assertEquals(expected, MinimumOperationsToReduceXToZero.minOperations(nums, x))
+        assertEquals(expected, MinimumOperationsToReduceXToZero.minOperationsDp(nums, x))
     }
 
+    @Test
+    fun test1SlidingWindow() {
+        val nums = intArrayOf(1, 1, 4, 2, 3)
+        val x = 5
+        val expected = 2
+
+        assertEquals(expected, MinimumOperationsToReduceXToZero.minOperationsSlidingWindow(nums, x))
+    }
+
+    @Test
+    fun test2SlidingWindow() {
+        val nums = intArrayOf(5, 6, 7, 8, 9)
+        val x = 4
+        val expected = -1
+
+        assertEquals(expected, MinimumOperationsToReduceXToZero.minOperationsSlidingWindow(nums, x))
+    }
+
+    @Test
+    fun test3SlidingWindow() {
+        val nums = intArrayOf(3, 2, 20, 1, 1, 3)
+        val x = 10
+        val expected = 5
+
+        assertEquals(expected, MinimumOperationsToReduceXToZero.minOperationsSlidingWindow(nums, x))
+    }
 }
